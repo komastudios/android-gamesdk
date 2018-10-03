0679d73f80cc47ecef23dbc2b1623f5fc3f62ad3
Dan Stoza <stoza@google.com>
2018-10-02 17:01:04 -0700

    Swappy + Bouncy Ball improvements
    This change does a few things:
    1) Fixes a bug in SF-to-app offset computation
    2) Adds a new "Hot Pocket" "feature" that spins a thread to keep CPU
       governors ramped up, which helps with SF FrameMissed due to the
       abnormally low load of the Bouncy Ball app
    3) Revises the frame scheduling logic, taking into account how long
       eglSwapBuffers is taking and compensating by waking up a bit earlier
    
    Test: manual
    Change-Id: Iafb61afc48b08f9d2f552a20eaf3088298a117f8

--- swappy/Swappy.h
+++ swappy/Swappy.h
@@ -50,8 +50,6 @@ class Swappy {
 
     static bool swap(EGLDisplay display, EGLSurface surface);
 
-    static void sleepModulo(int32_t modulo);
-
   private:
     static void init(std::chrono::nanoseconds refreshPeriod,
                      std::chrono::nanoseconds appOffset,
@@ -64,12 +62,13 @@ class Swappy {
     void onSettingsChanged();
 
     void handleChoreographer();
-    void wakeClient();
+    std::chrono::nanoseconds wakeClient();
+
+    void startFrame();
+
+    void waitUntil(int32_t frameNumber);
 
     void waitOneFrame();
-
-    // Waits for at least one frame and then until the frame number % swap interval == modulo
-    int32_t waitModulo(int32_t modulo);
 
     // Waits for the next frame, considering both Choreographer and the prior frame's completion
     void waitForNextFrame(EGLDisplay display);
@@ -81,6 +80,9 @@ class Swappy {
     // using eglPresentationTimeANDROID
     bool setPresentationTime(EGLDisplay display, EGLSurface surface);
 
+    void updateSwapDuration(std::chrono::nanoseconds duration);
+    std::atomic<std::chrono::nanoseconds> mSwapDuration;
+
     static std::mutex sInstanceMutex;
     static std::unique_ptr<Swappy> sInstance;
 
@@ -89,10 +91,14 @@ class Swappy {
 
     std::mutex mWaitingMutex;
     std::condition_variable mWaitingCondition;
+    std::chrono::steady_clock::time_point mCurrentFrameTimestamp = std::chrono::steady_clock::now();
     int32_t mCurrentFrame = 0;
 
     std::mutex mEglMutex;
     std::unique_ptr<EGL> mEgl;
+
+    int32_t mTargetFrame = 0;
+    std::chrono::steady_clock::time_point mPresentationTime = std::chrono::steady_clock::now();
 
     std::unique_ptr<ChoreographerFilter> mChoreographerFilter;
 };
