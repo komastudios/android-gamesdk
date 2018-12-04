711975ee14d9adcf2e3ee505114d9b693c8a9969
Francesco Carucci <fcarucci@google.com>
2018-12-04 11:45:41 -0800

    Add CpuInfo class to discover CPU topology. Set Affinity of the Choreographer thread to little cores.
    Choreographer thread running on a Pixel 3:
      - big core @ 825mhz: 0.064ms
      - little core @ 576mhz: 0.171ms
    
    Test: Checked if the thread is running exclusively on the little cores by taking a systrace before and after the patch to compare.
    
    Change-Id: I40b82d08724ed247b78e70ec85389bdf0521e903

--- src/swappy/src/main/cpp/Swappy.cpp
+++ src/swappy/src/main/cpp/Swappy.cpp
@@ -245,7 +245,6 @@ Swappy::Swappy(JavaVM *vm,
               vm,
               [this]{ handleChoreographer(); }))
 {
-
     Settings::getInstance()->addListener([this]() { onSettingsChanged(); });
 
     ALOGI("Initialized Swappy with refreshPeriod=%lld, appOffset=%lld, sfOffset=%lld",
