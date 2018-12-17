4dd44b88dffd3f3bfc4254b3a2664ec46b6685f5
Ady Abraham <adyabr@google.com>
2018-12-17 15:51:01 -0800

    swappy: add auto swap interval detection
    Swappy can detect cases where frame rendering is too fast or too slow.
    Based on this information swappy will tune its internal swap interval
    to better pace the frames. This change also introduces an API change
    to disable and override this mode.
    
    Test: run swappy with different rendering times and run systrace to
          examine swap interval
    
    Change-Id: Id7e5a755bbdcb45b5c03e50b89124f5d3fd16189

--- src/swappy/src/main/cpp/Swappy.h
+++ src/swappy/src/main/cpp/Swappy.h
@@ -29,6 +29,8 @@
 #include "swappy/swappy.h"
 #include "swappy/swappy_extra.h"
 
+#include "Thread.h"
+
 class ChoreographerFilter;
 class ChoreographerThread;
 class EGL;
@@ -63,6 +65,12 @@ class Swappy {
     // Pass callbacks for tracing within the swap function
     static void addTracer(const SwappyTracer *tracer);
 
+    static uint64_t getSwapIntervalNS();
+
+    static void setAutoSwapInterval(bool enabled);
+
+    static void overrideAutoSwapInterval(uint64_t swap_ns);
+
     static void destroyInstance();
 
 private:
@@ -79,6 +87,7 @@ private:
     void preWaitCallbacks();
     void postWaitCallbacks();
     void startFrameCallbacks();
+    void swapIntervalChangedCallbacks();
 
     void onSettingsChanged();
 
@@ -102,12 +111,18 @@ private:
     bool setPresentationTime(EGLDisplay display, EGLSurface surface);
 
     void updateSwapDuration(std::chrono::nanoseconds duration);
-    std::atomic<std::chrono::nanoseconds> mSwapDuration;
+
+    void recordFrameTime(int frames);
+
+    bool updateSwapInterval();
+
+    std::atomic<std::chrono::nanoseconds> mSwapDuration = std::chrono::nanoseconds(0);
 
     static std::mutex sInstanceMutex;
     static std::unique_ptr<Swappy> sInstance;
 
-    std::atomic<int32_t> mSwapInterval{1};
+    std::atomic<int32_t> mSwapInterval;
+    std::atomic<int32_t> mAutoSwapInterval;
 
     std::mutex mWaitingMutex;
     std::condition_variable mWaitingCondition;
@@ -134,7 +149,14 @@ private:
         std::list<Tracer> preSwapBuffers;
         std::list<Tracer> postSwapBuffers;
         std::list<Tracer> startFrame;
+        std::list<Tracer> swapIntervalChanged;
     };
 
     SwappyTracerCallbacks mInjectedTracers;
+
+    std::mutex mFrameDurationsMutex;
+    std::list<int> mFrameDurations GUARDED_BY(mFrameDurationsMutex);
+    int mFrameDurationsSum GUARDED_BY(mFrameDurationsMutex) = 0;
+    int mFrameDurationSamples GUARDED_BY(mFrameDurationsMutex);
+    bool mAutoSwapIntervalEnabled GUARDED_BY(mFrameDurationsMutex) = true;
 };
