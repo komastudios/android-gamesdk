6e97b4643d9a6ed6621e9668ac245fc9a89e5e9a
Ady Abraham <adyabr@google.com>
2018-12-20 20:54:48 -0800

    swappy: add stats based on ANDROID_get_frame_timestamps
    Add a feature to Swappy to collect frame statistics.
    
    Test: logcat -s FrameStatistics
    Change-Id: I4de420ab6ce0c8ac35882c04cecf0e243cb6c701

--- include/swappy/swappy_extra.h
+++ include/swappy/swappy_extra.h
@@ -21,6 +21,9 @@
 #include <EGL/eglext.h>
 #include <jni.h>
 
+#define MAX_FRAME_BUCKETS 6
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -52,6 +55,50 @@ void Swappy_injectTracer(const SwappyTracer *t);
 // NOTE: Swappy may still change this value based on new frames rendering time. To completely
 // override auto swap interval value app needs to call first to Swappy_setAutoSwapInterval(false);
 void Swappy_setAutoSwapInterval(bool enabled);
+
+// toggle statistics collection on/off
+// by default, stats collection if off (and there is no overhead related to stats).
+// App can turn on stats collection by calling Swappy_setStatsMode(true).
+// Then, App is expected to call Swappy_recordFrameStart for each frame before starting to do
+// any CPU related work related to that frame (such as updating the model).
+// Stats will be logged to logcat with 'FrameStatistics' tag. An App can get the stats by calling
+// Swappy_getStats
+void Swappy_enableStats(bool enabled);
+
+struct Swappy_Stats {
+    // total frames swapped by swappy
+    uint64_t totalFrames;
+
+    // histogram of the number of screen refreshes a frame waited in the compositor queue after
+    // rendering was completed.
+    // for example:
+    //     if a frame waited 2 refresh periods in the compositor queue after rendering was done,
+    //     the frame will be counted in idleFrames[2]
+    uint64_t idleFrames[MAX_FRAME_BUCKETS];
+
+    // histogram of the number of screen refreshes passed between the requested presentation time
+    // and the actual present time.
+    // for example:
+    //     if a frame was presented 2 refresh periods after the requested timestamp swappy set,
+    //     the frame will be counted in lateFrames[2]
+    uint64_t lateFrames[MAX_FRAME_BUCKETS];
+
+    // histogram of the number of screen refreshes passed between two consecutive frames
+    // for example:
+    //     if frame N was presented 2 refresh periods after frame N-1
+    //     frame N will be counted in offsetFromPrevFrame[2]
+    uint64_t offsetFromPrevFrame[MAX_FRAME_BUCKETS];
+
+    // histogram of the number of screen refreshes passed between the call to
+    // Swappy_recordFrameStart and the actual present time.
+    //     if a frame was presented 2 refresh periods after the call to Swappy_recordFrameStart
+    //     the frame will be counted in latencyFrames[2]
+    uint64_t latencyFrames[MAX_FRAME_BUCKETS];
+};
+
+void Swappy_recordFrameStart(EGLDisplay display, EGLSurface surface);
+
+void Swappy_getStats(Swappy_Stats *);
 
 #ifdef __cplusplus
 };
