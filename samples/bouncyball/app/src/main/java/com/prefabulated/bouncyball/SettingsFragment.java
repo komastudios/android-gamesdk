87a1aa2b373f1c784148bc136b919871d8dbd569
Ady Abraham <adyabr@google.com>
2018-12-21 17:31:39 -0800

    bouncyball: build swap interval option menu based on refresh rate
    Build swap interval menu dynamically based in refresh rate.
    
    Test: run bouncy ball on various devices
    Change-Id: I224fe86fc18bc9ba56aaca69ed8e30533e354c6f

--- samples/bouncyball/app/src/main/java/com/prefabulated/bouncyball/SettingsFragment.java
+++ samples/bouncyball/app/src/main/java/com/prefabulated/bouncyball/SettingsFragment.java
@@ -21,7 +21,12 @@ import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import androidx.preference.Preference;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
@@ -29,7 +34,7 @@ import androidx.preference.PreferenceScreen;
 public class SettingsFragment
         extends PreferenceFragmentCompat
         implements SharedPreferences.OnSharedPreferenceChangeListener {
-    private Preference mSwapIntervalPreference;
+    private ListPreference mSwapIntervalPreference;
     private String mSwapIntervalKey;
 
     @Override
@@ -42,15 +47,40 @@ public class SettingsFragment
             Preference preference = preferenceScreen.getPreference(i);
             final String key = preference.getKey();
             if (key != null && key.equals(mSwapIntervalKey)) {
-                mSwapIntervalPreference = preference;
+                mSwapIntervalPreference = (ListPreference)preference;
             }
         }
+
+        // fill the swap interval list based on the screen refresh rate
+        List<String> entries = new ArrayList<>();
+        List<String> entryValues = new ArrayList<>();
+
+        float refreshRate = getActivity().getWindowManager().getDefaultDisplay().getRefreshRate();
+        for(int dev = 1; refreshRate / dev >= 20; dev++) {
+            int fps = (int)(refreshRate / dev);
+            float ms =  1000 / (refreshRate / dev);
+
+            entries.add(String.format(Locale.US, "%.2fms (%dfps)", ms, fps));
+            entryValues.add(Float.toString(ms));
+        }
+
+        entries.add(String.format(Locale.US, "No pacing"));
+        entryValues.add(Float.toString(100));
+
+        mSwapIntervalPreference.setEntries(entries.toArray(new String[0]));
+        mSwapIntervalPreference.setEntryValues(entryValues.toArray(new String[0]));
+        mSwapIntervalPreference.setDefaultValue(entries.get(0));
 
         Context context = getContext();
         if (context != null) {
             SharedPreferences sharedPreferences =
                     PreferenceManager.getDefaultSharedPreferences(context);
             sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+            // set default if it doesn't exist
+            if (sharedPreferences.getString(mSwapIntervalKey, null) == null)
+                sharedPreferences.edit()
+                        .putString(mSwapIntervalKey, entryValues.get(0))
+                        .apply();
             updateSwapIntervalSummary(sharedPreferences.getString(mSwapIntervalKey, null));
         }
     }
@@ -65,10 +95,11 @@ public class SettingsFragment
         }
 
         float swapInterval = Float.parseFloat(swapIntervalString);
-        String summary = resources.getString(R.string.swap_interval_summary_prologue);
-        summary += swapInterval;
-        summary += resources.getString(R.string.swap_interval_summary_epilogue);
-        mSwapIntervalPreference.setSummary(summary);
+        mSwapIntervalPreference.setSummary(String.format(Locale.US,
+                "%s %.2f %s",
+                resources.getString(R.string.swap_interval_summary_prologue),
+                swapInterval,
+                resources.getString(R.string.swap_interval_summary_epilogue)));
     }
 
     @Override
