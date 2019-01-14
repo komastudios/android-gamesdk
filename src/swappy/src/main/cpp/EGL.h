2d62d90de01a76793fd0eb6d5525cc157673c67a
Ady Abraham <adyabr@google.com>
2019-01-14 13:12:05 -0800

    swappy: reduce frame latency in auto mode
    This change introduces a new feature to auto mode where
    swappy will try to reduce frame latency by scheduling cpu and gpu
    work in the same pipeline stage (as opposed to previous
    behavior where swappy will schedule cpu and gpu work in a different
    pipeline stage)
    
    Test: run bouncyball with different workloads and collected stats
    Bug: 122345769
    Change-Id: I8bb78744fc525bfdb9603fde9dc7d2d7b90962bb

--- src/swappy/src/main/cpp/EGL.h
+++ src/swappy/src/main/cpp/EGL.h
@@ -16,11 +16,15 @@
 
 #pragma once
 
+#include <chrono>
 #include <mutex>
 #include <optional>
+#include <thread>
 
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
+
+using std::chrono::nanoseconds;
 
 namespace swappy {
 
@@ -39,23 +43,24 @@ class EGL {
         EGLnsecsANDROID presented;
     };
 
-    explicit EGL(std::chrono::nanoseconds refreshPeriod, ConstructorTag)
+    explicit EGL(nanoseconds refreshPeriod, ConstructorTag)
         : mRefreshPeriod(refreshPeriod) {}
 
-    static std::unique_ptr<EGL> create(std::chrono::nanoseconds refreshPeriod);
+    static std::unique_ptr<EGL> create(nanoseconds refreshPeriod);
 
     void resetSyncFence(EGLDisplay display);
     bool lastFrameIsComplete(EGLDisplay display);
     bool setPresentationTime(EGLDisplay display,
                              EGLSurface surface,
                              std::chrono::steady_clock::time_point time);
+    nanoseconds getFencePendingTime() { return mFenceWaiter.getFencePendingTime(); }
 
     // for stats
     std::optional<EGLuint64KHR> getNextFrameId(EGLDisplay dpy, EGLSurface surface);
     std::unique_ptr<FrameTimestamps> getFrameTimestamps(EGLDisplay dpy, EGLSurface surface, EGLuint64KHR frameId);
 
   private:
-    const std::chrono::nanoseconds mRefreshPeriod;
+    const nanoseconds mRefreshPeriod;
 
     using eglPresentationTimeANDROID_type = EGLBoolean (*)(EGLDisplay, EGLSurface, EGLnsecsANDROID);
     eglPresentationTimeANDROID_type eglPresentationTimeANDROID = nullptr;
@@ -65,6 +70,7 @@ class EGL {
     eglDestroySyncKHR_type eglDestroySyncKHR = nullptr;
     using eglGetSyncAttribKHR_type = EGLBoolean (*)(EGLDisplay, EGLSyncKHR, EGLint, EGLint *);
     eglGetSyncAttribKHR_type eglGetSyncAttribKHR = nullptr;
+
 
     using eglGetError_type = EGLint (*)(void);
     eglGetError_type eglGetError = nullptr;
@@ -78,6 +84,32 @@ class EGL {
 
     std::mutex mSyncFenceMutex;
     EGLSyncKHR mSyncFence = EGL_NO_SYNC_KHR;
+
+    class FenceWaiter {
+    public:
+        FenceWaiter();
+        ~FenceWaiter();
+
+        void onFenceCreation(EGLDisplay display, EGLSyncKHR syncFence);
+        void waitForIdle();
+        nanoseconds getFencePendingTime();
+
+    private:
+        using eglClientWaitSyncKHR_type = EGLBoolean (*)(EGLDisplay, EGLSyncKHR, EGLint, EGLTimeKHR);
+        eglClientWaitSyncKHR_type eglClientWaitSyncKHR = nullptr;
+
+        void threadMain();
+        std::thread mFenceWaiter;
+        std::mutex mFenceWaiterLock;
+        std::condition_variable mFenceWaiterCondition;
+        bool mFenceWaiterRunning = true;
+        bool mFenceWaiterPending = false;
+        std::atomic<nanoseconds> mFencePendingTime;
+        EGLDisplay mDisplay;
+        EGLSyncKHR mSyncFence = EGL_NO_SYNC_KHR;
+    };
+
+    FenceWaiter mFenceWaiter;
 };
 
 } // namespace swappy
