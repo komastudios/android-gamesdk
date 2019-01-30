d546cdda584cb3f220346c405c65fa565811d259
Francesco Carucci <fcarucci@google.com>
2019-01-30 13:14:32 -0800

    Read system properties to disable Swappy during initialization.
    Test: tested manually.
    Change-Id: I30925592172920b7e360cf07b6a4cf2576c5a820

--- src/swappy/Swappy.h
+++ src/swappy/Swappy.h
@@ -134,6 +134,8 @@ private:
 
     static Swappy *getInstance();
 
+    bool enabled() const { return !disableSwappy; }
+
     EGL *getEgl();
 
     bool swapInternal(EGLDisplay display, EGLSurface surface);
@@ -183,6 +185,8 @@ private:
                       const std::chrono::nanoseconds& upperBound,
                       const std::chrono::nanoseconds& lowerBound,
                       const int32_t& newSwapInterval) REQUIRES(mFrameDurationsMutex);
+
+    bool disableSwappy = false;
 
     int32_t nanoToSwapInterval(std::chrono::nanoseconds);
 
