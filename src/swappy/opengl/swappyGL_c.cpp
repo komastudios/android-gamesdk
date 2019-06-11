da8ce08f0822b92053f9fb2ba1b06d0d3a3e8f23
Will Osborn <willosborn@google.com>
2019-06-11 12:18:48 +0100

    Make the injectTracer symbols always visible for Tuning Fork
    Test: ./gradlew swappyUnityZip
    Test: Build Unity with Swappy
    Test:  and check symbol visibility with readelf
    Change-Id: Ib9df99a087bbdcf05f6cd3a924fd7458f749016a

--- src/swappy/opengl/swappyGL_c.cpp
+++ src/swappy/opengl/swappyGL_c.cpp
@@ -30,6 +30,8 @@ extern "C" {
 
 void SwappyGL_init_internal(JNIEnv *env, jobject jactivity) {
     SwappyGL::init(env, jactivity);
+    // Call injectTracer to make sure the symbol is always active
+    SwappyGL_injectTracer(nullptr);
 }
 
 void SwappyGL_destroy() {
