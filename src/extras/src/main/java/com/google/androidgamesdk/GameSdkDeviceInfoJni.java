c7e71813a39a1021c8f775d31de4f513068d90ec
Florian Rival <florianrival@google.com>
2019-07-19 15:35:55 +0100

    Update device_info and add app to test it standalone
    * Add proto generation in the extras build.gradle for Java (so that apps can use it without building the proto by themselves)
    * Updated protobuf.cmake so that the generation is re-triggered if the proto source file is changed.
    * Add more info in the test app: list the compressed texture format and their support (true/false).
    
    Test: Properly logging information on Pixel 2, Samsung Galaxy S7, Asus ZenFone 2 (PVRTC), Nvidia SHIELD tablet (DXT5), Motorola Moto G
    Test: TODO: getline is not existing when compiling for some NDK
    version?
    
    Change-Id: I8eee7d498d510d2e1947cfd627a12b360df1e632

---
+++ src/extras/src/main/java/com/google/androidgamesdk/GameSdkDeviceInfoJni.java
@@ -0,0 +1,47 @@
+package com.google.androidgamesdk;
+
+/** JNI api for getting device information */
+public class GameSdkDeviceInfoJni {
+  private static Exception initializationException;
+
+  static {
+    try {
+      System.loadLibrary("game_sdk_device_info_jni");
+    } catch(Exception exception) {
+      initializationException = exception;
+    }
+  }
+
+  /**
+   * Returns a byte array, which is a serialized proto containing device information, or
+   * null if the native library "game_sdk_device_info_jni" could not be loaded.
+   *
+   * @return Optional with the serialized byte array, representing game sdk device info with errors,
+   * or null.
+   */
+  public static byte[] tryGetProtoSerialized() {
+    if (initializationException != null) {
+      return null;
+    }
+
+    return getProtoSerialized();
+  }
+
+
+  /**
+   * Returns the exception that was caught when trying to load the library, if any.
+   * Otherwise, returns null.
+   *
+   * @return The caught Exception or null.
+   */
+  public static Exception getInitializationException() {
+    return initializationException;
+  }
+
+  /**
+   * Returns a byte array, which is a serialized proto.
+   *
+   * @return serialized byte array, representing game sdk device info with errors.
+   */
+  private static native byte[] getProtoSerialized();
+}
