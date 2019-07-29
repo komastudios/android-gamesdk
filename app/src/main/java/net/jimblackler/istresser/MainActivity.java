6c1b2255dbd42c3ec0fe5aef061f9e08467a6259
Jim Blackler <jimblackler@google.com>
2019-07-29 13:05:42 +0100

    Check in of IDE-generated files.

---
+++ app/src/main/java/net/jimblackler/istresser/MainActivity.java
@@ -0,0 +1,13 @@
+package net.jimblackler.istresser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
