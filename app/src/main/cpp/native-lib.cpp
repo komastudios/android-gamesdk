0235a96e640041e9397e65fbb0d0bd1627a2286a
Jim Blackler <jimblackler@google.com>
2019-08-01 15:21:55 +0100

    Add on trim logging and more memory tests.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,8 +1,13 @@
 #include <android/log.h>
 #include <jni.h>
+#include <list>
+#include <mutex>
 #include <string>
 
 constexpr auto appname = "istresser";
+
+std::list<char *> allocated;
+std::mutex mtx;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_net_jimblackler_istresser_MainActivity_stringFromJNI(
@@ -13,12 +18,30 @@ Java_net_jimblackler_istresser_MainActivity_stringFromJNI(
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_net_jimblackler_istresser_MainActivity_nativeConsume(JNIEnv *env, jobject instance, jint bytes) {
-  __android_log_print(ANDROID_LOG_VERBOSE, appname, "Allocating %d", bytes);
+Java_net_jimblackler_istresser_MainActivity_nativeConsume(JNIEnv *env, jobject instance,
+                                                          jint bytes) {
+  __android_log_print(ANDROID_LOG_INFO, appname, "Allocating %d", bytes);
 
+  mtx.lock();
   auto byte_count = (size_t) bytes;
   char *data = (char *) malloc(byte_count);
+  allocated.push_back(data);
+  if (!data) {
+    __android_log_print(ANDROID_LOG_WARN, appname, "Could not allocate");
+    return;
+  }
   for (int count = 0; count < byte_count; count++) {
     data[count] = (char) count;
   }
+  mtx.unlock();
+}
+extern "C" JNIEXPORT void JNICALL
+Java_net_jimblackler_istresser_MainActivity_freeAll(JNIEnv *env, jobject instance) {
+  mtx.lock();
+  __android_log_print(ANDROID_LOG_INFO, appname, "Freeing %lu entries", allocated.size());
+  while(!allocated.empty()) {
+    free(allocated.front());
+    allocated.pop_front();
+  }
+  mtx.unlock();
 }
--- app/src/main/java/net/jimblackler/istresser/MainActivity.java
+++ app/src/main/java/net/jimblackler/istresser/MainActivity.java
@@ -3,17 +3,28 @@ package net.jimblackler.istresser;
 import android.os.Bundle;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multiset;
+
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String TAG = MainActivity.class.getSimpleName();
 
   static {
     System.loadLibrary("native-lib");
   }
+
+  private ArrayList<byte[]> data = Lists.newArrayList();
+
+  private Multiset<Integer> onTrims = HashMultiset.create();
 
   private static String memoryString(long bytes) {
     return String.format(Locale.getDefault(), "%.1f MB", (float) bytes / (1024 * 1024));
@@ -33,27 +44,96 @@ public class MainActivity extends AppCompatActivity {
       public void run() {
 
         nativeConsume(1024 * 512);
+        //jvmConsume(1024 * 512);
 
-        runOnUiThread(() -> {
-          TextView freeMemory = findViewById(R.id.freeMemory);
-          freeMemory.setText(memoryString(Runtime.getRuntime().freeMemory()));
-
-          TextView totalMemory = findViewById(R.id.totalMemory);
-          totalMemory.setText(memoryString(Runtime.getRuntime().totalMemory()));
-
-          TextView maxMemory = findViewById(R.id.maxMemory);
-          maxMemory.setText(memoryString(Runtime.getRuntime().maxMemory()));
-
-          TextView nativeHeap = findViewById(R.id.nativeHeap);
-          nativeHeap.setText(memoryString(Debug.getNativeHeapSize()));
-
-          TextView nativeAllocated = findViewById(R.id.nativeAllocated);
-          nativeAllocated.setText(memoryString(Debug.getNativeHeapAllocatedSize()));
-        });
+        updateInfo();
       }
     }, 0, 1000 / 50);
   }
 
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.i(TAG, "onDestroy");
+  }
+
+  private void updateInfo() {
+    runOnUiThread(() -> {
+      TextView freeMemory = findViewById(R.id.freeMemory);
+      freeMemory.setText(memoryString(Runtime.getRuntime().freeMemory()));
+
+      TextView totalMemory = findViewById(R.id.totalMemory);
+      totalMemory.setText(memoryString(Runtime.getRuntime().totalMemory()));
+
+      TextView maxMemory = findViewById(R.id.maxMemory);
+      maxMemory.setText(memoryString(Runtime.getRuntime().maxMemory()));
+
+      TextView nativeHeap = findViewById(R.id.nativeHeap);
+      nativeHeap.setText(memoryString(Debug.getNativeHeapSize()));
+
+      TextView nativeAllocated = findViewById(R.id.nativeAllocated);
+      nativeAllocated.setText(memoryString(Debug.getNativeHeapAllocatedSize()));
+
+      TextView trimMemoryComplete = findViewById(R.id.trimMemoryComplete);
+      trimMemoryComplete.setText(
+          String.format(Locale.getDefault(), "%d", onTrims.count(TRIM_MEMORY_COMPLETE)));
+
+      TextView trimMemoryModerate = findViewById(R.id.trimMemoryModerate);
+      trimMemoryModerate.setText(
+          String.format(Locale.getDefault(), "%d", onTrims.count(TRIM_MEMORY_MODERATE)));
+
+      TextView trimMemoryBackground = findViewById(R.id.trimMemoryBackground);
+      trimMemoryBackground.setText(
+          String.format(Locale.getDefault(), "%d", onTrims.count(TRIM_MEMORY_BACKGROUND)));
+
+      TextView trimMemoryUiHidden = findViewById(R.id.trimMemoryUiHidden);
+      trimMemoryUiHidden.setText(
+          String.format(Locale.getDefault(), "%d", onTrims.count(TRIM_MEMORY_UI_HIDDEN)));
+
+      TextView trimMemoryRunningCritical = findViewById(R.id.trimMemoryRunningCritical);
+      trimMemoryRunningCritical.setText(
+          String.format(Locale.getDefault(), "%d", onTrims.count(TRIM_MEMORY_RUNNING_CRITICAL)));
+
+      TextView trimMemoryRunningLow = findViewById(R.id.trimMemoryRunningLow);
+      trimMemoryRunningLow.setText(
+          String.format(Locale.getDefault(), "%d", onTrims.count(TRIM_MEMORY_RUNNING_LOW)));
+
+      TextView trimMemoryRunningModerate = findViewById(R.id.trimMemoryRunningModerate);
+      trimMemoryRunningModerate.setText(
+          String.format(Locale.getDefault(), "%d", onTrims.count(TRIM_MEMORY_RUNNING_MODERATE)));
+
+
+    });
+  }
+
+  void jvmConsume(int bytes) {
+    try {
+      byte[] array = new byte[bytes];
+      for (int count = 0; count < array.length; count++) {
+        array[count] = (byte) count;
+      }
+
+      data.add(array);
+    } catch (OutOfMemoryError e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void onTrimMemory(int level) {
+    Log.i(TAG, String.format("onTrimMemory %d", level));
+
+    freeAll();
+    data.clear();
+    System.gc();
+
+    onTrims.add(level);
+
+    updateInfo();
+    super.onTrimMemory(level);
+  }
+
+  public native void freeAll();
   public native void nativeConsume(int bytes);
   public native String stringFromJNI();
 }
