315beb988289da19a83b1fa368c7445e485ba194
Jim Blackler <jimblackler@google.com>
2019-09-11 17:43:31 +0100

    Success of native mallocs tracked.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -15,22 +15,24 @@ Java_net_jimblackler_istresser_MainActivity_nativeConsume(JNIEnv *env, jobject i
   mtx.lock();
   auto byte_count = (size_t) bytes;
   char *data = (char *) malloc(byte_count);
-  allocated.push_back(data);
-  if (!data) {
+
+  if (data) {
+    allocated.push_back(data);
+    for (int count = 0; count < byte_count; count++) {
+      data[count] = (char) count;
+    }
+  } else {
     __android_log_print(ANDROID_LOG_WARN, appname, "Could not allocate");
-    return false;
   }
-  for (int count = 0; count < byte_count; count++) {
-    data[count] = (char) count;
-  }
+
   mtx.unlock();
-  return true;
+  return data != nullptr;
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_net_jimblackler_istresser_MainActivity_freeAll(JNIEnv *env, jobject instance) {
   mtx.lock();
-  __android_log_print(ANDROID_LOG_INFO, appname, "Freeing %lu entries", allocated.size());
+  __android_log_print(ANDROID_LOG_INFO, appname, "Freeing %u entries", allocated.size());
   while(!allocated.empty()) {
     free(allocated.front());
     allocated.pop_front();
