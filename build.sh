d234ae55a4af8a411d3e2df04523b4f55edda097
Will Osborn <willosborn@google.com>
2019-10-15 10:26:47 +0100

    Add samples to build.sh and upgrade gradle plugin
    Test: ./build.sh
    Change-Id: Iab2bd72b5fb1445410cc86d77578a132fa78fbc6

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 export ANDROID_HOME=../prebuilts/sdk
-export ANDROID_NDK=../prebuilts/ndk/r17
+export ANDROID_NDK_HOME=../prebuilts/ndk/r17
 ./gradlew gamesdk gamesdkZip
+pushd samples/bouncyball
+./gradlew build
+popd
+# Commented out because of NDK warning
+#pushd samples/cube
+#./gradlew build
+#popd
+pushd samples/tuningfork/tftestapp
+./gradlew build
+popd
