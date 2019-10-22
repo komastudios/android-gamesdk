e8a36caa40626ecb03021b761321e6bb029e766b
Matt Wang <mattkw@google.com>
2019-10-22 16:17:08 -0700

    Add licensing to build script
    Change-Id: I47368c5c6e5e5f4f912314547f9a5404a3c49e33

--- bender/bender-base/build.sh
+++ bender/bender-base/build.sh
@@ -2,4 +2,5 @@
 set -e # Exit on error
 export ANDROID_HOME=`pwd`/../../../prebuilts/sdk
 export ANDROID_NDK_HOME=`pwd`/../../../prebuilts/ndk/r17
+cp -rf sdk_licenses ../../../prebuilts/sdk/licenses
 ./gradlew build
