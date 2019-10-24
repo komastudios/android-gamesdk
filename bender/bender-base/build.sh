a204dbc503eaa82364686711872f4a5a6a9645d4
Matt Wang <mattkw@google.com>
2019-10-24 20:03:53 +0000

    Revert "Adding CMake directory to Bender build"
    This reverts commit 7f194241d04871bc415fe6764e7d67f34cc27fe2.
    
    Reason for revert: Null Pointer when running app build from Android Studio (env variables not set).
    
    Change-Id: Idbfcf5c397db753be56b331f59137d5babb67eaa

--- bender/bender-base/build.sh
+++ bender/bender-base/build.sh
@@ -2,6 +2,5 @@
 set -e # Exit on error
 export ANDROID_HOME=`pwd`/../../../prebuilts/sdk
 export ANDROID_NDK_HOME=`pwd`/../../../prebuilts/ndk/r17
-export BENDER_CMAKE=`pwd`/../../../prebuilts/cmake/linux-x86
 cp -rf sdk_licenses ../../../prebuilts/sdk/licenses
 ./gradlew build
