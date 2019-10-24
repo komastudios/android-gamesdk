0b743e4484a068433ae457e8050f7764f2020289
Matt Wang <mattkw@google.com>
2019-10-24 16:01:11 -0700

    Adding CMake to PATH for Bender build
    Change-Id: I99587a33851087163382633f491d27ffcd827763

--- bender/bender-base/build.sh
+++ bender/bender-base/build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e # Exit on error
+
+# Provides sdk, ndk, and cmake dirs for android-build machines
+# WARNING: This script should not be run on Mac/Windows
+#   The gradle will rewrite local.properties to locate tools
+
+if [[ "$OSTYPE"!="linux-gnu" ]]; then
+	echo 'This script should only be run on Linux, and is
+	built primarily for use by android-build machines.'
+	exit 1;
+fi
+
+export BENDER_SCRIPT=true # Enables re-writing local.properties
 export ANDROID_HOME=`pwd`/../../../prebuilts/sdk
 export ANDROID_NDK_HOME=`pwd`/../../../prebuilts/ndk/r17
+export BENDER_CMAKE=`pwd`/../../../prebuilts/cmake/linux-x86
 cp -rf sdk_licenses ../../../prebuilts/sdk/licenses
+
 ./gradlew build
