58e477b7c1e3959b65621c11a3f471635bf2bd6c
Matt Wang <mattkw@google.com>
2019-10-24 16:01:11 -0700

    Adding CMake to directory to Bender build (v.2)
    Attempted fix for bender build, v.2 of aosp/1151235.
    Previous CL was modifying local.properties, which
    prevented correct building on Android Studio.
    Another fix was attempted through adding the CMake
    to PATH, but failed for unknown reasons (see patchsets
    1-6).
    
    Test: Forrest run P10213478, run on Android Studio locally.
    
    Change-Id: I99587a33851087163382633f491d27ffcd827763

--- bender/bender-base/build.sh
+++ bender/bender-base/build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e # Exit on error
+
+echo $OSTYPE
+
+if [ "$OSTYPE" != "linux-gnu" ]; then
+	echo 'This script should only be run on Linux, and is
+	built primarily for use by android-build machines.'
+	exit 1;
+fi
+
+
+# Provides sdk, ndk, and cmake dirs for android-build machines
+export BENDER_SCRIPT=true # Gates build.gradle code
 export ANDROID_HOME=`pwd`/../../../prebuilts/sdk
 export ANDROID_NDK_HOME=`pwd`/../../../prebuilts/ndk/r17
+export BENDER_CMAKE=`pwd`/../../../prebuilts/cmake/linux-x86
 cp -rf sdk_licenses ../../../prebuilts/sdk/licenses
+
 ./gradlew build
