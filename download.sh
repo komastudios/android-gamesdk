c3c8a1a3ac8577d8b08684a0007ebabfe1a01b84
Victor Repetskyi <vrepets@google.com>
2019-10-29 15:11:27 +0000

    Add description of partial repository download for different cases, and script to download the prebuilt NDKs and SDKs.
    Test: build Game SDK following instuctions
    Bug: 142732112
    Change-Id: I2c61374e1ee7476242bb930f6a0d93ee54a25995

---
+++ download.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo This script downloads prebuilt Android SDK and NDK from AOSP.
+echo It assumes that the root repo of Game SDK was initialized.
+echo 
+cd ..
+set -e # Exit on error
+#Fetch NDKs availble
+ndks=`grep -oP 'prebuilts/ndk[^\"]+' .repo/manifest.xml | sort -u`
+#Let user choose one
+echo NDKs available:
+oldIFS=$IFS
+IFS=$'\n'
+options=($ndks)
+IFS=$oldIFS
+PS3='Please enter your choice: '
+COLUMNS=12
+select opt in "${options[@]}" "Exit"; 
+do
+  case "$REPLY" in
+    $(( ${#options[@]}+1 )) ) echo "Done."; break;
+  esac
+  echo Downloading $opt ...
+  repo sync -c -j8 $opt > /dev/null 2>&1
+  echo Finished downloading $opt
+done
+#Fetch SDKs availble
+ndks=`grep -oP 'prebuilts/sdk[^\"]+' .repo/manifest.xml | sort -u`
+#Let user choose one
+echo SDKs available:
+oldIFS=$IFS
+IFS=$'\n'
+options=($ndks)
+IFS=$oldIFS
+PS3='Please enter your choice: '
+COLUMNS=12
+select opt in "${options[@]}" "Exit"; do
+  case "$REPLY" in
+    $(( ${#options[@]}+1 )) ) echo "Done."; break;;
+  esac
+  echo Downloading $opt ...
+  repo sync -c -j8 $opt > /dev/null 2>&1
+  echo Finished downloading $opt
+done
