e9ed1b145d9779afda3a85f24cd0e07e7ac5031b
Francesco Carucci <fcarucci@google.com>
2019-10-31 14:27:29 -0700

    Create UniformBuffer and UniformBufferObject classes to store and manipulate UBOs
    Change-Id: I28de7380034a0536cd9cd14dac5638e92b945127

---
+++ bender/bender-base/app/src/main/jni/uniform_buffer.cc
@@ -0,0 +1,35 @@
+//
+// Created by mattkw on 10/25/2019.
+//
+
+#include "uniform_buffer.h"
+
+
+UniformBuffer::UniformBuffer(BenderKit::Device& device, size_t bufferSize) :
+        device_(device) {
+  buffers_.resize(device_.getSwapchainLength());
+  bufferMemory_.resize(device_.getSwapchainLength());
+
+  for (size_t i = 0; i < device_.getSwapchainLength(); i++) {
+    device_.createBuffer(bufferSize, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+                         buffers_[i], bufferMemory_[i]);
+  }
+}
+
+UniformBuffer::~UniformBuffer() {
+  for (int i = 0; i < device_.getSwapchainLength(); ++i) {
+    vkDestroyBuffer(device_.getDevice(), buffers_[i], nullptr);
+    vkFreeMemory(device_.getDevice(), bufferMemory_[i], nullptr);
+  }
+}
+
+void* UniformBuffer::map(int frameIndex, size_t size) {
+  void* data;
+  vkMapMemory(device_.getDevice(), bufferMemory_[frameIndex], 0, size, 0, &data);
+  return data;
+}
+
+void UniformBuffer::unmap(int frameIndex) {
+  vkUnmapMemory(device_.getDevice(), bufferMemory_[frameIndex]);
+}
+
---
+++ bender/bender-base/app/src/main/jni/uniform_buffer.h
@@ -0,0 +1,45 @@
+//
+// Created by mattkw on 10/25/2019.
+//
+
+#pragma once
+
+#ifndef BENDER_BASE_UNIFORM_BUFFER_H
+#define BENDER_BASE_UNIFORM_BUFFER_H
+
+#include "vulkan_wrapper.h"
+#include "bender_kit.h"
+
+#include <vector>
+
+class UniformBuffer {
+public:
+  UniformBuffer(BenderKit::Device& device, size_t bufferSize);
+  ~UniformBuffer();
+
+  void* map(int frameIndex, size_t size);
+  void unmap(int frameIndex);
+
+  VkBuffer getBuffer(int frameIndex) const { return buffers_[frameIndex]; }
+
+private:
+  BenderKit::Device& device_;
+
+  std::vector<VkBuffer> buffers_;
+  std::vector<VkDeviceMemory> bufferMemory_;
+};
+
+template <typename T> class UniformBufferObject : private UniformBuffer {
+public:
+  UniformBufferObject(BenderKit::Device& device) : UniformBuffer(device, sizeof(T)) { }
+
+  void update(int frameIndex, std::function<void(T& data)> updater) {
+    void *data = map(frameIndex, sizeof(T));
+    updater(*reinterpret_cast<T*>(data));
+    unmap(frameIndex);
+  }
+
+  VkBuffer getBuffer(int frameIndex) const { return UniformBuffer::getBuffer(frameIndex); }
+};
+
+#endif //BENDER_BASE_UNIFORM_BUFFER_H
