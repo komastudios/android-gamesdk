a6179b40f2d3eb3e4a2aaca52bf0caceba2d2bda
Andy Yu <chingtangyu@google.com>
2019-10-31 14:05:21 -0700

    Extracting Texture class
    Refactor bender_textures to Texture class under bender
    Create Texture with given asset file path
    
    Test: Build and run on Pixel 3a
    Change-Id: I677ecca42712ca2c5e56102c6ae93463afa1d5a9

--- bender/bender-base/app/src/main/jni/texture.h
+++ bender/bender-base/app/src/main/jni/texture.h
@@ -11,30 +11,32 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#ifndef BENDER_BASE_UTILS_SRC_BENDER_TEXTURES_H_
-#define BENDER_BASE_UTILS_SRC_BENDER_TEXTURES_H_
 
+#ifndef BENDER_BASE_TEXTURE_H
+#define BENDER_BASE_TEXTURE_H
 
 #include "vulkan_wrapper.h"
 #include "bender_kit.h"
 
 #include <android_native_app_glue.h>
 
-typedef struct texture_object {
-    VkSampler sampler;
-    VkImage image;
-    VkImageLayout imageLayout;
-    VkDeviceMemory mem;
-    VkImageView view;
-    int32_t tex_width;
-    int32_t tex_height;
-} texture_object;
+class Texture {
+public:
+    Texture(BenderKit::Device& device, android_app *androidAppCtx,
+            const char *texture_file_name, VkFormat texture_format);
+    ~Texture();
+    VkImageView getImageView();
+private:
+    BenderKit::Device& device_;
+    VkImage image_;
+    VkImageLayout image_layout_;
+    VkDeviceMemory mem_;
+    VkImageView view_;
+    int32_t tex_width_;
+    int32_t tex_height_;
+    VkFormat texture_format_;
+    VkResult loadTextureFromFile(const char* filePath, android_app *android_app_,
+                                 VkImageUsageFlags usage, VkFlags required_props);
+};
 
-static const VkFormat kTexFmt = VK_FORMAT_R8G8B8A8_SRGB;
-#define TEXTURE_COUNT 1
-
-VkResult LoadTextureFromFile(const char* filePath, android_app *androidAppCtx,
-                             struct texture_object* tex_obj, BenderKit::Device *device,
-                             VkImageUsageFlags usage, VkFlags required_props);
-
-#endif //BENDER_BASE_TEXTURE_LOADER_H
+#endif //BENDER_BASE_TEXTURE_H
