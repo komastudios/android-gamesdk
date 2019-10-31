3a20da412b42a052309755725a10eac7cf986605
Matt Wang <mattkw@google.com>
2019-10-31 16:58:49 -0700

    Adding Mesh class
    Adding Mesh class, which defines a mesh (with an underlying
    geometry) that can construct its own pipeline and be called
    upon to submit a draw command. Later on, adding Material
    will have the Material submit descriptor set layout and
    updating descriptor set information.
    
    Test: Build/run in Android Studio
    
    Change-Id: I6a5602d0192bf998b30e334d3046d7bd720cbf60

---
+++ bender/bender-base/app/src/main/jni/mesh.h
@@ -0,0 +1,36 @@
+//
+// Created by mattkw on 10/31/2019.
+//
+
+#ifndef BENDER_BASE_MESH_H
+#define BENDER_BASE_MESH_H
+
+#import <vector>
+
+#import "vulkan_wrapper.h"
+#import "glm/glm.hpp"
+#import "bender_kit.h"
+#import "geometry.h"
+#import "shader_state.h"
+
+class Mesh {
+public:
+    Mesh(BenderKit::Device *device, std::vector<float> vertexData, std::vector<uint16_t> indexData,
+         VkDescriptorSetLayout *descriptorSetLayout, ShaderState *shaderState, VkRenderPass* renderPass);
+    ~Mesh();
+
+    void submitDraw(VkCommandBuffer commandBuffer, VkDescriptorSet descriptorSet) const;
+
+private:
+    BenderKit::Device *device_;
+    Geometry *geometry_;;
+
+    VkPipelineLayout layout_;
+    VkPipelineCache cache_;
+    VkPipeline pipeline_;
+
+    void createMeshPipeline(VkDescriptorSetLayout *descriptorSetLayout, ShaderState *shaderState,
+            VkRenderPass *renderPass);
+};
+
+#endif //BENDER_BASE_MESH_H
