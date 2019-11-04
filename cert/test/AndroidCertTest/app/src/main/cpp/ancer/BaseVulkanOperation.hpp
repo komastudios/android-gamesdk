62dd7dd5043f39e76a2e1b506f958dfbc0e1b0cb
Diego Dagum <dagum@google.com>
2019-11-04 12:59:20 -0800

    Added a C++ BaseVulkanOperation to capture commonalities for upcoming Vulkan operations.
    It includes helpers in ancer/utils.
    
    Test: I tested this revision after completing the next commit.
    
    Change-Id: I6246fd9a08221cd2654fe7e93f86729c57458567

---
+++ cert/test/AndroidCertTest/app/src/main/cpp/ancer/BaseVulkanOperation.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "ancer/util/VkHelpers.hpp"
+
+#include "BaseOperation.hpp"
+
+using namespace ancer;
+
+
+namespace ancer {
+    /**
+     * BaseVulkanOperation
+     * Base class for operations which intend to render Vulkan output
+     */
+    class BaseVulkanOperation : public BaseOperation {
+    public:
+        BaseVulkanOperation(const Log::Tag testTag);
+
+        virtual ~BaseVulkanOperation();
+
+        void Wait() override {}
+
+    protected:
+        /**
+         * Returns a vulkan_info struct with data pertaining to the current operation.
+         */
+        vulkan_info& GetInfo();
+
+        /**
+         * Retrieves a Vulkan device handle, if available.
+         */
+        const VkDevice& GetDevice();
+
+    private:
+        vulkan_info _operation_info;
+        const Log::Tag _test_tag;
+    };
+}
