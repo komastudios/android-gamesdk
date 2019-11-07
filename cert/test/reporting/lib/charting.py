6f246ad02fdd26b78c6d8d31c74cf7b4a9b1f9ca
Shamyl Zakariya <shamyl@google.com>
2019-11-07 11:44:15 -0800

    Implement a python pipeline to build and deploy the test application locally or via firebase test lab. Collects result data, processes, and provides a mechanism for charting.
    - add fix for broken Pipfile
    - remove trailing whitespaces
    - explicitly remove commented-out exit() call
    - add specific pyyaml dep to Pipfile
    - fix for shell command string broken when line wrapping to 80 cols
    
    Test: Local manual testing
    Change-Id: Ib83a5c6be792c6b64e5d41dc28f72aacfeb7b6eb

---
+++ cert/test/reporting/lib/charting.py
@@ -0,0 +1,202 @@
+#
+# Copyright 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import sys
+import csv
+from pathlib import Path
+from typing import Any, Dict, List
+from lib.csv import CSVEntry
+from lib.chart_components import *
+from lib.suite_handlers.loader import create_suite_handler
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.use('gtk3cairo')
+
+# -----------------------------------------------------------------------------
+
+#
+# Configure plot font sizes
+#
+
+SMALL_SIZE = 8
+MEDIUM_SIZE = 10
+BIGGER_SIZE = 12
+
+plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+# -----------------------------------------------------------------------------
+
+
+def label_line(line, label, x, y, color='0.5', size=12):
+    """Add a label to a line, at the proper angle.
+
+    Args:
+        line : matplotlib.lines.Line2D object,
+        label : str
+        x : float
+            x-position to place center of text (in data coordinated
+        y : float
+            y-position to place center of text (in data coordinates)
+        color : str
+        size : float
+    """
+    xdata, ydata = line.get_data()
+    x1 = xdata[0]
+    x2 = xdata[-1]
+    y1 = ydata[0]
+    y2 = ydata[-1]
+
+    ax = line.axes
+    text = ax.annotate(label,
+                       xy=(x, y),
+                       xytext=(-10, 0),
+                       textcoords='offset points',
+                       size=size,
+                       color=color,
+                       horizontalalignment='left',
+                       verticalalignment='bottom')
+
+    sp1 = ax.transData.transform_point((x1, y1))
+    sp2 = ax.transData.transform_point((x2, y2))
+
+    rise = (sp2[1] - sp1[1])
+    run = (sp2[0] - sp1[0])
+
+    slope_degrees = np.degrees(np.arctan2(rise, run))
+    text.set_rotation(slope_degrees)
+    return text
+
+
+# -----------------------------------------------------------------------------
+
+
+class SuiteImpl(Suite):
+
+    def __init__(self, suite_name: str, entries: List[CSVEntry]):
+        super().__init__(suite_name, entries)
+        self.charts: List[Chart] = []
+        self.handler = create_suite_handler(self)
+
+        if not self.handler:
+            print(f"No handler found for suite \"{suite_name}\"")
+        else:
+            self.datums_by_topic = {}
+            for e in entries:
+                d = Datum(e, e.timestamp, e.operation_id, e.token, e.value)
+                topic = (e.operation_id, e.token)
+                if not topic in self.datums_by_topic:
+                    self.datums_by_topic[topic] = []
+                self.datums_by_topic[topic].append(d)
+
+            self.charts = []
+            for topic in self.datums_by_topic:
+                c = Chart(suite_name, topic[0], topic[1],
+                          self.datums_by_topic[topic])
+                if self.handler.assign_renderer(c):
+                    self.charts.append(c)
+
+            self.handler.charts = self.charts
+
+    def topics(self):
+        """Returns the topics repsented by this suite
+        Returns:
+            List of tuple of (operation id, field name)
+        """
+        return list(self.datums_by_topic.keys())
+
+    def analyze(self, cb: Callable[[Any, Datum, str], None]):
+        if self.handler:
+
+            def _cb(s, d, m):
+                print(f"Suite[{s.suite_name}]::analyze msg: {m}")
+                cb(s, d, m)
+
+            return self.handler.analyze(_cb)
+        return None
+
+    def plot(self, title: str, fields: List[str] = [], skip: List[str] = []):
+        plt.figure()
+
+        charts_to_render = []
+        for c in self.charts:
+            include = False
+
+            if fields:
+                for field_name in fields:
+                    if field_name in c.field or field_name in c.operation_id:
+                        include = True
+                        break
+
+            if skip:
+                for field_name in skip:
+                    if field_name in c.field or field_name in c.operation_id:
+                        include = False
+                        break
+
+            if include or not fields:
+                charts_to_render.append(c)
+
+        start_time_seconds = min(
+            map(lambda c: min(c.renderer.get_xs()), self.charts)) / NS_PER_S
+        end_time_seconds = max(
+            map(lambda c: max(c.renderer.get_xs()), self.charts)) / NS_PER_S
+
+        count = len(charts_to_render)
+        for i, chart in enumerate(charts_to_render):
+            fig = plt.subplot(count, 1, i + 1)
+            plt.xlim(0, end_time_seconds - start_time_seconds)
+            chart.plot(fig, i, count, start_time_seconds, end_time_seconds)
+
+            fig.legend(labels=None, loc="upper right")
+
+            # hide the x axis for all but the last plot
+            # since they're all showing time, and it is noisy
+            if i < count - 1:
+                plt.gca().axes.get_xaxis().set_visible(False)
+
+        plt.suptitle(title)
+        plt.xlabel("time (seconds since start of test)")
+        plt.show()
+
+
+def load_suites(csv_file) -> List[Suite]:
+    with open(csv_file, newline='') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        reader = csv.reader(csvfile, dialect)
+
+        entries_by_suite: Dict[str, List[CSVEntry]] = {}
+
+        next(reader)  # skip the header line
+        for row in reader:
+            e = CSVEntry(*row)
+            entries_by_suite.setdefault(e.suite_id, []).append(e)
+
+        suites: List[Suite] = []
+        for suite_name in entries_by_suite:
+            suite = SuiteImpl(suite_name, entries_by_suite[suite_name])
+            suites.append(suite)
+
+        return suites
---
+++ cert/test/reporting/lib/csv.py
@@ -0,0 +1,259 @@
+#
+# Copyright 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import csv
+import json
+import argparse
+from pathlib import Path
+from typing import Dict, List, Any, Tuple
+
+# ----------------------------------------------------------------------
+# helpers
+
+
+def flatten_dict(d: dict) -> List[Tuple[str, Any]]:
+    """Recursively flatten a dict to a list of (pathname,value) tuples
+    Args:
+        d: the dict to flatten
+    Returns:
+        List of tuples where the 0th element is the full path name of the leaf
+            value, and the 1th element is the value
+    Note:
+        doesn't attempt to handle cycles so be careful
+    """
+    result = []
+    for k in d:
+        v = d[k]
+        if type(v) is dict:
+            for r in flatten_dict(v):
+                p = k + "." + r[0]
+                result.append((p, r[1]))
+        else:
+            result.append((k, v))
+
+    return result
+
+
+def try_parse_int(s, base=10, val=None):
+    try:
+        return int(s, base)
+    except ValueError:
+        return val
+
+
+time_units_per_ns = {
+    "nanoseconds": 1,
+    "ns": 1,
+    "milliseconds": 1e-6,
+    "ms": 1e-6,
+    "seconds": 1e-9,
+    "s": 1e-9
+}
+
+
+def timestamp_to_ns(timestamp):
+    if isinstance(timestamp, str):
+        if " " in timestamp:
+            tokens = timestamp.split()
+            num = float(tokens[0]) if "." in tokens[0] else int(tokens[0])
+            unit = tokens[1]
+            if unit in time_units_per_ns:
+                num = num / time_units_per_ns[unit]
+            return num
+        else:
+            return float(timestamp)
+    elif isinstance(timestamp, float) or isinstance(timestamp, int):
+        return timestamp
+    else:
+        raise ValueError(f"timestamp {timestamp} is not a parseable number")
+
+
+# ----------------------------------------------------------------------
+# Models
+
+
+class CSVEntry:
+
+    def __init__(self, timestamp, suite_id, operation_id, thread_id, cpu_id,
+                 token, value: Any):
+        self.timestamp = timestamp_to_ns(timestamp)
+        self.suite_id = suite_id
+        self.operation_id = operation_id
+        self.cpu_id = int(cpu_id)
+        self.thread_id = thread_id
+        self.token = token
+        self.value = value
+
+    def __str__(self):
+        return f"{self.timestamp}, \"{self.suite_id}\", \"{self.operation_id}\", {self.thread_id}, {self.cpu_id}, \"{self.token}\", \"{self.value}\""
+
+    @classmethod
+    def header(cls) -> str:
+        return "timestamp, suite_id, operation_id, thread_id, cpu_id, datum_id, value"
+
+
+class Datum(object):
+
+    def __init__(self, suite_id: str, operation_id: str, thread_id: str,
+                 cpu_id: int, timestamp: int, custom: dict):
+        self.suite_id = suite_id
+        self.operation_id = operation_id
+        self.thread_id = thread_id
+        self.cpu_id = cpu_id
+        self.timestamp = timestamp
+        self.custom = custom
+
+    def to_csvs(self) -> List[CSVEntry]:
+        return list(
+            map(
+                lambda r: CSVEntry(self.timestamp, self.suite_id, self.
+                                   operation_id, self.thread_id, self.cpu_id, r[
+                                       0], r[1]), flatten_dict(self.custom)))
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(**data)
+
+
+class BuildInfo(object):
+
+    def __init__(self, d: Dict[str, str]):
+        self._d = d
+
+    def get(self, feature: str) -> str:
+        return self._d[feature]
+
+    def __getitem__(self, key):
+        return self._d[key]
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(data)
+
+
+# ----------------------------------------------------------------------
+# Helpers
+
+
+def read_csv(csv_file: Path) -> List[CSVEntry]:
+    """Opens our CSV format
+    Args:
+        csv_file: Path to a file in our CSV format
+    Returns:
+        List of CSVEntry
+    """
+    with open(csv_file, newline='') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        reader = csv.reader(csvfile, dialect)
+
+        entries_by_suite: Dict[str, List[CSVEntry]] = {}
+
+        next(reader)  # skip the header line
+
+        csvs = [CSVEntry(*row) for row in reader]
+        return csvs
+
+
+def write_csv(out_csv_file: Path, csvs: List[CSVEntry]):
+    """Saves a CSV file in our format
+    Args:
+        out_csv_file: Path to a file to save to
+        csvs: Array of CSVEntry
+    """
+    csv_data = to_csv(csvs)
+    with open(out_csv_file, "w") as f:
+        f.write(CSVEntry.header() + "\n")
+        f.write(csv_data)
+
+
+def merge_csvs(input: List[List[CSVEntry]]) -> List[CSVEntry]:
+    """Flattens lists of lists of CSVEntry into one sorted list
+    Args:
+        input: List of list of CSVEntry
+    Returns:
+        List of CSVEntry, sorted by timestamp
+    """
+    flat = []
+    for csvs in input:
+        flat.extend(csvs)
+
+    flat = sorted(flat, key=lambda c: c.timestamp)
+    return flat
+
+
+# ----------------------------------------------------------------------
+# Bootstrap
+
+
+def load_report(file: Path) -> (BuildInfo, List[Datum], List[CSVEntry]):
+    """Loads a report file, parses it, and returns a tuple of BuildInfo,
+        List[Datum], and List[CSVEntry]
+    Args:
+        file: Path to a report json file
+    Returns:
+        tuple of BuildInfo, list of Datum, and list of CSVEntry
+    """
+    build: BuildInfo = None
+    data: List[Datum] = []
+    csvs: List[CSVEntry] = []
+
+    with open(file) as f:
+        for i, line in enumerate(f):
+            jd = json.loads(line)
+            # first line is the build info, every other is a report datum
+            if i == 0:
+                build = BuildInfo.from_json(jd)
+            else:
+                datum = Datum.from_json(jd)
+                data.append(datum)
+                csvs.extend(datum.to_csvs())
+
+    return build, data, csvs
+
+
+def to_csv(csvs: List[CSVEntry]) -> str:
+    """builds a string for saving to storage of a list of CSVEntry
+    Args:
+        csvs: list of CSVEntry
+    Returns:
+        a string suitable for saving to a csv file
+    """
+    csvs = sorted(csvs, key=lambda c: c.timestamp)
+
+    csv: str = ""
+    for c in csvs:
+        csv += str(c) + "\n"
+
+    return csv
+
+
+def json_report_to_csv(in_json_file: Path, out_csv_file: Path) -> BuildInfo:
+    """Reads a report JSON file and saves it to a CSV file
+    Args:
+        in_json_file: Path to a json report
+        out_csv_file: Path to where the CSV should be saved
+    Returns:
+        BuildInfo from the report suitable for IDing the device
+    """
+    build_info, data, csvs = load_report(in_json_file)
+    csv_data = to_csv(csvs)
+
+    with open(out_csv_file, "w") as f:
+        f.write(CSVEntry.header() + "\n")
+        f.write(csv_data)
+
+    return build_info
