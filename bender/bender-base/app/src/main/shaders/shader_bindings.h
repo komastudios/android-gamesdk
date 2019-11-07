b0d3166a17f446dea9fc2a788c67fc6b595459b3
fcarucci <fcarucci@google.com>
2019-11-07 14:01:08 -0800

    Make descriptor sets indices explicit
    Change-Id: Id563e33317f2f290dc8074547f9e31be8ec8238e

--- bender/bender-base/app/src/main/shaders/shader_bindings.h
+++ bender/bender-base/app/src/main/shaders/shader_bindings.h
@@ -1,5 +1,20 @@
-#define VERTEX_BINDING_MODEL_VIEW_PROJECTION 0
+// This file is the "source of truth" for sets and bindings indices
+// and it dictates the descriptors layout for meshes
+//
+// Shaders and host code use this defines
 
+//////// MATERIAL BINDINGS
+
+#define BINDING_SET_MATERIAL 0
 #define FRAGMENT_BINDING_SAMPLER 1
+
+//////// LIGHTS BINDINGS
+
+// TODO: change the lights set index once the lights set is extracted
+#define BINDING_SET_LIGHTS BINDING_SET_MATERIAL
 #define FRAGMENT_BINDING_LIGHTS 2
 
+//////// MESH BINDINGS
+
+#define BINDING_SET_MESH  1
+#define VERTEX_BINDING_MODEL_VIEW_PROJECTION 0
