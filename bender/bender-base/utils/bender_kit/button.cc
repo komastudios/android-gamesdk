0fb4bf89a09b479b3b632f491735b6d00db89094
Omar El Sheikh <theoking@google.com>
2019-11-25 15:34:39 -0500

    Implemented Buttons
    Created a customizable button class
    
    Buttons are rectangular and have labels (the label changes to "X" when
    button is being pressed)
    
    Has event handlers for button down, button hold, and button up events
    that can be specified
    
    Simplified the input namespace to provide basic information on pointer
    position and button/pointer state
    
    Added a bunch of event handlers functions for the buttons
    
    Refactored Input Handler into Input Namespace
    
    Made buttons adjust to screen orientation
    
    Updated Font rendering to adjust to screen orientation
    
    Change-Id: I10f704dad9d859eb458cfcfb34b777d80199a603

---
+++ bender/bender-base/utils/bender_kit/button.cc
@@ -0,0 +1,41 @@
+//
+// Created by theoking on 11/25/2019.
+//
+
+#include "button.h"
+
+Button::Button(float x,
+               float x_extent,
+               float y,
+               float y_extent,
+               const char *text) {
+  x_min = x - x_extent / 2;
+  x_max = x + x_extent / 2;
+  x_center = x;
+  y_min = y - y_extent / 2;
+  y_max = y + y_extent / 2;
+  y_center = y;
+  default_label = text;
+  current_label = text;
+  onUp = empty;
+  onDown = empty;
+  onHold = empty;
+}
+
+bool Button::testHit(int x, int y) {
+  int half_width = screen_width_ / 2;
+  int half_height = screen_height_ / 2;
+  return x > (x_min * half_width) + half_width && x < (x_max * half_width) + half_width
+      && y > (y_min * half_height) + half_height
+      && y < (y_max * half_height) + half_height;
+}
+
+void Button::drawButton(VkRenderPass render_pass, Font *font, Renderer *renderer) {
+  font->drawString(current_label,
+                   1.25f,
+                   x_center - (current_label.size() / 2 * FONT_SIZE_RATIO_X),
+                   y_center - (FONT_SIZE_RATIO_Y),
+                   renderer->getCurrentCommandBuffer(),
+                   render_pass,
+                   renderer->getCurrentFrame());
+}
