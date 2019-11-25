eb4bb2622b87ed34c46e53fb525f7b3ad228e7e3
Omar El Sheikh <theoking@google.com>
2019-11-25 15:34:39 -0500

    Implemented Buttons
    Created a customizable button class
    
    Buttons are rectangular and have labels (the label changes to "X" when
    button is being pressed)
    
    Has event handlers for button down, button hold, and button up events
    that can be specified
    
    Simplified the input namespace to provide basic information on pointer
    position and button/pointer state
    
    Added a bunch of event handlers functions for the buttons
    
    Refactored Input Handler into Input Namespace
    
    Change-Id: I10f704dad9d859eb458cfcfb34b777d80199a603

---
+++ bender/bender-base/utils/bender_kit/button.h
@@ -0,0 +1,55 @@
+//
+// Created by theoking on 11/25/2019.
+//
+
+#ifndef BENDER_BASE_UTILS_BENDER_KIT_BUTTON_H_
+#define BENDER_BASE_UTILS_BENDER_KIT_BUTTON_H_
+
+#include <functional>
+#include <string>
+#include "../../app/src/main/jni/font.h"
+
+#define FONT_SIZE_RATIO_X .05f
+#define FONT_SIZE_RATIO_Y .05f
+
+class Button {
+ public:
+  Button(float x_center,
+         float x_extent,
+         float y_center,
+         float y_extent,
+         int screen_width,
+         int screen_height,
+         const char *text);
+
+  bool testHit(int x, int y);
+  void drawButton(VkRenderPass render_pass, Font *font, Renderer *renderer);
+
+  void onButtonDown(){
+    current_label = "X";
+    onDown();
+  }
+
+  void onButtonHold(){
+    onHold();
+  }
+
+  void onButtonUp(){
+    current_label = default_label;
+    onUp();
+  }
+
+  std::function<void()> onDown;
+  std::function<void()> onUp;
+  std::function<void()> onHold;
+
+ private:
+  float x_min, y_min, x_max, y_max, x_center, y_center;
+  int screen_width_, screen_height_;
+  std::string default_label;
+  std::string current_label;
+  static void empty(){ }
+
+};
+
+#endif //BENDER_BASE_UTILS_BENDER_KIT_BUTTON_H_
