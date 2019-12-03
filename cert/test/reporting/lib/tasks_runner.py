a1992ffa987b0a03ca10a2df496b1403cc2b7afa
Shamyl Zakariya <shamyl@google.com>
2019-12-02 16:16:21 -0800

    New report generating pipeline
    - add preflight copy tasks
    - add postflight copy and delete tasks
    - move recipe files to recipes/ subdir and add examples
    - implement *some* new SuiteHandlers for new report API
    - some bugfixes
    
    Test: Manually tested
    
    Change-Id: I71248dd4282ab25f9628e2a2a1a2b51157c566d2

---
+++ cert/test/reporting/lib/tasks_runner.py
@@ -0,0 +1,68 @@
+#
+# Copyright 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from typing import List
+
+from lib.tasks import *
+import lib.tasks_preflight
+import lib.tasks_postflight
+
+#-------------------------------------------------------------------------------
+
+PREFLIGHT_TASKS = {
+    "copy" : lib.tasks_preflight.CopyTask
+}
+
+POSTFLIGHT_TASKS = {
+    "copy": lib.tasks_postflight.CopyTask, 
+    "delete": lib.tasks_postflight.DeleteTask
+}
+
+#-------------------------------------------------------------------------------
+
+def run(tasks: List[Task], device_id: str, env: Environment):
+    """Run a list of preflight tasks with the specified environment
+    Args:
+        tasks: List of tasks to run
+        device_id: The adb device id of a specific device to run tasks on
+        env: Environment object
+    """
+    for task in tasks:
+        task.run(device_id, env)
+
+
+def load(postflight: List[Dict], ctors:Dict) -> List[Task]:
+    """Loads a list of Task to run from a list of task description dictionaries
+    Args:
+        postflight: list loaded from the recipe's deployment.local.postflight list
+        Each entry is a dictionary in form:
+        {
+            "action": name
+            "param0": a param
+            "param1": a param
+        }
+
+        The action maps to a task class, and the remainder fields are handled
+        by the appropriate task subclass
+
+    """
+    tasks: List[Task] = []
+    for pd in postflight:
+        action = pd["action"]
+        if action in ctors:
+            tasks.append(ctors[action](pd))
+
+    return tasks
