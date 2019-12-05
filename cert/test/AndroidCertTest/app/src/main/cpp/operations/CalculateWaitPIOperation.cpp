741069e5fec5a27c99ef8b34cc09ab4e692a2d80
Perry Tyler <tylerperry@google.com>
2019-12-05 15:20:33 -0800

    Work for Wait for Pi
    Test: run wait for PI, and then pull log file to
    examine contents of test.
    
    Change-Id: I864ffe7f85e4a79d77562e7ad2a4a551b8125f38

---
+++ cert/test/AndroidCertTest/app/src/main/cpp/operations/CalculateWaitPIOperation.cpp
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cmath>
+#include <thread>
+#include <mutex>
+#include <sstream>
+
+#include <ancer/BaseOperation.hpp>
+#include <ancer/System.hpp>
+#include <ancer/util/Json.hpp>
+#include <unistd.h>
+
+using namespace ancer;
+
+
+//==================================================================================================
+
+namespace {
+    constexpr Log::Tag TAG{"calculate_pi_native"};
+
+    constexpr auto ITERATION_SAMPLE_PERIOD(int64_t(100));
+
+    constexpr auto PI = 3.14159265358979323846;
+}
+
+//==================================================================================================
+
+namespace {
+    struct configuration {
+        Milliseconds performance_sample_period{Milliseconds::zero()};
+        bool affinity{};
+        int iterationWaitCount;
+        int iterationWaitTime;
+    };
+
+    JSON_READER(configuration) {
+        JSON_REQVAR(performance_sample_period);
+        JSON_REQVAR(affinity);
+        JSON_REQVAR(iterationWaitCount);
+        JSON_REQVAR(iterationWaitTime);
+
+    }
+
+//--------------------------------------------------------------------------------------------------
+
+    struct pi {
+        Duration duration;
+        int64_t iterations;
+        double value;
+        double error;
+    };
+
+    struct datum {
+        pi pi;
+    };
+
+    JSON_WRITER(pi) {
+        JSON_REQVAR(iterations);
+    }
+
+    JSON_WRITER(datum) {
+        JSON_REQVAR(pi);
+    }
+}
+
+//==================================================================================================
+
+/*
+ * CalculatePIOperation
+ * Computes PI, iteratively, to heat up the CPU
+ */
+class CalculateWaitPIOperation : public BaseOperation {
+public:
+
+    CalculateWaitPIOperation() = default;
+
+    void Start() override {
+        BaseOperation::Start();
+
+        auto c = GetConfiguration<configuration>();
+        auto num_cores = std::thread::hardware_concurrency();
+        auto is_test = true;
+
+        for ( auto i = 0 ; i < num_cores ; i++ ) {
+            _threads.emplace_back(
+                    [this, i, is_test, c]() {
+                        if ( c.affinity ) {
+                            SetThreadAffinity(i);
+                        }
+
+                        CalculatePi(c);
+                    });
+        }
+    }
+
+    void Wait() override {
+        for ( std::thread& t : _threads ) {
+            if ( t.joinable()) {
+                t.join();
+            }
+        }
+    }
+
+private:
+
+    /*
+     * Performs an iterative calculation of PI for a time duration; note: the goal is not accuracy,
+     * but just to heat up the CPU.
+     * @param calculationDuration the total test duration time. If 0, run indefinitely
+     * @param reportSamplingDuration if > 0, a sample will be recorded every sampling duration
+     * @return the result (duration, iterations, result pi, etc)
+     */
+    void CalculatePi(configuration config) {
+        ANCER_SCOPED_TRACE("CalculatePIOperation::calculate_pi");
+
+        int frameCountSinceRest = 0;
+
+        auto is_test = GetMode() == Mode::DataGatherer;
+
+        long d = 3;
+        long i = 0;
+        double accumulator = 1;
+
+        pi current_pi{};
+
+        for (auto start_time = SteadyClock::now();; i++, d += 2, ++current_pi.iterations) {
+            if (++frameCountSinceRest == config.iterationWaitCount) {
+                usleep(config.iterationWaitTime); //1E-6
+                frameCountSinceRest = 0;
+            }
+
+            if (IsStopped()) {
+                break;
+            }
+
+            double factor = 1.0 / static_cast<double>((i % 2 == 0) ? -d : d);
+            accumulator += factor;
+
+            if (i % ITERATION_SAMPLE_PERIOD == 0) {
+                auto now = SteadyClock::now();
+                current_pi.duration = now - start_time;
+
+                if ((config.performance_sample_period > Duration::zero()) &&
+                    (current_pi.duration >= config.performance_sample_period)) {
+                    current_pi.value = 4 * accumulator;
+                    current_pi.error = std::abs(current_pi.value - PI);
+                    Report(datum{current_pi});
+
+                    current_pi = pi{Duration::zero(), current_pi.iterations};
+                    start_time = now;
+                }
+            }
+        }
+    }
+
+private:
+    std::vector<std::thread> _threads;
+};
+
+EXPORT_ANCER_OPERATION(CalculateWaitPIOperation)
