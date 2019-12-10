369879b061716c87a3ec8a86125ded35be6ee8aa
Matt Wang <mattkw@google.com>
2019-12-09 16:41:40 -0800

    Refactor user interface code
    Refactor user interface code from vulkan_main to UserInterface
    class. Enables clients to add more buttons or text to the UI.
    Also, move buttons and button logic to UserInterface.
    
    Test: build/run in Android Studio
    Change-Id: I23263d92533f542a5ac695ad03ff3113411bf099

--- bender/bender-base/app/src/main/jni/button.cc
+++ bender/bender-base/app/src/main/jni/button.cc
@@ -4,37 +4,49 @@
 
 #include "button.h"
 
-Button::Button(float x,
+Button::Button() : Button(0.0, 0.0, 0.0, 0.0, "") {}
+
+Button::Button(float x_center,
                float x_extent,
-               float y,
+               float y_center,
                float y_extent,
                const char *text) {
-  x_min = x - x_extent / 2;
-  x_max = x + x_extent / 2;
-  x_center = x;
-  y_min = y - y_extent / 2;
-  y_max = y + y_extent / 2;
-  y_center = y;
-  default_label = text;
-  current_label = text;
-  onUp = empty;
-  onDown = empty;
-  onHold = empty;
+  setPosition(x_center, x_extent, y_center, y_extent);
+  setLabel(text);
+
+  onUp = [] {};
+  onDown = [] {};
+  onHold = [] {};
+  updater = [] (Button&) {};
+}
+
+void Button::setLabel(std::string text) {
+  default_label_ = text;
+}
+
+void Button::setPosition(float x_center, float x_extent, float y_center, float y_extent) {
+  x_center_ = x_center;
+  x_min_ = x_center - x_extent / 2;
+  x_max_ = x_center + x_extent / 2;
+  y_center_ = y_center;
+  y_min_ = y_center - y_extent / 2;
+  y_max_ = y_center + y_extent / 2;
 }
 
 bool Button::testHit(int x, int y) {
   int half_width = screen_width_ / 2;
   int half_height = screen_height_ / 2;
-  return x > (x_min * half_width) + half_width && x < (x_max * half_width) + half_width
-      && y > (y_min * half_height) + half_height
-      && y < (y_max * half_height) + half_height;
+  return x > (x_min_ * half_width) + half_width && x < (x_max_ * half_width) + half_width
+      && y > (y_min_ * half_height) + half_height
+      && y < (y_max_ * half_height) + half_height;
 }
 
 void Button::drawButton(VkRenderPass render_pass, Font *font, Renderer *renderer) {
-  font->drawString(current_label,
+  std::string label = pressed_ ? "X" : default_label_;
+  font->drawString(label,
                    1.25f,
-                   x_center - (current_label.size() / 2 * FONT_SIZE_RATIO_X),
-                   y_center - (FONT_SIZE_RATIO_Y),
+                   x_center_ - (label.size() / 2 * FONT_SIZE_RATIO_X),
+                   y_center_ - (FONT_SIZE_RATIO_Y),
                    renderer->getCurrentCommandBuffer(),
                    render_pass,
                    renderer->getCurrentFrame());
---
+++ bender/bender-base/app/src/main/jni/userinterface.cc
@@ -0,0 +1,98 @@
+//
+// Created by Matt Wang on 2019-12-09.
+//
+
+#include "userinterface.h"
+
+std::vector<Button> UserInterface::buttons_;
+
+std::map<int, std::function<void(Data *, std::vector<Button> &)>> UserInterface::handlers_ = {
+        {AMOTION_EVENT_ACTION_DOWN, UserInterface::ActionDownHandler},
+        {AMOTION_EVENT_ACTION_UP, UserInterface::ActionUpHandler},
+        {AMOTION_EVENT_ACTION_MOVE, UserInterface::ActionMoveHandler},
+};
+
+UserInterface::UserInterface(Renderer *renderer, Font& font) : renderer_(renderer), font_(font) {}
+
+void UserInterface::RegisterButton(std::function<void(Button&)> updater) {
+    Button button;
+    button.updater = updater;
+    button.update();
+    buttons_.push_back(button);
+}
+
+void UserInterface::RegisterTextField(std::function<void(TextField&)> updater) {
+    TextField field;
+    field.updater = updater;
+    field.update();
+    text_fields_.push_back(field);
+}
+
+void UserInterface::DrawUserInterface(VkRenderPass render_pass) {
+    for (auto& button : buttons_) {
+        button.update();
+        button.drawButton(render_pass, &font_, renderer_);
+    }
+
+    for (auto& text_field : text_fields_) {
+        text_field.update();
+        font_.drawString(text_field.text,
+                         text_field.text_size,
+                         text_field.x_corner,
+                         text_field.y_corner,
+                         renderer_->getCurrentCommandBuffer(),
+                         render_pass,
+                         renderer_->getCurrentFrame());
+    }
+}
+
+int32_t UserInterface::Handler(android_app *app, AInputEvent *event) {
+    if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
+        Input::Data *input_data = (Input::Data *) app->userData;
+        Input::updateInputData(event, input_data);
+
+        if (input_data->lastButton != nullptr
+            && !input_data->lastButton->testHit(input_data->lastX, input_data->lastY)) {
+            input_data->lastButton->onButtonUp();
+            input_data->lastButton = nullptr;
+        }
+
+        int action = AMotionEvent_getAction(event);
+        if (handlers_.find(action) != handlers_.end()) {
+            handlers_[action](input_data, buttons_);
+        }
+        return 1;
+    }
+    return 0;
+}
+
+void UserInterface::ActionDownHandler(Input::Data *input_data, std::vector<Button> &buttons) {
+    for (auto &button : buttons) {
+        if (button.testHit(input_data->lastX, input_data->lastY)) {
+            input_data->lastButton = &button;
+            button.onButtonDown();
+        }
+    }
+}
+
+void UserInterface::ActionMoveHandler(Input::Data *input_data, std::vector<Button> &buttons) {
+    for (auto &button : buttons) {
+        if (button.testHit(input_data->lastX, input_data->lastY)) {
+            if (input_data->lastButton == nullptr) {
+                button.onButtonDown();
+            } else {
+                button.onButtonHold();
+            }
+            input_data->lastButton = &button;
+        }
+    }
+}
+
+void UserInterface::ActionUpHandler(Input::Data *input_data, std::vector<Button> &buttons) {
+    for (auto &button : buttons) {
+        if (button.testHit(input_data->lastX, input_data->lastY)) {
+            button.onButtonUp();
+        }
+    }
+    Input::clearInput(input_data);
+}
