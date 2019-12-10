fbf7b07943857c1ecaf458ac2f762720b99a6435
Tyler Millican <tmillican@google.com>
2019-12-10 09:16:56 -0800

    Corrected big.LITTLE core counting mechanism to account for middle-sized cores, which some architectures apparently have.
    Code will now use the lesser of android_getCpuCount and GameSDK's CPU count. The latter uses cpu/kernel_max, which doesn't necessarily match the actual hardware.
    
    Test: Ran on a Pixel 3 (standard big.LITTLE setup), a Pixel 4 (has 3 different core speeds), and a Sony Xperia (which has the kernel_max issue); all three gave correct info.
    Change-Id: I6f8f1aa5f21af140c5eec8501a14e01a1aa76061

--- cert/test/AndroidCertTest/app/src/main/cpp/ancer/System.hpp
+++ cert/test/AndroidCertTest/app/src/main/cpp/ancer/System.hpp
@@ -130,13 +130,13 @@ namespace ancer {
     void RunSystemGc();
 
 
-    enum class ThreadAffinity { kAnyCore, kBigCore, kLittleCore };
+    enum class ThreadAffinity { kLittleCore, kMiddleCore, kBigCore, kAnyCore };
     // Returns how many cores are in a given affinity category.
-    [[nodiscard]] int NumCores(ThreadAffinity);
+    [[nodiscard]] int NumCores(ThreadAffinity = ThreadAffinity::kAnyCore);
 
     // Sets our affinity to a specific core in the given group.
     // An index of -1 acts as SetThreadAffinity(affinity).
-    void SetThreadAffinity(int index, ThreadAffinity affinity = ThreadAffinity::kAnyCore);
+    void SetThreadAffinity(int index, ThreadAffinity = ThreadAffinity::kAnyCore);
     // Sets our affinity to any/all of the cores in a group.
     void SetThreadAffinity(ThreadAffinity affinity);
 
