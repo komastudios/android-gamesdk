7a93818a1f529b0e63123ec7cbf9a3c107e6a90c
Diego Dagum <dagum@google.com>
2019-12-16 19:06:49 -0800

    Added GLES3 BufferStorageEXT operation
    Test: manual and in Firebase.
    Change-Id: Idc3a98342faeb78e1372ceed1c3efcf573efbd9f

--- cert/test/AndroidCertTest/app/src/main/cpp/ancer/util/GLHelpers.cpp
+++ cert/test/AndroidCertTest/app/src/main/cpp/ancer/util/GLHelpers.cpp
@@ -18,220 +18,234 @@
 #include "Error.hpp"
 #include "Log.hpp"
 
-
 namespace ancer::glh {
-    namespace {
-        constexpr Log::Tag TAG{"GLHelpers"};
-    }
+namespace {
+constexpr Log::Tag TAG{"GLHelpers"};
+}
 
+namespace color {
 
-    namespace color {
+hsv rgb2hsv(rgb in) {
+  hsv out;
+  float min, max, delta;
 
-        hsv rgb2hsv(rgb in) {
-            hsv out;
-            float min, max, delta;
+  min = in.r < in.g ? in.r : in.g;
+  min = min < in.b ? min : in.b;
 
-            min = in.r < in.g ? in.r : in.g;
-            min = min < in.b ? min : in.b;
+  max = in.r > in.g ? in.r : in.g;
+  max = max > in.b ? max : in.b;
 
-            max = in.r > in.g ? in.r : in.g;
-            max = max > in.b ? max : in.b;
+  out.v = max;                                // v
+  delta = max - min;
+  if (delta < 0.00001F) {
+    out.s = 0;
+    out.h = 0; // undefined, maybe nan?
+    return out;
+  }
+  if (max > 0.0) { // NOTE: if Max is == 0, this divide would cause a crash
+    out.s = (delta/max);                  // s
+  } else {
+    // if max is 0, then r = g = b = 0
+    // s = 0, h is undefined
+    out.s = 0.0;
+    out.h = NAN;                            // its now undefined
+    return out;
+  }
+  if (in.r >= max)                           // > is bogus, just keeps compilor happy
+    out.h = (in.g - in.b)/delta;        // between yellow & magenta
+  else if (in.g >= max)
+    out.h = 2.0F + (in.b - in.r)/delta;  // between cyan & yellow
+  else
+    out.h = 4.0F + (in.r - in.g)/delta;  // between magenta & cyan
 
-            out.v = max;                                // v
-            delta = max - min;
-            if ( delta < 0.00001F ) {
-                out.s = 0;
-                out.h = 0; // undefined, maybe nan?
-                return out;
-            }
-            if ( max > 0.0 ) { // NOTE: if Max is == 0, this divide would cause a crash
-                out.s = (delta / max);                  // s
-            } else {
-                // if max is 0, then r = g = b = 0
-                // s = 0, h is undefined
-                out.s = 0.0;
-                out.h = NAN;                            // its now undefined
-                return out;
-            }
-            if ( in.r >= max )                           // > is bogus, just keeps compilor happy
-                out.h = (in.g - in.b) / delta;        // between yellow & magenta
-            else if ( in.g >= max )
-                out.h = 2.0F + (in.b - in.r) / delta;  // between cyan & yellow
-            else
-                out.h = 4.0F + (in.r - in.g) / delta;  // between magenta & cyan
+  out.h *= 60.0F;                              // degrees
 
-            out.h *= 60.0F;                              // degrees
+  if (out.h < 0.0)
+    out.h += 360.0F;
 
-            if ( out.h < 0.0 )
-                out.h += 360.0F;
+  return out;
+}
 
-            return out;
-        }
+rgb hsv2rgb(hsv in) {
+  float hh, p, q, t, ff;
+  long i;
+  rgb out;
 
+  if (in.s <= 0.0) {       // < is bogus, just shuts up warnings
+    out.r = in.v;
+    out.g = in.v;
+    out.b = in.v;
+    return out;
+  }
+  hh = in.h;
+  if (hh >= 360.0F) hh = 0.0;
+  hh /= 60.0F;
+  i = (long) hh;
+  ff = hh - i;
+  p = in.v*(1.0F - in.s);
+  q = in.v*(1.0F - (in.s*ff));
+  t = in.v*(1.0F - (in.s*(1.0F - ff)));
 
-        rgb hsv2rgb(hsv in) {
-            float hh, p, q, t, ff;
-            long i;
-            rgb out;
+  switch (i) {
+    case 0:out.r = in.v;
+      out.g = t;
+      out.b = p;
+      break;
+    case 1:out.r = q;
+      out.g = in.v;
+      out.b = p;
+      break;
+    case 2:out.r = p;
+      out.g = in.v;
+      out.b = t;
+      break;
 
-            if ( in.s <= 0.0 ) {       // < is bogus, just shuts up warnings
-                out.r = in.v;
-                out.g = in.v;
-                out.b = in.v;
-                return out;
-            }
-            hh = in.h;
-            if ( hh >= 360.0F ) hh = 0.0;
-            hh /= 60.0F;
-            i = (long)hh;
-            ff = hh - i;
-            p = in.v * (1.0F - in.s);
-            q = in.v * (1.0F - (in.s * ff));
-            t = in.v * (1.0F - (in.s * (1.0F - ff)));
+    case 3:out.r = p;
+      out.g = q;
+      out.b = in.v;
+      break;
+    case 4:out.r = t;
+      out.g = p;
+      out.b = in.v;
+      break;
+    case 5:
+    default:out.r = in.v;
+      out.g = p;
+      out.b = q;
+      break;
+  }
+  return out;
+}
+} // namespace color
 
-            switch ( i ) {
-            case 0:out.r = in.v;
-                out.g = t;
-                out.b = p;
-                break;
-            case 1:out.r = q;
-                out.g = in.v;
-                out.b = p;
-                break;
-            case 2:out.r = p;
-                out.g = in.v;
-                out.b = t;
-                break;
-
-            case 3:out.r = p;
-                out.g = q;
-                out.b = in.v;
-                break;
-            case 4:out.r = t;
-                out.g = p;
-                out.b = in.v;
-                break;
-            case 5:
-            default:out.r = in.v;
-                out.g = p;
-                out.b = q;
-                break;
-            }
-            return out;
-        }
-    } // namespace color
-
-    bool CheckGlError(const char* func_name) {
+bool CheckGlError(const char *func_name) {
 #ifndef NDEBUG
-        GLint err = glGetError();
-        if ( err != GL_NO_ERROR ) {
-            // TODO: Need to review if this should be fatal or not.
-            Log::E(TAG, "GL error after %s(): 0x%08x\n", func_name, err);
-            return true;
-        }
-        return false;
+  GLint err = glGetError();
+  if (err!=GL_NO_ERROR) {
+    // TODO: Need to review if this should be fatal or not.
+    Log::E(TAG, "GL error after %s(): 0x%08x\n", func_name, err);
+    return true;
+  }
+  return false;
 #endif
-        return false;
+  return false;
+}
+
+GLuint CreateProgramSrc(const char *vtx_src, const char *frag_src) {
+  GLuint vtx_shader = 0;
+  GLuint frag_shader = 0;
+  GLuint program = 0;
+  GLint linked = GL_FALSE;
+
+  vtx_shader = CreateShader(GL_VERTEX_SHADER, vtx_src);
+  if (!vtx_shader)
+    goto exit;
+
+  frag_shader = CreateShader(GL_FRAGMENT_SHADER, frag_src);
+  if (!frag_shader)
+    goto exit;
+
+  program = glCreateProgram();
+  if (!program) {
+    CheckGlError("glCreateProgram");
+    goto exit;
+  }
+  glAttachShader(program, vtx_shader);
+  glAttachShader(program, frag_shader);
+
+  glLinkProgram(program);
+  glGetProgramiv(program, GL_LINK_STATUS, &linked);
+  if (!linked) {
+    GLint info_log_len = 0;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &info_log_len);
+    if (info_log_len) {
+      auto *info_log = (GLchar *) malloc(static_cast<size_t>(info_log_len));
+      if (info_log) {
+        glGetProgramInfoLog(program, info_log_len, nullptr, info_log);
+        FatalError(TAG, "Could not link program:\n%s\n", info_log);
+        free(info_log);
+      }
     }
+    glDeleteProgram(program);
+    program = 0;
+    FatalError(TAG, "Could not link program");
+  }
 
-    GLuint CreateProgramSrc(const char* vtx_src, const char* frag_src) {
-        GLuint vtx_shader = 0;
-        GLuint frag_shader = 0;
-        GLuint program = 0;
-        GLint linked = GL_FALSE;
+  exit:
+  glDeleteShader(vtx_shader);
+  glDeleteShader(frag_shader);
+  return program;
+}
 
-        vtx_shader = CreateShader(GL_VERTEX_SHADER, vtx_src);
-        if ( !vtx_shader )
-            goto exit;
+GLuint CreateShader(GLenum shader_type, const char *src) {
+  GLuint shader = glCreateShader(shader_type);
+  if (!shader) {
+    CheckGlError("glCreateShader");
+    return 0;
+  }
+  glShaderSource(shader, 1, &src, nullptr);
 
-        frag_shader = CreateShader(GL_FRAGMENT_SHADER, frag_src);
-        if ( !frag_shader )
-            goto exit;
-
-        program = glCreateProgram();
-        if ( !program ) {
-            CheckGlError("glCreateProgram");
-            goto exit;
-        }
-        glAttachShader(program, vtx_shader);
-        glAttachShader(program, frag_shader);
-
-        glLinkProgram(program);
-        glGetProgramiv(program, GL_LINK_STATUS, &linked);
-        if ( !linked ) {
-            GLint info_log_len = 0;
-            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &info_log_len);
-            if ( info_log_len ) {
-                auto* info_log = (GLchar*)malloc(static_cast<size_t>(info_log_len));
-                if ( info_log ) {
-                    glGetProgramInfoLog(program, info_log_len, nullptr, info_log);
-                    FatalError(TAG, "Could not link program:\n%s\n", info_log);
-                    free(info_log);
-                }
-            }
-            glDeleteProgram(program);
-            program = 0;
-            FatalError(TAG, "Could not link program");
-        }
-
-        exit:
-        glDeleteShader(vtx_shader);
-        glDeleteShader(frag_shader);
-        return program;
+  GLint compiled = GL_FALSE;
+  glCompileShader(shader);
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+  if (!compiled) {
+    GLint info_log_len = 0;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_log_len);
+    if (info_log_len > 0) {
+      auto *info_log = (GLchar *) malloc(static_cast<size_t>(info_log_len));
+      if (info_log) {
+        glGetShaderInfoLog(shader, info_log_len, nullptr, info_log);
+        // TODO: Need to review if this should be fatal or not.
+        Log::E(
+            TAG, "Could not compile %s shader:\n%s\n",
+            shader_type==GL_VERTEX_SHADER ? "vertex" : "fragment",
+            info_log);
+        free(info_log);
+      }
     }
+    glDeleteShader(shader);
+    return 0;
+  }
 
-    GLuint CreateShader(GLenum shader_type, const char* src) {
-        GLuint shader = glCreateShader(shader_type);
-        if ( !shader ) {
-            CheckGlError("glCreateShader");
-            return 0;
-        }
-        glShaderSource(shader, 1, &src, nullptr);
+  return shader;
+}
 
-        GLint compiled = GL_FALSE;
-        glCompileShader(shader);
-        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-        if ( !compiled ) {
-            GLint info_log_len = 0;
-            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_log_len);
-            if ( info_log_len > 0 ) {
-                auto* info_log = (GLchar*)malloc(static_cast<size_t>(info_log_len));
-                if ( info_log ) {
-                    glGetShaderInfoLog(shader, info_log_len, nullptr, info_log);
-                    // TODO: Need to review if this should be fatal or not.
-                    Log::E(
-                            TAG, "Could not compile %s shader:\n%s\n",
-                            shader_type == GL_VERTEX_SHADER ? "vertex" : "fragment",
-                            info_log);
-                    free(info_log);
-                }
-            }
-            glDeleteShader(shader);
-            return 0;
-        }
+glm::mat4 Ortho2d(float left, float top, float right, float bottom) {
+  // this was cribbed from elsewhere because glm's ortho function proved hard to use
+  const float z_near = -1.0F;
+  const float z_far = 1.0F;
+  const float inv_z = 1.0F/(z_far - z_near);
+  const float inv_y = 1.0F/(-top + bottom);
+  const float inv_x = 1.0F/(right - left);
 
-        return shader;
+  glm::mat4 result;
+  result[0] = glm::vec4(2*inv_x, 0, 0, 0);
+  result[1] = glm::vec4(0, 2*inv_y, 0, 0);
+  result[2] = glm::vec4(0, 0, -2*inv_z, 0);
+  result[3] = glm::vec4(
+      -(right + left)*inv_x,
+      (top + bottom)*inv_y,
+      -(z_far + z_near)*inv_z,
+      1
+  );
+
+  return result*glm::scale(glm::mat4(1), glm::vec3(1, -1, 1));
+}
+
+bool IsExtensionSupported(const char *gl_extension) {
+  auto supported{false};
+  auto index{-1};
+
+  int number_of_extensions;
+  glGetIntegerv(GL_NUM_EXTENSIONS, &number_of_extensions);
+
+  while (not supported && ++index < number_of_extensions) {
+    const auto *ccc = glGetStringi(GL_EXTENSIONS, index);
+    if (strcmp((const char *)ccc, gl_extension)==0) {
+      supported = true; // The extension is supported by our hardware and driver
     }
+  }
 
-    glm::mat4 Ortho2d(float left, float top, float right, float bottom) {
-        // this was cribbed from elsewhere because glm's ortho function proved hard to use
-        const float z_near = -1.0F;
-        const float z_far = 1.0F;
-        const float inv_z = 1.0F / (z_far - z_near);
-        const float inv_y = 1.0F / (-top + bottom);
-        const float inv_x = 1.0F / (right - left);
-
-        glm::mat4 result;
-        result[0] = glm::vec4(2 * inv_x, 0, 0, 0);
-        result[1] = glm::vec4(0, 2 * inv_y, 0, 0);
-        result[2] = glm::vec4(0, 0, -2 * inv_z, 0);
-        result[3] = glm::vec4(
-                -(right + left) * inv_x,
-                (top + bottom) * inv_y,
-                -(z_far + z_near) * inv_z,
-                1
-        );
-
-        return result * glm::scale(glm::mat4(1), glm::vec3(1, -1, 1));
-    }
+  return supported;
+}
 }  // namespace ancer::glh
