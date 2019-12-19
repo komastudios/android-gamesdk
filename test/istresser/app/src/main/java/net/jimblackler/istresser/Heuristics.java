c50aba5b8f1589fd9785e7cdf51086482844fe43
Jim Blackler <jimblackler@google.com>
2019-12-19 17:05:10 +0000

    New values registered for potential new use in Heuristics.
    Test: Manually
    Change-Id: If70519f0f171e44a074bd7d454cde8cae8584f46

--- test/istresser/app/src/main/java/net/jimblackler/istresser/Heuristics.java
+++ test/istresser/app/src/main/java/net/jimblackler/istresser/Heuristics.java
@@ -1,45 +1,69 @@
 package net.jimblackler.istresser;
 
-import android.app.Activity;
 import android.app.ActivityManager;
-import android.content.Context;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.os.Debug;
+import android.os.Debug.MemoryInfo;
+import android.util.Log;
+import com.google.common.primitives.Ints;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Heuristics {
-  private static List<Integer> getPids(Activity activity) {
-    ActivityManager activityManager =
-        (ActivityManager)
-            Objects.requireNonNull(activity.getSystemService((Context.ACTIVITY_SERVICE)));
-    List<Integer> pids = new ArrayList<>();
-    List<ActivityManager.RunningAppProcessInfo> runningAppProcesses =
-        activityManager.getRunningAppProcesses();
-    for (ActivityManager.RunningAppProcessInfo runningAppProcessInfo : runningAppProcesses) {
+
+  private static final String TAG = Heuristics.class.getSimpleName();
+
+  /**
+   * Return a collection of the process identifiers associated with the running app.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The collection of PIDs.
+   */
+  private static Collection<Integer> getPids(ActivityManager activityManager) {
+    Collection<Integer> pids = new ArrayList<>();
+    for (RunningAppProcessInfo runningAppProcessInfo : activityManager.getRunningAppProcesses()) {
       pids.add(runningAppProcessInfo.pid);
     }
     return pids;
   }
 
-  static int getOomScore(Activity activity) {
+  /**
+   * Returns the OOM score associated with the main application process. As multiple processes can
+   * be associated with an app the largest OOM score of all process is selected.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The OOM score.
+   */
+  static int getOomScore(ActivityManager activityManager) {
     try {
-      List<Integer> pids = getPids(activity);
-      return Integer.parseInt(readFile(("/proc/" + pids.get(0)) + "/oom_score"));
+      int largestOom = 0;
+      for (int pid : getPids(activityManager)) {
+        int oom = Integer.parseInt(readFile(("/proc/" + pid) + "/oom_score"));
+        largestOom = Math.max(largestOom, oom);
+      }
+      return largestOom;
     } catch (IOException | NumberFormatException e) {
       return 0;
     }
   }
 
+  /**
+   * Loads all the text from an input string and returns the result as a string.
+   *
+   * @param inputStream The stream to read.
+   * @return All of the text from the stream.
+   * @throws IOException Thrown if a read error occurs.
+   */
   private static String readStream(InputStream inputStream) throws IOException {
     try (InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader reader = new BufferedReader(inputStreamReader)) {
@@ -56,43 +80,119 @@ public class Heuristics {
     }
   }
 
+  /**
+   * Loads all text from the specified file and returns the result as a string.
+   *
+   * @param filename The name of the file to read.
+   * @return All of the text from the file.
+   * @throws IOException Thrown if a read error occurs.
+   */
   private static String readFile(String filename) throws IOException {
     return readStream(new FileInputStream(filename));
   }
 
-  static ActivityManager.MemoryInfo getMemoryInfo(Activity activity) {
-    ActivityManager activityManager =
-        (ActivityManager)
-            Objects.requireNonNull(activity.getSystemService((Context.ACTIVITY_SERVICE)));
+  /**
+   * Returns the Activity Manager's MemoryInfo object.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The MemoryInfo object.
+   */
+  static ActivityManager.MemoryInfo getMemoryInfo(ActivityManager activityManager) {
     ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
     activityManager.getMemoryInfo(memoryInfo);
     return memoryInfo;
   }
 
+  /**
+   * Returns a dictionary of values extracted from the /proc/meminfo file.
+   *
+   * @return A dictionary of values.
+   */
   static Map<String, Long> processMeminfo() {
     Map<String, Long> output = new HashMap<>();
 
+    String filename = "/proc/meminfo";
     try {
-      String meminfoText = readFile("/proc/meminfo");
+      String meminfoText = readFile(filename);
       Pattern pattern = Pattern.compile("([^:]+)[^\\d]*(\\d+).*\n");
       Matcher matcher = pattern.matcher(meminfoText);
       while (matcher.find()) {
         output.put(matcher.group(1), Long.parseLong(Objects.requireNonNull(matcher.group(2))));
       }
     } catch (IOException e) {
-      // Intentionally silenced
+      Log.w(TAG, "Failed to read " + filename);
     }
     return output;
   }
 
-  static boolean lowMemoryCheck(Activity activity) {
-    return getMemoryInfo(activity).lowMemory;
+  /**
+   * Return a dictionary of values extracted from the application processes' /proc/../status files.
+   * Where multiple processes are found, for each case the highest value is selected.
+   *
+   * @param activityManager The ActivityManager.
+   * @return A dictionary of values.
+   */
+  static Map<String, Long> processStatus(ActivityManager activityManager) {
+    Map<String, Long> output = new HashMap<>();
+    //
+    {
+      for (int pid : getPids(activityManager)) {
+        String filename = "/proc/" + pid + "/status";
+        try {
+          String meminfoText = readFile(filename);
+          Pattern pattern = Pattern.compile("([a-zA-Z]+)[^\\d]*(\\d+) kB.*\n");
+          Matcher matcher = pattern.matcher(meminfoText);
+          while (matcher.find()) {
+            String key = matcher.group(1);
+            long value = Long.parseLong(Objects.requireNonNull(matcher.group(2)));
+            Long existingValue = output.get(key);
+            if (existingValue == null || existingValue < value) {
+              output.put(key, value);
+            }
+          }
+        } catch (IOException e) {
+          Log.w(TAG, "Failed to read " + filename);
+        }
+      }
+    }
+    return output;
   }
 
-  static boolean oomCheck(Activity activity) {
-    return getOomScore(activity) > 650;
+  /**
+   * Returns 'true' if the memory info 'low memory' value is currently true.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The low memory status.
+   */
+  static boolean lowMemoryCheck(ActivityManager activityManager) {
+    return getMemoryInfo(activityManager).lowMemory;
   }
 
+  /**
+   * Returns 'true' if the OOM score for the activity is above a level that requires memory to be
+   * freed to prevent the application from being killed.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The status value.
+   */
+  static boolean oomCheck(ActivityManager activityManager) {
+    return getOomScore(activityManager) > 650;
+  }
+
+  /**
+   * Returns the process memory info for the application.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The process memory info.
+   */
+  static MemoryInfo[] getDebugMemoryInfo(ActivityManager activityManager) {
+    return activityManager.getProcessMemoryInfo(Ints.toArray(getPids(activityManager)));
+  }
+
+  /**
+   * Returns 'true' if the commit limit is above a level that requires memory to be freed to prevent
+   * the application from being killed.
+   */
   static boolean commitLimitCheck() {
     Long value = processMeminfo().get("CommitLimit");
     if (value == null) {
@@ -101,26 +201,47 @@ public class Heuristics {
     return Debug.getNativeHeapAllocatedSize() / 1024 > value;
   }
 
-  static boolean availMemCheck(Activity activity) {
-    ActivityManager.MemoryInfo memoryInfo = getMemoryInfo(activity);
+  /**
+   * Returns 'true' if the availMem value is below a level that requires memory to be freed to
+   * prevent the application from being killed.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The status value.
+   */
+  static boolean availMemCheck(ActivityManager activityManager) {
+    ActivityManager.MemoryInfo memoryInfo = getMemoryInfo(activityManager);
     return memoryInfo.availMem < memoryInfo.threshold * 2;
   }
 
-  static boolean cachedCheck(Activity activity) {
+  /**
+   * Returns 'true' if the cached value is at a level that requires memory to be freed to prevent
+   * the application from being killed.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The status value.
+   */
+  static boolean cachedCheck(ActivityManager activityManager) {
     Long value = processMeminfo().get("Cached");
     if (value == null || value == 0) {
       return false;
     }
-    ActivityManager.MemoryInfo memoryInfo = getMemoryInfo(activity);
+    ActivityManager.MemoryInfo memoryInfo = getMemoryInfo(activityManager);
     return value < memoryInfo.threshold / 1024;
   }
 
-  static boolean memAvailableCheck(Activity activity) {
+  /**
+   * Returns 'true' if the MemAvailable value is at a level that requires memory to be freed to
+   * prevent the application from being killed.
+   *
+   * @param activityManager The ActivityManager.
+   * @return The status value.
+   */
+  static boolean memAvailableCheck(ActivityManager activityManager) {
     Long value = processMeminfo().get("MemAvailable");
     if (value == null || value == 0) {
       return false;
     }
-    ActivityManager.MemoryInfo memoryInfo = getMemoryInfo(activity);
+    ActivityManager.MemoryInfo memoryInfo = getMemoryInfo(activityManager);
     return value < memoryInfo.threshold * 2 / 1024;
   }
 }
