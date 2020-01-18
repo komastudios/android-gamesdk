e0be26f2a4415fae0a0ccff07337058969dcc5b0
Shamyl Zakariya <shamyl@google.com>
2020-01-17 16:17:48 -0800

    Refinements to the marching cubes test
    - to run on more devices I've made SetThreadAffinity fail gracefully instead of calling FatalError
    - adds finer grained sleep timings
    - adds thread setup selection to configuration
    - adds a optimizedNative build type to gradle
    - adds support for selecting the build task to build and deployment scripts
    
    Test: Run locally and on FTL
    Change-Id: Icba994a2e885108ad3f8093c4cfcfa3c6bec0d24

--- cert/test/reporting/lib/graphers/marching_cubes_suite_handler.py
+++ cert/test/reporting/lib/graphers/marching_cubes_suite_handler.py
@@ -17,13 +17,12 @@
 reports from marching cubes operation.
 """
 
-
 from typing import List, Tuple
 
 import math
 import matplotlib.pyplot as plt
 
-from lib.common import Error, nanoseconds_to_seconds
+from lib.common import Error, nanoseconds_to_seconds, nanoseconds_to_milliseconds
 from lib.report import Datum, Suite
 from lib.graphers.suite_handler import SuiteHandler
 
@@ -33,7 +32,7 @@ SLEEP_DUR = \
 X_LABELS = ["sleep(ms) (floating)", "sleep(ms) (pinned)"]
 
 THREAD_SETUPS = [
-    "One", "OnePerBigCore", "OnePerLittleCore", "OnePerCore"
+    "OneBigCore", "AllBigCores", "OneLittleCore", "AllLittleCores", "AllCores"
 ]
 
 
@@ -44,24 +43,47 @@ def y_label(datum: Datum, thread_setup):
         n_threads = int(
             datum.get_custom_field_numeric(
                 "marching_cubes_permutation_results.num_threads_used"))
-        if thread_setup == "One":
-            desc = "One Core"
-        elif thread_setup == "OnePerBigCore":
+        if thread_setup == "OneBigCore":
+            desc = "One Big Core"
+        elif thread_setup == "AllBigCores":
             desc = f"{n_threads} Big Cores"
-        elif thread_setup == "OnePerLittleCore":
+        elif thread_setup == "OneLittleCore":
+            desc = "1 Little Core"
+        elif thread_setup == "AllLittleCores":
             desc = f"{n_threads} Little Cores"
-        elif thread_setup == "OnePerCore":
+        elif thread_setup == "AllCores":
             desc = f"All {n_threads} Cores"
-    elif thread_setup == "One":
-        desc = "One Core"
-    elif thread_setup == "OnePerBigCore":
-        desc = "Big Cores"
-    elif thread_setup == "OnePerLittleCore":
-        desc = "Little Cores"
-    elif thread_setup == "OnePerCore":
+        return desc
+
+    if thread_setup == "OneBigCore":
+        desc = "One Big Core"
+    elif thread_setup == "AllBigCores":
+        desc = "All Big Cores"
+    elif thread_setup == "OneLittleCore":
+        desc = "1 Little Core"
+    elif thread_setup == "AllLittleCores":
+        desc = "All Little Cores"
+    elif thread_setup == "AllCores":
         desc = "All Cores"
 
     return desc
+
+
+def apply_bar_value_labels(axis, rects):
+    """Attach a text label above each bar in *rects*, displaying its height.
+    Args:
+        axis: pyplot axes
+        rects: The bar rects returned by plt.bar()
+    """
+    for rect in rects:
+        height = rect.get_height()
+        axis.annotate(
+            '{}'.format(int(height)),
+            xy=(rect.get_x() + rect.get_width() / 2, height),
+            xytext=(0, 0),  # 3 points vertical offset
+            textcoords="offset points",
+            ha='center',
+            va='bottom')
 
 
 def voxels_per_second(datum: Datum):
@@ -138,42 +160,63 @@ class MarchingCubesSuiteHandler(SuiteHandler):
         return None
 
     def render_plot(self) -> str:
-        row_names = list(self.data_by_thread_setup_floating.keys())
-        row_names.sort()
-        rows = len(row_names)
+        thread_setups = list(self.data_by_thread_setup_floating.keys())
+
+        # THREAD_SETUPS is already in the desired order, so filter it
+        # to those in our data, while retaining THREAD_SETUPS's order
+        thread_setups = [ts for ts in THREAD_SETUPS if ts in thread_setups]
+        num_rows = len(thread_setups)
 
         for col, data_by_thread_setup in enumerate([
                 self.data_by_thread_setup_floating,
                 self.data_by_thread_setup_pinned
         ]):
-            for row, thread_setup in enumerate(row_names):
+            for row, thread_setup in enumerate(thread_setups):
                 idx = 1 + row * 2 + col
-                plt.subplot(rows, 2, idx)
+                axes = plt.subplot(num_rows, 2, idx)
 
                 data = data_by_thread_setup[thread_setup]
                 if data:
                     y_values_by_sleep_dur = {}
                     for datum in data:
                         vps = voxels_per_second(datum)
-                        sleep_dur = datum.get_custom_field_numeric(SLEEP_DUR)
+                        sleep_dur = nanoseconds_to_milliseconds(
+                            datum.get_custom_field_numeric(SLEEP_DUR))
                         y_values_by_sleep_dur[sleep_dur] = vps
 
                     durs = list(y_values_by_sleep_dur.keys())
                     durs.sort()
 
                     y_values = [y_values_by_sleep_dur[dur] for dur in durs]
-                    min_y = min(y_values)
-                    max_y = max(y_values)
-                    y_range = max(
-                        max_y - min_y,
-                        10)  # a little padding in case we have one data point
-                    min_y -= y_range * 0.25
-                    max_y += y_range * 0.25
+                    bar_width = 1 / len(y_values)
 
-                    plt.gca().set_ylim([min_y, max_y])
-                    plt.bar(durs, y_values)
+                    if len(y_values) > 1:
+                        min_y = min(y_values)
+                        max_y = max(y_values)
+                        y_range = max(
+                            max_y - min_y, 10
+                        )  # a little padding in case we have one data point
+                        min_y -= y_range * 0.25
+                        max_y += y_range * 0.25
+                        plt.gca().set_ylim([min_y, max_y])
 
-                    plt.xticks(durs, [str(int(d)) for d in durs])
+                    else:
+                        # some reports have just one value, and that
+                        # wreaks layout havok with the min_y, max_y range
+                        plt.yticks(y_values)
+
+                    plt.xticks(durs, ["{:.2f}".format(d) for d in durs])
+
+                    bar_colors = [(0.25, 0.75, 0.25) if y >= max(y_values) else
+                                  (0.5, 0.5, 0.5) for y in y_values]
+
+                    rendered_bars = plt.bar(durs,
+                                            y_values,
+                                            color=bar_colors,
+                                            width=bar_width)
+
+                    apply_bar_value_labels(axes, rendered_bars)
+
                 else:
                     plt.yticks([])
                     plt.xticks([])
@@ -183,9 +226,11 @@ class MarchingCubesSuiteHandler(SuiteHandler):
                     datum = data[0] if data else None
                     plt.ylabel(y_label(datum, thread_setup))
 
-                if row == len(row_names) - 1:
+                if row == len(thread_setups) - 1:
                     # only show x labels on last row
                     plt.xlabel(X_LABELS[col])
+
+        plt.subplots_adjust(hspace=0.5)
 
         # no interesting summary
         return None
