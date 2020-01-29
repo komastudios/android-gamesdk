a6224d1e2cbd99559fcff722d969d7b267340cc4
Michael Baxter <baxtermichael@google.com>
2020-01-29 15:20:20 -0800

    Wait for Pi calculation
    Heats up the device to measure performance under load. Can be
    configured to use several different throttling mechanisms.
    
    Test: The operation runs for the specified amount of time and
    generates a report.json file. The number of threads listed in
    that file should match the number of threads on the device.
    
    Change-Id: Ib688c3c8ecca2f12712a89861c2249cd49f5120a

--- cert/test/AndroidCertTest/app/src/main/cpp/operations/CalculateWaitPIOperation.cpp
+++ cert/test/AndroidCertTest/app/src/main/cpp/operations/CalculateWaitPIOperation.cpp
@@ -18,155 +18,264 @@
 #include <thread>
 #include <mutex>
 #include <sstream>
+#include <array>
 
 #include <ancer/BaseOperation.hpp>
+#include <ancer/util/ThreadSyncPoint.hpp>
 #include <ancer/System.hpp>
 #include <ancer/util/Json.hpp>
 #include <unistd.h>
 
 using namespace ancer;
 
+// Purpose: Heats up the device to measure performance under load. Can be
+// configured to use several different throttling mechanisms.
 
-//==================================================================================================
+// Note: The "duration" set in configuration.json must be slightly longer than
+// N times the "interval_between_samples", where N is some whole
+// number, as there may be a delay of several milliseconds before the last sets
+// of data are recorded.
 
-namespace {
-    constexpr Log::Tag TAG{"calculate_pi_native"};
-
-    constexpr auto ITERATION_SAMPLE_PERIOD(int64_t(100));
-
-    constexpr auto PI = 3.14159265358979323846;
-}
-
-//==================================================================================================
+//==============================================================================
 
 namespace {
-    struct configuration {
-        Milliseconds performance_sample_period{Milliseconds::zero()};
-        bool affinity{};
-        int iteration_wait_count_;
-        int iteration_wait_time_;
-    };
+constexpr Log::Tag TAG{"calculate_pi_native"};
 
-    JSON_READER(configuration) {
-        JSON_REQVAR(performance_sample_period);
-        JSON_REQVAR(affinity);
-        JSON_REQVAR(iteration_wait_count_);
-        JSON_REQVAR(iteration_wait_time_);
+constexpr auto ITERATION_SAMPLE_PERIOD(int64_t(100));
 
-    }
+constexpr auto PI = 3.14159265358979323846;
+}  // end anonymous namespace
 
-//--------------------------------------------------------------------------------------------------
+//==============================================================================
 
-    struct pi {
-        Duration duration;
-        int64_t iterations;
-        double value;
-        double error;
-    };
+namespace {
+const std::string wait_mutex_name = "mutex";
+const std::string wait_sleep_name = "sleep";
+const std::string wait_spinlock_name = "spinlock";
 
-    struct datum {
-        pi pi;
-    };
+enum class WaitMethod { Mutex, Sleep, Spinlock };
+}  // end anonymous namespace
 
-    JSON_WRITER(pi) {
-        JSON_REQVAR(iterations);
-    }
+//==============================================================================
 
-    JSON_WRITER(datum) {
-        JSON_REQVAR(pi);
-    }
+namespace {
+struct configuration {
+  Milliseconds interval_between_samples{Milliseconds::zero()};
+  Milliseconds interval_between_wait_periods{Milliseconds::zero()};
+  Milliseconds wait_period{Milliseconds::zero()};
+  Milliseconds heat_time{Milliseconds::zero()};
+
+  bool affinity{};
+  std::string wait_method_name;  // refer to `wait_mutex_name`, etc.
+  WaitMethod wait_method;
+};
+
+JSON_READER(configuration) {
+  JSON_REQVAR(interval_between_samples);
+  JSON_REQVAR(interval_between_wait_periods);
+  JSON_REQVAR(wait_period);
+  JSON_REQVAR(heat_time);
+
+  JSON_REQVAR(affinity);
+  JSON_REQVAR(wait_method_name);
+}
+}  // end anonymous namespace
+
+//==============================================================================
+
+namespace {
+struct pi_datum {
+  double value;
+  uint64_t iterations;
+  uint64_t first_half_iterations;
+  uint64_t last_half_iterations;
+};
+
+JSON_WRITER(pi_datum) {
+  JSON_REQVAR(iterations);
+  JSON_REQVAR(first_half_iterations);
+  JSON_REQVAR(last_half_iterations);
 }
 
-//==================================================================================================
+//------------------------------------------------------------------------------
+
+struct core_sample_datum {
+  int core_id;
+  std::vector<uint64_t> iterations;
+
+  core_sample_datum(int id) { core_id = id; }
+};
+
+JSON_WRITER(core_sample_datum) {
+  JSON_REQVAR(iterations);
+  JSON_REQVAR(core_id);
+}
+
+struct core_performance_samples {
+  std::vector<core_sample_datum> cores;
+};
+
+JSON_WRITER(core_performance_samples) { JSON_REQVAR(cores); }
+}  // end anonymous namespace
+
+//==============================================================================
 
 /*
  * CalculatePIOperation
  * Computes PI, iteratively, to heat up the CPU
  */
 class CalculateWaitPIOperation : public BaseOperation {
-public:
+ public:
+  CalculateWaitPIOperation() = default;
 
-    CalculateWaitPIOperation() = default;
+  ~CalculateWaitPIOperation() { Report(_performance_samples); }
 
-    void Start() override {
-        BaseOperation::Start();
+  void Start() override {
+    BaseOperation::Start();
 
-        auto c = GetConfiguration<configuration>();
-        auto num_cores = std::thread::hardware_concurrency();
-        auto is_test = true;
+    const auto config = ValidateConfig(GetConfiguration<configuration>());
+    Report(config.wait_method_name);
 
-        for ( auto i = 0 ; i < num_cores ; i++ ) {
-            _threads.emplace_back(
-                    [this, i, is_test, c]() {
-                        if ( c.affinity ) {
-                            SetThreadAffinity(i);
-                        }
+    const int num_cores = std::thread::hardware_concurrency();
+    _sync_point.Reset(num_cores);
 
-                        CalculatePi(c);
-                    });
-        }
+    for (int i = 0; i < num_cores; ++i) {
+      _performance_samples.cores.push_back(core_sample_datum(i));
     }
 
-    void Wait() override {
-        for ( std::thread& t : _threads ) {
-            if ( t.joinable()) {
-                t.join();
-            }
+    for (int i = 0; i < num_cores; ++i) {
+      _threads.emplace_back([this, i, config]() {
+        if (config.affinity) {
+          SetThreadAffinity(i);
         }
+        CalculatePi(config, _performance_samples.cores[i], _sync_point);
+      });
+    }
+  }
+
+  void Wait() override {
+    for (std::thread& t : _threads) {
+      if (t.joinable()) {
+        t.join();
+      }
+    }
+  }
+
+ private:
+  /*
+   * Performs an iterative calculation of PI for a time duration; note: the goal
+   * is not accuracy, but just to heat up the CPU.
+   * @param calculationDuration the total test duration time. If 0, run
+   * indefinitely
+   * @param reportSamplingDuration if > 0, a sample will be recorded every
+   * sampling duration
+   * @return the result (duration, iterations, result pi, etc)
+   */
+  void CalculatePi(const configuration& config, core_sample_datum& current_core,
+                   ThreadSyncPoint& sync_point) {
+    ANCER_SCOPED_TRACE("CalculatePIOperation::calculate_pi");
+
+    using namespace std::chrono;
+
+    // Leibniz's infinite series approximation: 1 - 1/3 + 1/5 - 1/7 ... = Pi/4.
+    // Each term is calculated as 1.0 / pi_d, negative if pi_k is a factor of 2.
+    uint64_t pi_k = 0;  // k is 0, 1, 2, ...
+    uint64_t pi_d = 3;  // denominator for the kth term of the approximation
+    double accumulator = 1.0;
+    uint64_t iteration_count = 0;
+
+    pi_datum pi_data{};
+
+    const time_point t0 = SteadyClock::now();
+    time_point next_wait_time = t0 + config.interval_between_wait_periods;
+    time_point next_sample_time = t0 + config.interval_between_samples;
+    const time_point heat_epoch_time = t0 + config.heat_time;
+
+    while (!IsStopped()) {
+      const time_point now = SteadyClock::now();
+
+      // Wait/throttle
+      if (now >= next_wait_time) {
+        switch (config.wait_method) {
+          case WaitMethod::Mutex: {
+            sync_point.Sync(std::this_thread::get_id());
+
+            // Use one thread to sleep them all.
+            if (current_core.core_id == 0) {
+              while ((SteadyClock::now() - now) < config.wait_period)
+                ;
+            }
+
+            sync_point.Sync(std::this_thread::get_id());
+            break;
+          }
+          case WaitMethod::Sleep: {
+            std::this_thread::sleep_for(config.wait_period);
+            break;
+          }
+          case WaitMethod::Spinlock: {
+            while ((SteadyClock::now() - now) < config.wait_period)
+              ;
+            break;
+          }
+        }
+
+        next_wait_time =
+            SteadyClock::now() + config.interval_between_wait_periods;
+        continue;
+      }
+
+      // Update heat value
+      // (This first/last half iteration count is no longer being used, but we
+      // keep it in because it does contribute to slowing down the inner loop.
+      // If we removed it, we wouldn't be able to compare with older results.)
+      if (now < heat_epoch_time) {
+        ++pi_data.first_half_iterations;
+      } else {
+        ++pi_data.last_half_iterations;
+      }
+
+      ++iteration_count;
+
+      // Record performance sample
+      if (now >= next_sample_time) {
+        next_sample_time = SteadyClock::now() + config.interval_between_samples;
+        current_core.iterations.push_back(iteration_count);
+        iteration_count = 0;
+      }
+
+      // Update Pi approximation
+      // Leibniz's infinite series approximation: Pi/4 = 1 - 1/3 + 1/5 - 1/7 ...
+      const double factor =
+          1.0 / static_cast<double>((pi_k % 2 == 0) ? -pi_d : pi_d);
+      accumulator += factor;
+      pi_data.value = accumulator * 4.0;
+
+      ++pi_k;
+      pi_d += 2;
+      ++pi_data.iterations;
     }
 
-private:
+    Report(pi_datum{pi_data});
+  }
 
-    /*
-     * Performs an iterative calculation of PI for a time duration; note: the goal is not accuracy,
-     * but just to heat up the CPU.
-     * @param calculationDuration the total test duration time. If 0, run indefinitely
-     * @param reportSamplingDuration if > 0, a sample will be recorded every sampling duration
-     * @return the result (duration, iterations, result pi, etc)
-     */
-    void CalculatePi(configuration config) {
-        ANCER_SCOPED_TRACE("CalculatePIOperation::calculate_pi");
-
-        int frame_count_since_rest = 0;
-
-        long d = 3;
-        long i = 0;
-        double accumulator = 1;
-
-        pi current_pi{};
-
-        for (auto start_time = SteadyClock::now();; i++, d += 2, ++current_pi.iterations) {
-            if (++frame_count_since_rest == config.iteration_wait_count_) {
-                usleep(config.iteration_wait_time_);
-                frame_count_since_rest = 0;
-            }
-
-            if (IsStopped()) {
-                break;
-            }
-
-            double factor = 1.0 / static_cast<double>((i % 2 == 0) ? -d : d);
-            accumulator += factor;
-
-            if (i % ITERATION_SAMPLE_PERIOD == 0) {
-                auto now = SteadyClock::now();
-                current_pi.duration = now - start_time;
-
-                if ((config.performance_sample_period > Duration::zero()) &&
-                    (current_pi.duration >= config.performance_sample_period)) {
-                    current_pi.value = 4 * accumulator;
-                    current_pi.error = std::abs(current_pi.value - PI);
-                    Report(datum{current_pi});
-
-                    current_pi = pi{Duration::zero(), current_pi.iterations};
-                    start_time = now;
-                }
-            }
-        }
+  configuration ValidateConfig(configuration config) const {
+    if (wait_mutex_name == config.wait_method_name) {
+      config.wait_method = WaitMethod::Mutex;
+    } else if (wait_sleep_name == config.wait_method_name) {
+      config.wait_method = WaitMethod::Sleep;
+    } else if (wait_spinlock_name == config.wait_method_name) {
+      config.wait_method = WaitMethod::Spinlock;
+    } else {
+      FatalError(TAG, "unrecognized value for config.wait_method_name");
     }
+    return config;
+  }
 
-private:
-    std::vector<std::thread> _threads;
+ private:
+  core_performance_samples _performance_samples;
+  std::vector<std::thread> _threads;
+  ThreadSyncPoint _sync_point = {0};
 };
 
 EXPORT_ANCER_OPERATION(CalculateWaitPIOperation)
