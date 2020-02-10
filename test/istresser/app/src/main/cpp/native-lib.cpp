5fc5a0a6805743f2cfcc15db3d2982b6e0e635fe
Henry Daitx <daitx@google.com>
2020-02-10 17:17:21 +0000

    Fill mmapped pages with pseudo-random values
    This apparently defeats some copy-on-write optimization which let us
    mmap infinitely if the pages were not written to.
    
    Test: None
    Change-Id: I9670bd58c1c86c07a05e1735f8d78d3cc062e556

--- test/istresser/app/src/main/cpp/native-lib.cpp
+++ test/istresser/app/src/main/cpp/native-lib.cpp
@@ -8,13 +8,23 @@
 #include <unistd.h>
 
 constexpr auto appname = "istresser";
-
-static const int PRIME_1 = 214013;
-static const int PRIME_2 = 2531011;
+static constexpr uint32_t HASH_PRIME = 214013;
 
 std::list<char *> allocated;
 std::mutex mtx;
-TestRenderer* testRenderer;
+TestRenderer *testRenderer;
+static uint32_t current_hash = 1234567890;
+
+static void hash_fill(void *addr, size_t byte_len) {
+  uint32_t *data = (uint32_t *) addr;
+  size_t word_len = byte_len / 4;
+  for (size_t word_count = 0; word_count < word_len; ++word_count) {
+    current_hash ^= word_count;
+    current_hash *= HASH_PRIME;
+    *data = current_hash;
+    ++data;
+  }
+}
 
 extern "C" JNIEXPORT bool JNICALL
 Java_net_jimblackler_istresser_MainActivity_nativeConsume(JNIEnv *env, jobject instance,
@@ -25,12 +35,7 @@ Java_net_jimblackler_istresser_MainActivity_nativeConsume(JNIEnv *env, jobject i
 
   if (data) {
     allocated.push_back(data);
-    int value = rand();
-    for (int count = 0; count < byte_count; count++) {
-      value *= PRIME_1;
-      value += PRIME_2;
-      data[count] = (char) value;
-    }
+    hash_fill(data, byte_count);
   } else {
     __android_log_print(ANDROID_LOG_WARN, appname, "Could not allocate");
   }
@@ -43,7 +48,7 @@ extern "C" JNIEXPORT void JNICALL
 Java_net_jimblackler_istresser_MainActivity_freeAll(JNIEnv *env, jobject instance) {
   mtx.lock();
   __android_log_print(ANDROID_LOG_INFO, appname, "Freeing %u entries", allocated.size());
-  while(!allocated.empty()) {
+  while (!allocated.empty()) {
     free(allocated.front());
     allocated.pop_front();
   }
@@ -51,20 +56,21 @@ Java_net_jimblackler_istresser_MainActivity_freeAll(JNIEnv *env, jobject instanc
 }
 
 extern "C" JNIEXPORT size_t JNICALL
-Java_net_jimblackler_istresser_MainActivity_mmapConsume(JNIEnv *env, jobject instance, jint bytes) {
-    auto byte_count = (size_t) bytes;
-    // Actual mmapped length will always be a multiple of the page size, so we round up to that in
-    // order to account properly.
-    long page_size_align = sysconf(_SC_PAGE_SIZE) - 1;
-    byte_count = (byte_count + page_size_align) & ~page_size_align;
+Java_net_jimblackler_istresser_MainActivity_mmapAnonConsume(JNIEnv *env, jobject instance, jint bytes) {
+  auto byte_count = (size_t) bytes;
+  // Actual mmapped length will always be a multiple of the page size, so we round up to that in
+  // order to account properly.
+  long page_size_align = sysconf(_SC_PAGE_SIZE) - 1;
+  byte_count = (byte_count + page_size_align) & ~page_size_align;
 
-    auto addr = mmap(NULL, byte_count, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (addr == MAP_FAILED) {
-        __android_log_print(ANDROID_LOG_WARN, appname, "Could not mmap");
-    } else {
-        __android_log_print(ANDROID_LOG_INFO, appname, "mmapped %u bytes at %p", byte_count, addr);
-    }
-    return addr != MAP_FAILED ? byte_count : 0;
+  auto addr = mmap(NULL, byte_count, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (addr == MAP_FAILED) {
+    __android_log_print(ANDROID_LOG_WARN, appname, "Could not mmap");
+  } else {
+    __android_log_print(ANDROID_LOG_INFO, appname, "mmapped %u bytes at %p", byte_count, addr);
+    hash_fill(addr, byte_count);
+  }
+  return addr != MAP_FAILED ? byte_count : 0;
 }
 
 extern "C"
@@ -81,7 +87,7 @@ Java_net_jimblackler_istresser_Heuristic_tryAlloc(JNIEnv *env, jobject thiz, jin
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_net_jimblackler_istresser_MainActivity_initGl(JNIEnv* env,
+Java_net_jimblackler_istresser_MainActivity_initGl(JNIEnv *env,
                                                    jclass thiz,
                                                    jobject assets) {
   testRenderer = new TestRenderer();
@@ -89,12 +95,12 @@ Java_net_jimblackler_istresser_MainActivity_initGl(JNIEnv* env,
 
 extern "C"
 JNIEXPORT int JNICALL
-Java_net_jimblackler_istresser_MainActivity_nativeDraw(JNIEnv* env,
+Java_net_jimblackler_istresser_MainActivity_nativeDraw(JNIEnv *env,
                                                        jclass clazz) {
   return testRenderer->render();
 }
 extern "C"
 JNIEXPORT void JNICALL
-Java_net_jimblackler_istresser_MainActivity_release(JNIEnv* env, jclass clazz) {
+Java_net_jimblackler_istresser_MainActivity_release(JNIEnv *env, jclass clazz) {
   testRenderer->release();
 }
