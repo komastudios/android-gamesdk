d72721c78c586f006c7748f4ee0df8a5c2af5e29
Diego Dagum <dagum@google.com>
2020-02-17 15:15:52 -0800

    [NOTE FOR REVIEWERS: the meat of this CL is SaneCpusetOperation.cpp. You may want to check that one first.]
    F2.1: CPUSET functional test
    
    Verifies whether CPUSET is enabled on the device works as expected. Also
    checks that CPU core numbering assigns the lowest numbers to big cores,
    then mid-cores and finally LITTLE ones.
    
    Test: I tested this on my device and in the farm.
    
    Change-Id: I6e92fbbb574f4409e7c2b9e5af1fe5c2fc3a4760

--- cert/test/reporting/lib/graphers/__init__.py
+++ cert/test/reporting/lib/graphers/__init__.py
@@ -21,21 +21,20 @@ registered with the HANDLERS global.
 
 from .affinity_test_suite_handler import AffinityTestSuiteHandler
 from .buffer_storage_suite_handler import BufferStorageSuiteHandler
+from .cpuset_suite_handler import CpusetSuiteHandler
 from .file_performance_suite_handler import FilePerformanceSuiteHandler
 from .half_float_precision import HalfFloatPrecisionSuiteHandler
 from .memory_allocation_suite_handler import MemoryAllocationSuiteHandler
 from .marching_cubes_suite_handler import MarchingCubesSuiteHandler
 from .mprotect_suite_handler import MProtectSuiteHandler
 from .mediump_vec_norm_suite_handler import MediumPVecNormSuiteHandler
-
 from .memory_access.single_core_comparison import SingleCoreComparisonHandler
 
 HANDLERS = [
-    AffinityTestSuiteHandler, BufferStorageSuiteHandler,
+    AffinityTestSuiteHandler, BufferStorageSuiteHandler, CpusetSuiteHandler,
     FilePerformanceSuiteHandler, HalfFloatPrecisionSuiteHandler,
     MemoryAllocationSuiteHandler, MarchingCubesSuiteHandler,
     MProtectSuiteHandler, MediumPVecNormSuiteHandler,
-
     SingleCoreComparisonHandler
 ]
 """List containing all registered SuiteHandler implementations to
