5ac7a70f1d16ab680c50339f8b80fb8a1b63d6a6
Omar El Sheikh <theoking@google.com>
2020-03-10 12:55:55 -0400

    Vertex Format Compression
    Vertex Data now stored using SNORM/UNORM
    
    Scale factor added to Geometry class to undo the conversion to
    unorm/snorm on position data
    
    Added unorm/snorm to Vertex Format type
    
    Added unorm/snorm conversion function to bender helpers
    
    Implemented QTangents to represent tangent space matrix
    
    Updated OBJLoader with new MeshVertex struct to better organize
    obj-loaded vertex data
    
    Updated Polyhedra to support MeshVertex format
    
    Implemented packed vertex format structs to organize vertex data to be
    sent to the GPU
    
    Fixed bug where triangles with equal texture coordinates on some/all of
    its vertices would return a nan/inf tangent/bitangent vector
    
    Fixed issue with normal and specular maps not being set to correct
    VK_FORMAT, causing many lighting discontinuities
    
    Fixed small issue with the OBJ Loader where we had previously hardcoded
    the mtl file path, now it reads it from the OBJ file
    
    Change-Id: I0cf6fea1f7c12bd114be02ae1d7b3305a3097f8a

--- bender/bender-base/app/src/main/jni/geometry.cc
+++ bender/bender-base/app/src/main/jni/geometry.cc
@@ -5,30 +5,81 @@
 #include "geometry.h"
 #include "polyhedron.h"
 #include <vector>
+#include <packed_types.h>
 
 #include "bender_helpers.h"
 
 using namespace benderhelpers;
 
 Geometry::Geometry(benderkit::Device &device,
-                   const std::vector<float> &vertex_data,
+                   const std::vector<MeshVertex> &vertex_data,
                    const std::vector<uint16_t> &index_data)
     : device_(device) {
-  CreateVertexBuffer(vertex_data, index_data);
+  for (int x = 0; x < vertex_data.size(); x++){
+    glm::vec3 pos = vertex_data[x].pos;
 
-  for (int x = 0; x < vertex_data.size() / 14; x++){
-    float xCoord = vertex_data[x * 14];
-    float yCoord = vertex_data[x * 14 + 1];
-    float zCoord = vertex_data[x * 14 + 2];
-
-    if (xCoord > bounding_box_.max.x) bounding_box_.max.x = xCoord;
-    if (xCoord < bounding_box_.min.x) bounding_box_.min.x = xCoord;
-    if (yCoord > bounding_box_.max.y) bounding_box_.max.y = yCoord;
-    if (yCoord < bounding_box_.min.y) bounding_box_.min.y = yCoord;
-    if (zCoord > bounding_box_.max.z) bounding_box_.max.z = zCoord;
-    if (zCoord < bounding_box_.min.z) bounding_box_.min.z = zCoord;
+    if (pos.x > bounding_box_.max.x) bounding_box_.max.x = pos.x;
+    if (pos.x < bounding_box_.min.x) bounding_box_.min.x = pos.x;
+    if (pos.y > bounding_box_.max.y) bounding_box_.max.y = pos.y;
+    if (pos.y < bounding_box_.min.y) bounding_box_.min.y = pos.y;
+    if (pos.z > bounding_box_.max.z) bounding_box_.max.z = pos.z;
+    if (pos.z < bounding_box_.min.z) bounding_box_.min.z = pos.z;
   }
   bounding_box_.center = (bounding_box_.max + bounding_box_.min) * .5f;
+
+  std::vector<packed_vertex> real_vertex_data(vertex_data.size());
+  float max_x = std::max(std::abs(bounding_box_.min.x), std::abs(bounding_box_.max.x));
+  float max_y = std::max(std::abs(bounding_box_.min.y), std::abs(bounding_box_.max.y));
+  float max_z = std::max(std::abs(bounding_box_.min.z), std::abs(bounding_box_.max.z));
+  scale_factor_ = {max_x, max_y, max_z};
+
+  for (int x = 0; x < vertex_data.size(); x++){
+    real_vertex_data[x].position.Set(vertex_data[x].pos / scale_factor_, 1.0f);
+
+    glm::vec3 normal = vertex_data[x].normal;
+    glm::vec3 avg_tangent = vertex_data[x].tangent;
+    glm::vec3 avg_bitangent = vertex_data[x].bitangent;
+
+    normal = glm::normalize(normal);
+    avg_tangent = glm::normalize(avg_tangent);
+    glm::vec3 tangent = glm::normalize(avg_tangent - (normal * (glm::dot(normal, avg_tangent))));
+    glm::vec3 bitangent = glm::normalize(glm::cross(normal, tangent));
+
+    glm::mat3 tbn = {normal, tangent, bitangent};
+    glm::quat q_tangent(tbn);
+    if (glm::any(glm::isnan(q_tangent))){
+        if (glm::any(glm::isnan(glm::normalize(normal)))){
+            normal = {1.0, 0.0, 0.0};
+        }
+        tangent = glm::normalize(normal + glm::vec3(0.5f, 1.5f, 0.5f));
+        tangent = glm::normalize(tangent - (normal * (glm::dot(normal, tangent))));
+        bitangent = glm::cross(normal, tangent);
+        tbn = {normal, tangent, bitangent};
+        q_tangent = tbn;
+    }
+    q_tangent = glm::normalize(q_tangent);
+
+    if( q_tangent.w < 0.0f )
+      q_tangent = -q_tangent;
+
+    const float bias = 1.0f / SNORM_MAX;
+    const float norm_factor = std::sqrt(1 - bias * bias);
+
+    if (q_tangent.w < bias){
+      q_tangent.w = bias;
+      q_tangent.x *= norm_factor;
+      q_tangent.y *= norm_factor;
+      q_tangent.z *= norm_factor;
+    }
+
+    if (glm::dot(bitangent, avg_bitangent) <= 0.0f)
+      q_tangent = -q_tangent;
+
+    real_vertex_data[x].q_tangent.Set(q_tangent);
+    real_vertex_data[x].tex_coord.Set(vertex_data[x].tex_coord);
+  }
+
+  CreateVertexBuffer(real_vertex_data, index_data);
 }
 
 Geometry::~Geometry() {
@@ -39,7 +90,7 @@ Geometry::~Geometry() {
   vkFreeMemory(device_.GetDevice(), index_buffer_device_memory_, nullptr);
 }
 
-void Geometry::CreateVertexBuffer(const std::vector<float>& vertex_data, const std::vector<uint16_t>& index_data) {
+void Geometry::CreateVertexBuffer(const std::vector<packed_vertex>& vertex_data, const std::vector<uint16_t>& index_data) {
   vertex_count_ = vertex_data.size();
   index_count_ = index_data.size();
 
---
+++ bender/bender-base/utils/src/packed_types.h
@@ -0,0 +1,108 @@
+//
+// Created by theoking on 3/19/2020.
+//
+
+#ifndef BENDER_BASE_PACKED_TYPES_H
+#define BENDER_BASE_PACKED_TYPES_H
+
+#pragma pack(push, 0)
+
+#include <cstdint>
+#include "bender_helpers.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+using namespace benderhelpers;
+
+struct vec4_snorm16 {
+
+    int16_t x, y, z, w;
+
+    const vec4_snorm16 &Set(glm::vec4 in) {
+        x = NormFloatToSnorm16(in.x);
+        y = NormFloatToSnorm16(in.y);
+        z = NormFloatToSnorm16(in.z);
+        w = NormFloatToSnorm16(in.w);
+        return *this;
+    }
+
+    const vec4_snorm16 &Set(glm::quat in) {
+            x = NormFloatToSnorm16(in.x);
+            y = NormFloatToSnorm16(in.y);
+            z = NormFloatToSnorm16(in.z);
+            w = NormFloatToSnorm16(in.w);
+            return *this;
+    }
+
+    const vec4_snorm16 &Set(glm::vec3 in, float in_w) {
+            x = NormFloatToSnorm16(in.x);
+            y = NormFloatToSnorm16(in.y);
+            z = NormFloatToSnorm16(in.z);
+            w = NormFloatToSnorm16(in_w);
+            return *this;
+    }
+};
+
+struct vec3_snorm16 {
+    int16_t x, y, z;
+
+    const vec3_snorm16 &Set(glm::vec3 in) {
+            x = NormFloatToSnorm16(in.x);
+            y = NormFloatToSnorm16(in.y);
+            z = NormFloatToSnorm16(in.z);
+            return *this;
+    }
+};
+
+struct vec2_snorm16 {
+    int16_t x, y;
+
+    const vec2_snorm16 &Set(glm::vec2 in) {
+            x = NormFloatToSnorm16(in.x);
+            y = NormFloatToSnorm16(in.y);
+            return *this;
+    }
+};
+
+struct vec4_unorm16 {
+    int16_t x, y, z, w;
+
+    const vec4_unorm16 &Set(glm::vec4 in) {
+            x = NormFloatToUnorm16(in.x);
+            y = NormFloatToUnorm16(in.y);
+            z = NormFloatToUnorm16(in.z);
+            w = NormFloatToUnorm16(in.w);
+            return *this;
+    }
+};
+
+struct vec3_unorm16 {
+    int16_t x, y, z;
+
+    const vec3_unorm16 &Set(glm::vec3 in) {
+            x = NormFloatToUnorm16(in.x);
+            y = NormFloatToUnorm16(in.y);
+            z = NormFloatToUnorm16(in.z);
+            return *this;
+    }
+};
+
+struct vec2_unorm16 {
+    int16_t x, y;
+
+    const vec2_unorm16 &Set(glm::vec2 in) {
+            x = NormFloatToUnorm16(in.x);
+            y = NormFloatToUnorm16(in.y);
+            return *this;
+    }
+};
+
+#pragma pack(pop)
+
+struct packed_vertex {
+    vec4_snorm16 position;
+    vec4_snorm16 q_tangent;
+    vec2_unorm16 tex_coord;
+};
+
+#endif //BENDER_BASE_PACKED_TYPES_H
