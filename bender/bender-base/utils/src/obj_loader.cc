723134bd0d3490d77fcee2a3f144b3a6b977bc3b
Omar El Sheikh <theoking@google.com>
2020-03-27 13:46:35 -0400

    Update Tangent Space Calculations
    Integrated MikkTSpace tangent space calulator into OBJLoader
    
    Each OBJ subsection is treated as a single Mesh for tangent space
    calculations to occur on
    
    While not recommended, currently we average out the returned tangent and
    bitangent vector per vertex because we do not have a mesh data structure
    which allows for the traversal of a mesh by coincident face
    
    Change-Id: I998fe2189868988390c545582f4d3af29e0f93b9

--- bender/bender-base/utils/src/obj_loader.cc
+++ bender/bender-base/utils/src/obj_loader.cc
@@ -5,6 +5,7 @@
 #include "obj_loader.h"
 #include "glm/glm.hpp"
 #include "glm/gtx/hash.hpp"
+#include <mikktspace.h>
 #include <set>
 
 namespace OBJLoader {
@@ -13,14 +14,10 @@ void AddVertex(glm::vec3 &currVert,
                OBJ &currOBJ,
                std::vector<glm::vec3> &position,
                std::vector<glm::vec3> &normal,
-               std::vector<glm::vec2> &texCoord,
-               glm::vec3 tangent,
-               glm::vec3 bitangent) {
+               std::vector<glm::vec2> &texCoord) {
   if (currOBJ.vert_to_index.find(currVert) != currOBJ.vert_to_index.end()) {
     uint16_t index = currOBJ.vert_to_index[currVert];
     currOBJ.index_buffer.push_back(index);
-    currOBJ.vertex_buffer[index].tangent += tangent;
-    currOBJ.vertex_buffer[index].bitangent += bitangent;
     return;
   }
 
@@ -29,9 +26,52 @@ void AddVertex(glm::vec3 &currVert,
 
   currOBJ.vertex_buffer.emplace_back(position[TrueIndex(currVert.x, position.size())],
                                      normal[TrueIndex(currVert.z, normal.size())],
-                                     tangent,
-                                     bitangent,
+                                     glm::vec3(0),
+                                     glm::vec3(0),
                                      texCoord[TrueIndex(currVert.y, texCoord.size())]);
+}
+
+int GetNumFaces(const SMikkTSpaceContext *context) {
+  OBJ *mData = (OBJ *) context->m_pUserData;
+  return mData->index_buffer.size() / 3;
+}
+
+int GetNumVerticesOfFace(const SMikkTSpaceContext *context, const int face) {
+  return 3;
+}
+
+void GetPosition(const SMikkTSpaceContext *context, float *pos_out, const int face, const int vert) {
+  OBJ *obj_data = (OBJ *) context->m_pUserData;
+  glm::vec3 pos = obj_data->vertex_buffer[obj_data->index_buffer[face * 3 + vert]].pos;
+  pos_out[0] = pos.x;
+  pos_out[1] = pos.y;
+  pos_out[2] = pos.z;
+}
+
+void GetNormal(const SMikkTSpaceContext *context, float *norm_out, const int face, const int vert) {
+  OBJ *obj_data = (OBJ *) context->m_pUserData;
+  glm::vec3 norm = obj_data->vertex_buffer[obj_data->index_buffer[face * 3 + vert]].normal;
+  norm_out[0] = norm.x;
+  norm_out[1] = norm.y;
+  norm_out[2] = norm.z;
+}
+
+void GetTexCoord(const SMikkTSpaceContext *context, float *tex_out, const int face, const int vert) {
+  OBJ *obj_data = (OBJ *) context->m_pUserData;
+  glm::vec2 tex = obj_data->vertex_buffer[obj_data->index_buffer[face * 3 + vert]].tex_coord;
+  tex_out[0] = tex.x;
+  tex_out[1] = tex.y;
+}
+
+void SetTSpace(const SMikkTSpaceContext *context, const float *new_tangent,
+               const float *new_bitangent,
+               const float mag_tangent, const float mag_bitangent,
+               const tbool bIsOrientationPreserving, const int face, const int vert) {
+  OBJ *obj_data = (OBJ *) context->m_pUserData;
+  obj_data->vertex_buffer[obj_data->index_buffer[face * 3 + vert]].tangent +=
+          glm::vec3(new_tangent[0], new_tangent[1], new_tangent[2]) * mag_tangent;
+  obj_data->vertex_buffer[obj_data->index_buffer[face * 3 + vert]].bitangent +=
+          glm::vec3(new_bitangent[0], new_bitangent[1], new_bitangent[2]) * mag_bitangent;
 }
 
 void LoadMTL(AAssetManager *mgr,
@@ -67,6 +107,18 @@ void LoadOBJ(AAssetManager *mgr,
 
   std::stringstream data(std::string(fileContent, AAsset_getLength(file)));
 
+  SMikkTSpaceInterface interface;
+  interface.m_getPosition = GetPosition;
+  interface.m_getNormal = GetNormal;
+  interface.m_getTexCoord = GetTexCoord;
+  interface.m_getNumFaces = GetNumFaces;
+  interface.m_getNumVerticesOfFace = GetNumVerticesOfFace;
+  interface.m_setTSpace = SetTSpace;
+  interface.m_setTSpaceBasic = NULL;
+
+  SMikkTSpaceContext context;
+  context.m_pInterface = &interface;
+
   std::string line;
   while (std::getline(data, line) && !data.eof()) {
     std::stringstream lineStream(line);
@@ -89,11 +141,14 @@ void LoadOBJ(AAssetManager *mgr,
       lineStream >> x >> y >> z;
       normal.push_back({x, y, z});
     } else if (label == "usemtl") {
+      if (modelData.size() > 0)
+        genTangSpaceDefault(&context);
       std::string materialName;
       lineStream >> materialName;
       OBJ curr;
       curr.material_name = materialName;
       modelData.push_back(curr);
+      context.m_pUserData = (void *) &modelData.back();
     } else if (label == "f") {
       glm::vec3 vertex1, vertex2, vertex3;
       char skip;
@@ -101,39 +156,16 @@ void LoadOBJ(AAssetManager *mgr,
       lineStream >> vertex2.x >> skip >> vertex2.y >> skip >> vertex2.z;
       lineStream >> vertex3.x >> skip >> vertex3.y >> skip >> vertex3.z;
 
-      glm::vec3 posVec1 = position[TrueIndex(vertex1.x, position.size())];
-      glm::vec3 posVec2 = position[TrueIndex(vertex2.x, position.size())];
-      glm::vec3 posVec3 = position[TrueIndex(vertex3.x, position.size())];
-
-      glm::vec2 texVec1 = texCoord[TrueIndex(vertex1.y, texCoord.size())];
-      glm::vec2 texVec2 = texCoord[TrueIndex(vertex2.y, texCoord.size())];
-      glm::vec2 texVec3 = texCoord[TrueIndex(vertex3.y, texCoord.size())];
-
-      glm::vec3 edge1 = posVec2 - posVec1;
-      glm::vec3 edge2 = posVec3 - posVec1;
-      glm::vec2 deltaUV1 = texVec2 - texVec1;
-      glm::vec2 deltaUV2 = texVec3 - texVec1;
-
-      float f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
-      glm::vec3 tangent, bitangent;
-      tangent = f * (deltaUV2.y * edge1 - deltaUV1.y * edge2);
-      bitangent = f * (-deltaUV2.x * edge1 + deltaUV1.x * edge2);
-
-      if (glm::any(glm::isnan(tangent)) || glm::any(glm::isinf(tangent))){
-        tangent = glm::vec3(0.0f);
-      }
-      if (glm::any(glm::isnan(bitangent)) || glm::any(glm::isnan(bitangent))){
-        bitangent = glm::vec3(0.0f);
-      }
-
-      AddVertex(vertex3, modelData.back(), position, normal, texCoord, tangent, bitangent);
-      AddVertex(vertex2, modelData.back(), position, normal, texCoord, tangent, bitangent);
-      AddVertex(vertex1, modelData.back(), position, normal, texCoord, tangent, bitangent);
+      AddVertex(vertex3, modelData.back(), position, normal, texCoord);
+      AddVertex(vertex2, modelData.back(), position, normal, texCoord);
+      AddVertex(vertex1, modelData.back(), position, normal, texCoord);
 
       if (modelData.back().vert_to_index.size() > MAX_VERTEX_COUNT){
+        genTangSpaceDefault(&context);
         OBJ curr;
         curr.material_name = modelData.back().material_name;
         modelData.push_back(curr);
+        context.m_pUserData = (void *) &modelData.back();
       }
     }
   }
