a0aa82e7c9bc3fd42cc599f6d33e2ab7b9b40213
Andy Yu <chingtangyu@google.com>
2020-04-22 16:48:28 -0700

    Buffer-less font rendering
    This is the initial version of font rendering without using a vertex buffer
    and index buffer. By utilizing uniform buffer objects and push constants,
    we can render about a hundred characters per draw call depeneding on the
    size of the push constants available on the device. The implementation can
    be seperated into these parts:
    
    - Set up vertex empty input state in the pipeline
    - Set up push constant range in the pipeline
    - Change culling and front face settings for rasterization
    - Push font data to a UBO
    - Add data parsing and assembling logic to the vertex shader
    
    What is further coming:
    - Add empty vertex format to shader_state and vertex_format
    - Support splitting long strings into multiple draw calls
    
    Test: Build and run on Pixel 4 XL via Android Studio
    
    Change-Id: I8fdf2d2c34353afd32e3f0627a1a53410ae48308

---
+++ bender/bender-base/app/src/main/jni/push_consts_app.h
@@ -0,0 +1,8 @@
+//
+// Created by chingtangyu on 4/27/2020.
+//
+
+typedef glm::vec4 vec4;
+typedef uint32_t uint;
+
+#include "push_constants.h"
---
+++ bender/bender-base/app/src/main/shaders/push_constants.h
@@ -0,0 +1,8 @@
+//
+// Created by chingtangyu on 4/27/2020.
+//
+
+struct push_constants {
+    vec4 coordinates;
+    uint string_data[28];
+};
