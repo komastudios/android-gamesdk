639d233b6f14207f86113c99e29f6559d2b363dd
Will Osborn <willosborn@google.com>
2020-05-14 16:35:24 +0100

    Fix bug in Signature generation on emulator
    Test: build and run sample apps
    Change-Id: I4c555190d3b8500f41d43ac534acc239555e498a

--- src/tuningfork/tuningfork_utils.cpp
+++ src/tuningfork/tuningfork_utils.cpp
@@ -35,11 +35,11 @@
 
 namespace tuningfork {
 
-std::string Base16(const std::vector<char>& bytes) {
+std::string Base16(const std::vector<unsigned char>& bytes) {
     static char hex_char[] = "0123456789abcdef";
     std::string s(bytes.size()*2,' ');
     auto q = s.begin();
-    for(char b: bytes) {
+    for(unsigned char b: bytes) {
         *q++ = hex_char[b>>4];
         *q++ = hex_char[b&0xf];
     }
