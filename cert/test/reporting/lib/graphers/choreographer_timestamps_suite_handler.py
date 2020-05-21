d9d0b6d8f5b1b543e633732dffa335a394e081bf
Michael Baxter <baxtermichael@google.com>
2020-05-21 16:19:56 -0700

    Measure Choreographer with EGL extension
    Removes dependence on systrace in favor of using
    the EGL_ANDROID_get_frame_timestamps extensions
    for evaluating the accuracy of Choreographer
    timestamps.
    
    Test: Generates report.json and report document
    
    Change-Id: Iab4c41ad52409260fd95a5a6843f916275ce92fa

--- cert/test/reporting/lib/graphers/choreographer_timestamps_suite_handler.py
+++ cert/test/reporting/lib/graphers/choreographer_timestamps_suite_handler.py
@@ -17,23 +17,34 @@
 A grapher for the Choreographer Timestamps operation.
 """
 
-import os
-import re
 from typing import List, Optional
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
 from lib.graphers.suite_handler import SuiteHandler, SuiteSummarizer
-from lib.report import Datum, SummaryContext, Suite
+from lib.report import Datum, SummaryContext
 import lib.items as items
-import lib.systrace as systrace
+
 
 class ChoreographerTimestampsSummarizer(SuiteSummarizer):
     """Suite summarizer for Choreographer Timestamps test."""
+
     @classmethod
     def default_handler(cls) -> SuiteHandler:
         return ChoreographerTimestampsSuiteHandler
+
+    def render_synthesis(self, ctx):
+        passed = 0
+        failed = 0
+        for handler in self.suite_handlers:
+            if handler.fail:
+                failed += 1
+            else:
+                passed += 1
+
+        msg = f"{passed} passed, {failed} failed."
+        return [items.Text(msg)]
 
 
 class ChoreographerTimestampsSuiteHandler(SuiteHandler):
@@ -44,92 +55,133 @@ class ChoreographerTimestampsSuiteHandler(SuiteHandler):
     def __init__(self, suite):
         super().__init__(suite)
 
+        # Feature availability information
+        self.has_fp_post_frame_callback = False
+        self.has_egl_extension = False
+        self.enable_surface_timestamps_success = False
+        self.has_fp_get_instance = False
+        self.has_fp_get_next_frame_id = False
+        self.has_fp_get_frame_timestamp_supported = False
+
+        self.fail = False
+
+        # Ordered data
+        self.choreographer_timestamps = []
+        self.egl_frame_timestamps = []
+        self.egl_missed_frame_ids = []
+        self.has_corrected_timestamps = False
+        self.cannot_correct_timestamps = False
+
         self.timestamp_variance = []
 
-        systrace_path = self.get_systrace_path(suite)
-        if not os.path.exists(systrace_path):
-            print(f"Looking for {systrace_path} but it doesn't exist.")
-            return
+        choreographer_ts_name = "choreographer_timestamp_ns"
+        egl_frame_ts_name = "egl_frame_timestamp_ns"
 
-        offset_ns = self.read_clock_sync_offset_ns(systrace_path)
-        if not offset_ns:
-            print("We weren't able to get a clock sync!")
-            return
+        # Order data from report file
+        for d in self.data:
+            if "has_extension" in d.custom:
+                self.has_egl_extension = d.get_custom_field("has_extension")
+                self.enable_surface_timestamps_success = d.get_custom_field(
+                    "enable_surface_timestamps_success")
+                self.has_fp_get_instance = d.get_custom_field(
+                    "has_fp_get_instance")
+                self.has_fp_post_frame_callback = d.get_custom_field(
+                    "has_fp_post_frame_callback")
+                self.has_fp_get_next_frame_id = d.get_custom_field(
+                    "has_fp_get_next_frame_id")
+                self.has_fp_get_frame_timestamp_supported = d.get_custom_field(
+                    "has_fp_get_frame_timestamp_supported")
 
-        systrace_timestamps = self.get_systrace_timestamps(systrace_path, \
-            offset_ns)
+            elif choreographer_ts_name in d.custom:
+                # Collect Choreographer timestamps
+                choreographer_ts = d.get_custom_field(choreographer_ts_name)
+
+                # Check if timestamps need to be corrected (as in the case where
+                # they were truncated to 32-bit integers).
+                corrected_ts = correct_timestamp(choreographer_ts, d.timestamp)
+                if corrected_ts is None:
+                    self.cannot_correct_timestamps = True
+                    continue
+
+                if choreographer_ts != corrected_ts:
+                    self.has_corrected_timestamps = True
+                self.choreographer_timestamps.append(corrected_ts)
+
+            elif egl_frame_ts_name in d.custom:
+                # Collect EGL display present timestamps
+                if d.get_custom_field("success"):
+                    self.egl_frame_timestamps.append(
+                        d.get_custom_field(egl_frame_ts_name))
+                else:
+                    self.egl_missed_frame_ids.append(
+                        d.get_custom_field("frame_id"))
 
         # Calculate variance (how much Choreographer timestamps varied from
-        # systrace timestamps)
-        if systrace_timestamps:
-            for row in self.data:
-                timestamp = row.custom.get('timestamp')
-                if timestamp:
-                    index = binary_search_nearest(systrace_timestamps, \
-                        timestamp)
-                    sys_ts = systrace_timestamps[index]
-                    self.timestamp_variance.append(sys_ts - timestamp)
+        # EGL frame timestamps)
+        for timestamp in self.choreographer_timestamps:
+            index = binary_search_nearest(self.egl_frame_timestamps, timestamp)
+            if index:
+                egl_timestamp = self.egl_frame_timestamps[index]
+                variance = egl_timestamp - timestamp
+                self.timestamp_variance.append(variance)
+
 
     @classmethod
     def can_handle_datum(cls, datum: Datum):
         return "ChoreographerTimestampsOperation" in datum.operation_id
 
-    def get_systrace_path(self, suite: Suite) -> str:
-        """
-        Returns the expected path of the systrace file, given the report file
-        path from suite.
-        """
-        report_dir = os.path.dirname(suite.file)
-        report_name = os.path.basename(suite.file)
-        systrace_name = report_name.replace(".json", "_trace.html")
-        systrace_path = os.path.join(report_dir, systrace_name)
-        return systrace_path
-
-
-    def read_clock_sync_offset_ns(self, path: str) -> Optional[int]:
-        """
-        Looks for the clock sync marker in the file at `path` and returns the
-        nanosecond offset that can be added to a systrace timestamp to convert
-        it relative to the report file's timestamps. Returns None if the clock
-        sync marker was not present in the file.
-        """
-        with open(path) as file:
-            for line in file:
-                if systrace.CLOCK_SYNC_MARKER in line:
-                    return systrace.get_ancer_clocksync_offset_ns(line)
-            return None
-
-
-    def get_systrace_timestamps(self, path: str, offset_ns: int) -> [int]:
-        """
-        Returns a list of timestamps for "VSYNC-app" traces in the file at
-        `path`, adding `offset_ns` nanoseconds to each timestamp. (This can be
-        used to make them relative to the report file's timestamps.)
-        """
-        timestamps = []
-        with open(path) as file:
-            vsync_pattern = re.compile(r'\s(\d+\.?\d*):\s.*VSYNC-app')
-            for line in file:
-                match = vsync_pattern.search(line)
-                if match:
-                    timestamp = systrace.get_systrace_timestamp_ns(line)
-                    if timestamp:
-                        timestamps.append(timestamp + offset_ns)
-        return timestamps
 
     def render(self, ctx: SummaryContext) -> List[items.Item]:
+        # Fail early if there is not enough data to run the evaluation
+        if not self.has_fp_post_frame_callback:
+            msg = "Missing function pointer for Choreographer " \
+                "postFrameCallback."
+            return [items.Text(msg)]
+
+        if not self.has_egl_extension \
+            or not self.has_fp_get_instance \
+            or not self.has_fp_get_next_frame_id \
+            or not self.has_fp_get_frame_timestamp_supported:
+            msg = "EGL_ANDROID_get_frame_timestamps extension is unsupported."
+            return [items.Text(msg)]
+
+        if not self.enable_surface_timestamps_success:
+            msg = "Failed to enable surface timestamps."
+            return [items.Text(msg)]
+
+        if not self.choreographer_timestamps or not self.egl_frame_timestamps:
+            msg = f"Missing data:\n"
+            msg += f"- {len(self.choreographer_timestamps)} timestamps from " \
+                "Choreographer\n"
+            msg += f"- {len(self.egl_frame_timestamps)} timestamps from " \
+                "EGL_ANDROID_get_frame_timestamps"
+            return [items.Text(msg)]
+
+        return self.render_report(ctx)
+
+
+    def render_report(self, ctx: SummaryContext):
         image_path = self.plot(ctx, self.render_plot)
         image = items.Image(image_path, self.device())
-        return [image]
+
+        msg = "- Fail\n" if self.fail else "- Pass\n"
+        if self.has_corrected_timestamps:
+            msg += "- Has corrected timestamps\n"
+
+        for missed_frame_id in self.egl_missed_frame_ids:
+            msg += f"- Missed frame ({missed_frame_id})\n"
+
+        return [image, items.Text(msg)]
+
 
     def render_plot(self):
         """
         We create a plot showing how much the Choreographer timestamps varied
-        from the VSYNC values gather from systrace. The graph has an upper and
-        lower bound, such that if a Choreographer timestamp varied from a
-        systrace timestamp by enough to pass the upper or lower bound, we
-        consider the device as failing the test, and the plot will be marked
-        accordingly.
+        from the present values gather from EGL_ANDROID_get_frame_timestamps.
+        The graph has an upper and lower bound, such that if a Choreographer
+        timestamp varied from a present timestamp by enough to pass the upper or
+        lower bound, we consider the device as failing the test, and the plot
+        will be marked accordingly.
         """
 
         _, axes = plt.subplots()
@@ -138,17 +190,17 @@ class ChoreographerTimestampsSuiteHandler(SuiteHandler):
         variance_ms = [i / 1_000_000 for i in self.timestamp_variance]
 
         if not variance_ms:
-            return 'Unable to collect timestamps from systrace'
+            return
 
         # Set bounds
-        max_y = 2.0
+        max_y = 6.0
         min_y = -max_y
         max_x = len(variance_ms)
         axes.axis([0, max_x, min_y, max_y])
         axes.set_xlabel("Frame")
         axes.set_ylabel("Timestamp Variance (ms)")
 
-        upper_bound = 1.0
+        upper_bound = 3.0
         lower_bound = -upper_bound
 
         # Line for upper bound
@@ -187,10 +239,14 @@ class ChoreographerTimestampsSuiteHandler(SuiteHandler):
             add_region_marker('PASS', 0.0, upper_bound - lower_bound, 'green')
 
         if lowest_val < lower_bound:
-            add_region_marker('FAIL', (min_y + lower_bound) / 2, lower_bound - min_y, 'red')
+            self.fail = True
+            add_region_marker('FAIL', (min_y + lower_bound) / 2,
+                              lower_bound - min_y, 'red')
 
         if highest_val > upper_bound:
-            add_region_marker('FAIL', (max_y + upper_bound) / 2, max_y - upper_bound, 'red')
+            self.fail = True
+            add_region_marker('FAIL', (max_y + upper_bound) / 2,
+                              max_y - upper_bound, 'red')
 
 
 def binary_search_nearest(values: List[int], target: int) -> Optional[int]:
@@ -225,3 +281,127 @@ def binary_search_nearest(values: List[int], target: int) -> Optional[int]:
     if abs(target - values[low]) < abs(target - values[high]):
         return low
     return high
+
+
+def make_unsigned_int(value: int, n: int) -> Optional[int]:
+    """Convert an n-bit integer into an unsigned 64-bit integer equivalent,
+    assuming two's complement encoding. (This is useful in recovering part of a
+    64-bit unsigned integer that was truncated into a 32-bit signed integer.)
+
+    Returns:
+        None if the value is not an n-bit integer.
+    """
+    # Only handle n-bit integers.
+    if value > (2**n) - 1 or value < -2**(n - 1):
+        return None
+
+    if value < 0:
+        return 2**n + value
+
+    return value
+
+
+def correct_timestamp(timestamp: int, ref_timestamp: int) -> Optional[int]:
+    """Attempts to undo the truncation of a 64-bit `timestamp` into a 32-bit
+    integer by using an untruncated reference timestamp. If the `timestamp` is
+    negative, two's-complement encoding is assumed and the value is converted
+    to the unsigned form before attempting to handle truncation. If it can be
+    determined that the `timestamp` was not truncated, this function will merely
+    return a `timestamp` unaltered.
+
+    `ref_timestamp` should be an untruncated 64-bit timestamp (in nanoseconds),
+    whose value is within roughly two seconds of the original (untruncated)
+    timestamp in order for the information recovery to succeed. IT IS IMPORTANT
+    TO NOTE THAT THIS FUNCTION WILL BE UNABLE TO TELL IF THIS ASSUMPTION IS
+    VIOLATED, so `ref_timestamp` must be close enough to the original timestamp.
+
+    Returns:
+        Timestamp (in nanoseconds) relative to ref_timestamp, or None if
+        `timestamp` was less than 2^32 (clearly neither a 32-bit signed int nor
+        a 64-bit unsigned int).
+    """
+    if (timestamp >> 32) > 0:
+        return timestamp
+
+    u_timestamp = make_unsigned_int(timestamp, 32)
+    if u_timestamp is None:
+        # Likely a negative 64-bit integer, which we can't handle
+        return None
+
+    # We wish to borrow the most significant 32 bits of `ref_timestamp` to fill
+    # in the truncated bits of `timestamp`. This can be accomplished by a
+    # bitwise operation on the two timestamps.
+    #
+    # However, we need to account for boundary conditions around multiples of
+    # 2**32. If `ref_timestamp` was on one side of a boundary, and the original
+    # untruncated `timestamp` was on the other, we will have to compensate by
+    # adjusting `ref_timestamp`). Since we cannot know for sure which side the
+    # original timestamp was on, we make the assumption that `ref_timestamp`
+    # and the original timestamp were close enough together that we can use
+    # their least significant 32 bits to make our decision. This assumption will
+    # fail if the original timestamp and `ref_timestamp` were more than 2^16
+    # nanoseconds apart. If the assumption holds, we can split the least
+    # significant 32 bits into two ranges: [0, 2^16) and [2^16, 2^32).
+
+    mid = 2**16
+
+    # Get least significant 32 bits of `ref_timestamp` and `timestamp`
+    ref_timestamp_lsb = ref_timestamp & 0x00000000ffffffff
+    u_timestamp_lsb = u_timestamp & 0x00000000ffffffff
+
+    if ref_timestamp_lsb < mid and u_timestamp_lsb >= mid:
+        # Assumed to indicate that `ref_timestamp` is on the higher side of the
+        # 2**32 boundary, while `timestamp` is on the lower side, so we need to
+        # account for `ref_timestamp`'s upper 32-bits being too high.
+        if ref_timestamp > 2**32:
+            # Avoid wrap-around from underflow
+            ref_timestamp -= 2**32
+    elif ref_timestamp_lsb >= mid and u_timestamp_lsb < mid:
+        # Assumed to indicate the opposite, that we need to account for
+        # `ref_timestamp`'s upper 32-bits being too low. (We ignore handling
+        # integer overflow by assuming our timestamps won't come near 2^64 ns.)
+        ref_timestamp += 2**32
+
+    return (ref_timestamp & 0xffffffff00000000) | (u_timestamp &
+                                                   0x00000000ffffffff)
+
+
+def test_correction():
+    class Test:
+        def __init__(self, ref, val):
+            self.ref = ref
+            self.val = val
+
+    ref_delta = 5
+    val_delta = 10
+    tests = [
+        Test(2**32 - val_delta, 2**32 - ref_delta),
+        Test(2**32 + val_delta, 2**32 - ref_delta),
+        Test(2**32 - val_delta, 2**32 + ref_delta),
+        Test(2**32 + val_delta, 2**32 + ref_delta),
+
+        Test(2**33 - val_delta, 2**33 - ref_delta),
+        Test(2**33 + val_delta, 2**33 - ref_delta),
+        Test(2**33 - val_delta, 2**33 + ref_delta),
+        Test(2**33 + val_delta, 2**33 + ref_delta),
+
+        Test(2**32, 2**32),
+        Test(2**32 - 1, 2**32 - 1),
+        Test(2**32 + 1, 2**32 + 1),
+    ]
+
+    passed = True
+    for i, test in enumerate(tests):
+        val = test.val & 0x00000000ffffffff
+        result = correct_timestamp(val, test.ref)
+        if result != test.val:
+            passed = False
+            print(f"- Test case {i+1}: want {test.val}, got {result}")
+    if passed:
+        print("test_correction passed")
+    else:
+        print("test_correction failed")
+
+
+if __name__ == "__main__":
+    test_correction()
