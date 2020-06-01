01c490a9afc016ae82545c288c499c36456067be
Florian Rival <florianrival@google.com>
2020-06-01 16:16:29 +0100

    Change Swappy "long" to "int64_t"
    Script used to do the replacements: gpaste/4673313815134208
    
    Added compatibility tracers to Tuning Fork
    
    Test: all 4 samples compiling and running
    Test: Swappy and TF test apps both compiling and running
    Test: ./gradlew archiveZip archiveTfZip
    Test: ./build.sh full
    Change-Id: I067d85f1a5d07b294b030e36f471108bfd809317

--- src/tuningfork/tuningfork_swappy.cpp
+++ src/tuningfork/tuningfork_swappy.cpp
@@ -30,20 +30,29 @@ SwappyTraceWrapper::SwappyTraceWrapper(const Settings& settings)
         pre13_trace->startFrame = StartFrameCallbackPre1_3;
         pre13_trace->preWait =  PreWaitCallbackPre1_3;
         pre13_trace->postWait = PostWaitCallbackPre1_3;
+        pre13_trace->preSwapBuffers = PreSwapBuffersCallback;
+        pre13_trace->postSwapBuffers = PostSwapBuffersCallbackPre1_5;
+    } else if (swappyVersion < SWAPPY_VERSION_1_5) {
+        auto pre15_trace = reinterpret_cast<SwappyTracerPre1_5*>(&trace_);
+        pre15_trace->startFrame = StartFrameCallbackPre1_5;
+        pre15_trace->preWait =  PreWaitCallback;
+        pre15_trace->postWait = PostWaitCallbackPre1_5;
+        pre15_trace->preSwapBuffers = PreSwapBuffersCallback;
+        pre15_trace->postSwapBuffers = PostSwapBuffersCallbackPre1_5;
     } else {
         trace_.startFrame = StartFrameCallback;
         trace_.preWait =  PreWaitCallback;
         trace_.postWait = PostWaitCallback;
+        trace_.preSwapBuffers = PreSwapBuffersCallback;
+        trace_.postSwapBuffers = PostSwapBuffersCallback;
     }
-    trace_.preSwapBuffers = PreSwapBuffersCallback;
-    trace_.postSwapBuffers = PostSwapBuffersCallback;
     trace_.userData = this;
     swappyTracerFn_(&trace_);
 }
 
 // Swappy trace callbacks
 void SwappyTraceWrapper::StartFrameCallback(void* userPtr, int /*currentFrame*/,
-                                     long /*currentFrameTimeStampMs*/) {
+                                     int64_t /*desiredPresentationTimeMillis*/) {
     SwappyTraceWrapper* _this = (SwappyTraceWrapper*)userPtr;
     auto err = TuningFork_frameTick(TFTICK_PACED_FRAME_TIME);
     if (err != TFERROR_OK && err != TFERROR_TUNINGFORK_NOT_INITIALIZED) {
@@ -54,8 +63,8 @@ void SwappyTraceWrapper::StartFrameCallback(void* userPtr, int /*currentFrame*/,
 void SwappyTraceWrapper::PreWaitCallback(void* userPtr) {
 }
 
-void SwappyTraceWrapper::PostWaitCallback(void* userPtr, long cpuTimeNs, long gpuTimeNs) {
-    static long prevCpuTimeNs = 0;
+void SwappyTraceWrapper::PostWaitCallback(void* userPtr, int64_t cpuTimeNs, int64_t gpuTimeNs) {
+    static int64_t prevCpuTimeNs = 0;
     SwappyTraceWrapper *_this = (SwappyTraceWrapper *) userPtr;
     auto err = TuningFork_frameDeltaTimeNanos(TFTICK_CPU_TIME, cpuTimeNs);
     if (err != TFERROR_OK && err != TFERROR_TUNINGFORK_NOT_INITIALIZED) {
@@ -80,13 +89,28 @@ void SwappyTraceWrapper::PreSwapBuffersCallback(void* userPtr) {
     // We no longer record the swap time
 }
 
-void SwappyTraceWrapper::PostSwapBuffersCallback(void* userPtr, long /*desiredPresentationTimeMs*/) {
+void SwappyTraceWrapper::PostSwapBuffersCallback(void* userPtr, int64_t /*desiredPresentationTimeMillis*/) {
     // We no longer record the swap time
 }
 
-// Callbacks for swappy version < 1.3 where we don't have direct GPU time
+// Callbacks for swappy version < 1.5 where we use longs instead of int64_t
+void SwappyTraceWrapper::StartFrameCallbackPre1_5(void* userPtr, int currentFrame,
+                                     long desiredPresentationTimeMillis) {
+    SwappyTraceWrapper::StartFrameCallback(userPtr, currentFrame, desiredPresentationTimeMillis);
+}
+
+void SwappyTraceWrapper::PostSwapBuffersCallbackPre1_5(void* userPtr, long /*desiredPresentationTimeMillis*/) {
+    // We no longer record the swap time
+}
+
+void SwappyTraceWrapper::PostWaitCallbackPre1_5(void* userPtr, long cpuTimeNs, long gpuTimeNs) {
+    SwappyTraceWrapper::PostWaitCallback(userPtr, cpuTimeNs, gpuTimeNs);
+}
+
+// Callbacks for swappy version < 1.3 where we don't have direct GPU time and
+// we use longs instead of int64_t.
 void SwappyTraceWrapper::StartFrameCallbackPre1_3(void* userPtr, int /*currentFrame*/,
-                                     long /*currentFrameTimeStampMs*/) {
+                                     long /*desiredPresentationTimeMillis*/) {
     SwappyTraceWrapper* _this = (SwappyTraceWrapper*)userPtr;
     // There's no distinction between RAW and PACED frame time for swappy < 1.3
     // since we can't get real raw frame time.
