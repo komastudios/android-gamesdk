27c4f9a2de191698dce3bebb9e7a8a87e6991d7f
Jim Blackler <jimblackler@google.com>
2020-06-09 12:47:19 +0100

    Move parameter selection into Utils class, and allow deep merging.
    The same code is in two locations (app and tool) - I plan to create a
    library (b/158546987).
    
    Bug: 157280190
    Test: Manual
    Change-Id: I08d6037e2cd64b0cf353d0e3cf8d323d2bb9eede

--- test/istresser/app/src/main/java/net/jimblackler/istresser/Utils.java
+++ test/istresser/app/src/main/java/net/jimblackler/istresser/Utils.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import java.io.File;
 import java.io.IOException;
 
+import java.util.Iterator;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -84,6 +86,60 @@ public class Utils {
         return index;
       }
       index++;
+    }
+  }
+
+  /**
+   * Selects the parameters for a run based on the 'tests' and 'coordinates' of the test
+   * specification file.
+   * @param spec The test specification file.
+   * @return The selected parameters.
+   */
+  static JSONObject flattenParams(JSONObject spec) {
+    JSONObject params = new JSONObject();
+    try {
+      JSONArray coordinates = spec.getJSONArray("coordinates");
+      JSONArray tests = spec.getJSONArray("tests");
+
+      for (int coordinateNumber = 0; coordinateNumber != coordinates.length(); coordinateNumber++) {
+        JSONArray jsonArray = tests.getJSONArray(coordinateNumber);
+        JSONObject jsonObject = jsonArray.getJSONObject(coordinates.getInt(coordinateNumber));
+        merge(jsonObject, params);
+      }
+    } catch (JSONException e) {
+      throw new IllegalStateException(e);
+    }
+    return params;
+  }
+
+  /**
+   * Creates a deep union of two JSON objects. Arrays are concatenated and dictionaries are merged.
+   * @param in The first JSON object; read only.
+   * @param out The second JSON object and the object into which changes are written.
+   * @throws JSONException In the case of a JSON handling error.
+   */
+  private static void merge(JSONObject in, JSONObject out) throws JSONException {
+    in = new JSONObject(in.toString());  // Clone to allow original (freely mutable) copies.
+    Iterator<String> keys = in.keys();
+    while (keys.hasNext()) {
+      String key = keys.next();
+      Object inObject = in.get(key);
+      if (out.has(key)) {
+        Object outObject = out.get(key);
+        if (inObject instanceof JSONArray && outObject instanceof JSONArray) {
+          JSONArray inArray = (JSONArray) inObject;
+          JSONArray outArray = (JSONArray) outObject;
+          for (int idx = 0; idx != inArray.length(); idx++) {
+            outArray.put(inArray.get(idx));
+          }
+          continue;
+        }
+        if (inObject instanceof JSONObject && outObject instanceof JSONObject) {
+          merge((JSONObject) inObject, (JSONObject) outObject);
+          continue;
+        }
+      }
+      out.put(key, inObject);
     }
   }
 }
