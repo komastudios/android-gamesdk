2aa0135c77a9f3b67f95694248171bb0367e9283
Barış Kaya <bkaya@google.com>
2020-06-12 01:45:57 +0100

    Modified istresser so that a ./gradlew build command can build the entire app without warnings
    Change-Id: I952fbca90ffa6cb0a574da7ff89770df75978b36

--- test/istresser/app/src/main/java/net/jimblackler/istresser/ServiceCommunicationHelper.java
+++ test/istresser/app/src/main/java/net/jimblackler/istresser/ServiceCommunicationHelper.java
@@ -3,6 +3,9 @@ package net.jimblackler.istresser;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.util.Log;
 
 class ServiceCommunicationHelper {
 
@@ -11,6 +14,8 @@ class ServiceCommunicationHelper {
   private static final String ALLOCATE_MEMORY_ACTION = "allocate";
   private static final String FREE_MEMORY_ACTION = "free";
   private static final String FIRST_TIME = "first_time";
+
+  private static final String TAG = MainActivity.class.getSimpleName();
 
   static final String CRASHED_BEFORE = "crashed_before";
   static final String TOTAL_MEMORY_MB = "total_memory_mb";
@@ -25,6 +30,7 @@ class ServiceCommunicationHelper {
 
   void allocateServerMemory(int megabytes) {
 
+    if (VERSION.SDK_INT >= VERSION_CODES.O) {
     Intent intent = new Intent();
     intent.setComponent(
         new ComponentName(
@@ -34,10 +40,14 @@ class ServiceCommunicationHelper {
     intent.putExtra(TOTAL_MEMORY_MB, megabytes);
     intent.putExtra(FIRST_TIME, isFirstTime);
     isFirstTime = false;
-    context.startForegroundService(intent);
+      context.startForegroundService(intent);
+    } else {
+      Log.w(TAG, "Unable to request server memory, version is below 26");
+    }
   }
 
   void queryServerMemory() {
+    if (VERSION.SDK_INT >= VERSION_CODES.O) {
     Intent intent = new Intent();
     intent.setComponent(
         new ComponentName(
@@ -46,10 +56,14 @@ class ServiceCommunicationHelper {
     intent.putExtra(ACTION_TYPE, QUERY_ACTION);
     intent.putExtra(FIRST_TIME, isFirstTime);
     isFirstTime = false;
-    context.startForegroundService(intent);
+      context.startForegroundService(intent);
+    } else {
+      Log.w(TAG, "Unable to query server memory, version is below 26");
+    }
   }
 
   void freeServerMemory() {
+    if (VERSION.SDK_INT >= VERSION_CODES.O) {
     Intent intent = new Intent();
     intent.setComponent(
         new ComponentName(
@@ -58,6 +72,9 @@ class ServiceCommunicationHelper {
     intent.putExtra(ACTION_TYPE, FREE_MEMORY_ACTION);
     intent.putExtra(FIRST_TIME, isFirstTime);
     isFirstTime = false;
-    context.startForegroundService(intent);
+      context.startForegroundService(intent);
+    } else {
+      Log.w(TAG, "Unable to free server memory, version is below 26");
+    }
   }
 }
