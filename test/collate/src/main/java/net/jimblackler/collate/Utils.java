819154d49c8ef1cc9c2e9e0f56babf118c241949
Jim Blackler <jimblackler@google.com>
2020-06-13 00:45:00 +0100

    Device test: take the timeout value from the params.
    The getDuration() code is copy pasted from the iStresser app but I will eventually create a shared library.
    
    Test: Manual
    Change-Id: I0afbfc0beffc2b25c62c8c1404fcbde4e528a9cb

--- test/collate/src/main/java/net/jimblackler/collate/Utils.java
+++ test/collate/src/main/java/net/jimblackler/collate/Utils.java
@@ -153,4 +153,54 @@ class Utils {
   static String getProjectId() throws IOException {
     return execute(Config.GCLOUD_EXECUTABLE.toString(), "config", "get-value", "project");
   }
+
+  /**
+   * Converts a time duration in an object to a number of milliseconds. If the object is a number,
+   * it is interpreted as the number of milliseconds. If the value is a string, it is converted
+   * according to the specified unit. e.g. "30s", "1H". No unit is interpreted as milliseconds.
+   *
+   * @param object The object to extract from.
+   * @return The equivalent number of milliseconds.
+   */
+  static long getDuration(Object object) {
+    if (object instanceof Number) {
+      return ((Number) object).longValue();
+    }
+
+    if (object instanceof String) {
+      String str = ((String) object).toUpperCase();
+      int unitPosition = str.indexOf('S');
+      int unitMultiplier = 1000;
+      if (unitPosition == -1) {
+        unitPosition = str.indexOf('M');
+        unitMultiplier *= 60;
+        if (unitPosition == -1) {
+          unitPosition = str.indexOf('H');
+          unitMultiplier *= 60;
+          if (unitPosition == -1) {
+            unitMultiplier = 1;
+          }
+        }
+      }
+      float value = Float.parseFloat(str.substring(0, unitPosition));
+      return (long) (value * unitMultiplier);
+    }
+    throw new IllegalArgumentException("Input to getDuration neither string or number.");
+  }
+
+  /**
+   * Return the value associated with the key in the given object. If the object does not define
+   * the key, return the specified default value.
+   * @param object The object to extract the value from.
+   * @param key The key associated with the value.
+   * @param defaultValue The value to return if the object does not specify the key.
+   * @return The associated value, or the defaultValue if the object does not define the key.
+   */
+  static Object getOrDefault(JSONObject object, String key, Object defaultValue) {
+    try {
+      return object.get(key);
+    } catch (JSONException e) {
+      return defaultValue;
+    }
+  }
 }
