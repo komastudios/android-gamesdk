3419946b8963b1bfc74040126f984a61d4107a36
Florian Rival <florianrival@google.com>
2020-06-17 16:01:33 +0100

    Factor CMake options into BuildOptions and BuildFolders
    Test: ./gradlew archiveTfZip archiveZip
    Bug: 139918123
    Change-Id: I546a4b07057bee59d972c16cd411e3ebdda1edf9

---
+++ buildSrc/src/main/java/com/google/androidgamesdk/BuildOptions.kt
@@ -0,0 +1,29 @@
+package com.google.androidgamesdk
+
+data class BuildOptions(
+    val buildType: String,
+    val threadChecks: Boolean,
+    var stl: String? = null,
+    var arch: String? = null,
+    var buildFolders: BuildFolders? = null
+) {
+    constructor(buildType: String, threadChecks: Boolean) :
+        this(buildType, threadChecks, null, null, null)
+    constructor(buildType: String, threadChecks: Boolean, stl: String) :
+        this(buildType, threadChecks, stl, null, null)
+
+    fun isCompleted(): Boolean {
+        return stl != null && arch != null && buildFolders != null
+    }
+
+    // Helpers for Groovy that does not have copy() support:
+    fun withStl(stl: String): BuildOptions {
+        return this.copy(stl = stl)
+    }
+    fun withArch(arch: String): BuildOptions {
+        return this.copy(arch = arch)
+    }
+    fun withBuildFolders(buildFolders: BuildFolders): BuildOptions {
+        return this.copy(buildFolders = buildFolders)
+    }
+}
