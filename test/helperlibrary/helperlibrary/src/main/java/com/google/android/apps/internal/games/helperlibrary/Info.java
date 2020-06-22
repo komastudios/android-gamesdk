2a4f09e29e6d43e8f8b0666aba9b2e08902ff805
Jim Blackler <jimblackler@google.com>
2020-06-22 17:18:35 +0100

    Memory API calls no longer throw JSONException.
    It places an undue burden on the client - what are they supposed to do with these errors?
    
    In practice I have never seen a 'put' action throw one of these anyway.
    
    Bug: 159588257
    Test: Manual
    Change-Id: I2023bc174350c48cf34e611b0a5c37c06dfc7f6a

--- test/helperlibrary/helperlibrary/src/main/java/com/google/android/apps/internal/games/helperlibrary/Info.java
+++ test/helperlibrary/helperlibrary/src/main/java/com/google/android/apps/internal/games/helperlibrary/Info.java
@@ -9,6 +9,7 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Debug;
+import android.util.Log;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,8 @@ import org.json.JSONObject;
 
 /** A class to provide metrics of current memory usage to an application in JSON format. */
 public class Info {
+  private static final String TAG = Info.class.getSimpleName();
+
   private static final List<String> MEMINFO_FIELDS = Arrays.asList("Active", "Active(anon)",
       "Active(file)", "AnonPages", "MemAvailable", "MemFree", "VmData", "VmRSS");
   private static final List<String> MEMINFO_FIELDS_CONSTANT =
@@ -36,68 +39,72 @@ public class Info {
    * @param fetchDebug Whether to fetch debug-based params.
    * @return A JSONObject containing current memory metrics.
    */
-  public JSONObject getMemoryMetrics(Context context, boolean fetchDebug) throws JSONException {
+  public JSONObject getMemoryMetrics(Context context, boolean fetchDebug) {
     JSONObject report = new JSONObject();
-    report.put("nativeAllocated", Debug.getNativeHeapAllocatedSize());
+    try {
+      report.put("nativeAllocated", Debug.getNativeHeapAllocatedSize());
 
-    ActivityManager activityManager = (ActivityManager) Objects.requireNonNull(
-        context.getSystemService((Context.ACTIVITY_SERVICE)));
-    ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
-    activityManager.getMemoryInfo(memoryInfo);
-    report.put("availMem", memoryInfo.availMem);
-    if (memoryInfo.lowMemory) {
-      report.put("lowMemory", true);
-    }
+      ActivityManager activityManager = (ActivityManager) Objects.requireNonNull(
+          context.getSystemService((Context.ACTIVITY_SERVICE)));
+      ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+      activityManager.getMemoryInfo(memoryInfo);
+      report.put("availMem", memoryInfo.availMem);
+      if (memoryInfo.lowMemory) {
+        report.put("lowMemory", true);
+      }
 
-    if (mapTester == null) {
-      mapTester = new MapTester(context.getCacheDir());
-    }
+      if (mapTester == null) {
+        mapTester = new MapTester(context.getCacheDir());
+      }
 
-    if (mapTester.warning()) {
-      report.put("mapTester", true);
-      mapTester.reset();
-    }
+      if (mapTester.warning()) {
+        report.put("mapTester", true);
+        mapTester.reset();
+      }
 
-    report.put("oom_score", getOomScore());
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && fetchDebug) {
-      Debug.MemoryInfo[] debugMemoryInfos = getDebugMemoryInfo(activityManager);
-      if (debugMemoryInfos.length > 0) {
-        for (String key : SUMMARY_FIELDS) {
-          long value = 0;
-          for (Debug.MemoryInfo debugMemoryInfo : debugMemoryInfos) {
-            value += Long.parseLong(debugMemoryInfo.getMemoryStat(key));
+      report.put("oom_score", getOomScore());
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && fetchDebug) {
+        Debug.MemoryInfo[] debugMemoryInfos = getDebugMemoryInfo(activityManager);
+        if (debugMemoryInfos.length > 0) {
+          for (String key : SUMMARY_FIELDS) {
+            long value = 0;
+            for (Debug.MemoryInfo debugMemoryInfo : debugMemoryInfos) {
+              value += Long.parseLong(debugMemoryInfo.getMemoryStat(key));
+            }
+            report.put(key, value * BYTES_IN_KILOBYTE);
           }
-          report.put(key, value * BYTES_IN_KILOBYTE);
         }
       }
-    }
 
-    Map<String, Long> memInfo = processMeminfo();
-    for (Map.Entry<String, Long> pair : memInfo.entrySet()) {
-      String key = pair.getKey();
-      if (MEMINFO_FIELDS.contains(key)) {
-        report.put(key, pair.getValue());
-      }
-    }
-
-    for (Map.Entry<String, Long> pair : processStatus().entrySet()) {
-      String key = pair.getKey();
-      if (STATUS_FIELDS.contains(key)) {
-        report.put(key, pair.getValue());
-      }
-    }
-    if (baseline == null) {
-      JSONObject constant = new JSONObject();
+      Map<String, Long> memInfo = processMeminfo();
       for (Map.Entry<String, Long> pair : memInfo.entrySet()) {
         String key = pair.getKey();
-        if (MEMINFO_FIELDS_CONSTANT.contains(key)) {
-          constant.put(key, pair.getValue());
+        if (MEMINFO_FIELDS.contains(key)) {
+          report.put(key, pair.getValue());
         }
       }
-      constant.put("totalMem", memoryInfo.totalMem);
-      constant.put("threshold", memoryInfo.threshold);
-      report.put("constant", constant);
-      baseline = new JSONObject(report.toString());
+
+      for (Map.Entry<String, Long> pair : processStatus().entrySet()) {
+        String key = pair.getKey();
+        if (STATUS_FIELDS.contains(key)) {
+          report.put(key, pair.getValue());
+        }
+      }
+      if (baseline == null) {
+        JSONObject constant = new JSONObject();
+        for (Map.Entry<String, Long> pair : memInfo.entrySet()) {
+          String key = pair.getKey();
+          if (MEMINFO_FIELDS_CONSTANT.contains(key)) {
+            constant.put(key, pair.getValue());
+          }
+        }
+        constant.put("totalMem", memoryInfo.totalMem);
+        constant.put("threshold", memoryInfo.threshold);
+        report.put("constant", constant);
+        baseline = new JSONObject(report.toString());
+      }
+    } catch (JSONException ex) {
+      Log.w(TAG, "Problem getting memory metrics", ex);
     }
     return report;
   }
