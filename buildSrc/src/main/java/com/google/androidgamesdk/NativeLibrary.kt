51f43431ed418a0c3c14ed3b194ed0f337ead01c
Florian Rival <florianrival@google.com>
2020-06-26 16:25:55 +0100

    Refactor build of TuningFork and Swappy
    * Remove usage of a boolean to indicate if Tuning Fork must be built. Instead, allow to configure the native libraries in build.gradle
    * Also avoid library specific work in tasks to copy samples.
    
    Next step will be to rework tasks to reduce their number, prefering command line options.
    
    Test: ./gradlew archiveZip archiveTfZip
    Bug: 1352366
    Change-Id: I50edf0a4a4645d03f54836a3f41bf9fc7a36f1bd

---
+++ buildSrc/src/main/java/com/google/androidgamesdk/NativeLibrary.kt
@@ -0,0 +1,51 @@
+package com.google.androidgamesdk
+
+data class NativeLibrary(
+    val nativeLibraryName: String,
+    val cmakeOption: String
+) {
+    var aarLibraryName: String = ""
+        private set
+    var aarVersion: String = "0.0.0"
+        private set
+    var sampleAndroidProjectPaths: ArrayList<SampleFolder> = ArrayList()
+        private set
+    var sampleExtraFolderPaths: ArrayList<SampleFolder> = ArrayList()
+        private set
+
+    fun setAarLibrary(
+        aarLibraryName: String,
+        aarVersion: String
+    ): NativeLibrary {
+        this.aarLibraryName = aarLibraryName
+        this.aarVersion = aarVersion
+
+        return this
+    }
+
+    fun addSampleAndroidProject(sampleFolder: SampleFolder): NativeLibrary {
+        sampleAndroidProjectPaths.add(sampleFolder)
+        return this
+    }
+
+    fun addSampleExtraFolder(sampleFolder: SampleFolder): NativeLibrary {
+        sampleExtraFolderPaths.add(sampleFolder)
+        return this
+    }
+
+    data class SampleFolder(
+        val sourcePath: String,
+        val destinationPath: String
+    ) {
+        constructor(sourcePath: String) : this(sourcePath, sourcePath)
+
+        var includePattern: String? = null
+            private set
+
+        fun include(includePattern: String): SampleFolder {
+            this.includePattern = includePattern
+
+            return this
+        }
+    }
+}
