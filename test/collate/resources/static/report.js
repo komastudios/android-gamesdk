44a7470ef3e9e76e7308e59607229540531042ff
Jim Blackler <jimblackler@google.com>
2020-07-14 18:12:44 +0100

    Splitting up assets from the reports.
    Breaking out CSS into targeted files.
    Breaking out the 'data explorer' into its own JS file.
    Reworking the link rewriter to remove any dependency for the above.
    
    Bug: 161203844
    Test: Manual
    Change-Id: Ia30bb59ee76cd5f031d3c1f3b8c0f6c5ffcc53b4

--- test/collate/resources/static/report.js
+++ test/collate/resources/static/report.js
@@ -1,3 +1,5 @@
+import {appendStyledJson} from './dataExplorer.js';
+
 function hashCode(str) {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
@@ -6,10 +8,12 @@ function hashCode(str) {
   return hash;
 }
 
-function linkClicked(evt) {
-  evt.target.href =
-      evt.target.href.replace('gs://', 'https://storage.cloud.google.com/');
-}
+document.body.addEventListener('click', evt => {
+  if (evt.target.href) {
+    evt.target.href =
+        evt.target.href.replace('gs://', 'https://storage.cloud.google.com/');
+  }
+}, false);
 
 function rowMetrics(row) {
   if ('advice' in row) {
@@ -25,65 +29,6 @@ function rowMetrics(row) {
 
 function rowTime(row) {
   return rowMetrics(row).meta.time;
-}
-
-function appendStyledJson(node, obj) {
-  if (Array.isArray(obj)) {
-    const ol = document.createElement('ol');
-    ol.setAttribute('start', 0);
-    for (let idx = 0; idx !== obj.length; idx++) {
-      const li = document.createElement('li');
-      appendStyledJson(li, obj[idx]);
-      ol.appendChild(li);
-    }
-    node.appendChild(ol);
-  } else if (typeof obj === 'object') {
-    const table = document.createElement('table');
-    for (const [key, value] of Object.entries(obj)) {
-      const tr = document.createElement('tr');
-      const td0 = document.createElement('td');
-      td0.appendChild(document.createTextNode(key));
-      tr.appendChild(td0);
-      const td1 = document.createElement('td');
-      if (typeof value === 'object' || Array.isArray(value)) {
-        const details = document.createElement('details');
-        const summary = document.createElement('summary');
-        const span = document.createElement('span');
-        const text = document.createTextNode(JSON.stringify(value));
-        span.appendChild(text);
-        summary.appendChild(span);
-        details.appendChild(summary);
-        appendStyledJson(details, value);
-        td1.appendChild(details);
-      } else {
-        appendStyledJson(td1, value);
-      }
-      tr.appendChild(td1);
-      table.appendChild(tr);
-    }
-    node.append(table);
-  } else {
-    if (typeof obj !== 'string') {
-      obj = obj.toString();
-    }
-    const textNode = document.createTextNode(obj);
-    if (/[\r\n]/.exec(obj)) {
-      const pre = document.createElement('pre');
-      pre.appendChild(textNode);
-      node.appendChild(pre);
-    } else {
-      const a = document.createElement('a');
-      a.appendChild(textNode);
-      a.addEventListener('click', linkClicked, false);
-      a.setAttribute('target', '_blank');
-      a.href = obj;
-      if (a.href === obj) {
-        node.appendChild(a);
-      } else {
-        node.appendChild(textNode);
-      }
-    }
-  }
 }
 
 const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
@@ -146,7 +91,6 @@ for (const result of data) {
           const anchor = document.createElement('a');
           span.appendChild(anchor);
           anchor.href = toolLogs[idx].fileUri;
-          anchor.addEventListener('click', linkClicked, false);
           anchor.appendChild(document.createTextNode('Logs'));
         }
         paragraph.appendChild(span);
