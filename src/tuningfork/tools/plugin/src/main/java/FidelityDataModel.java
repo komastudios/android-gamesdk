c85aad07e92426f1a1f97ab3e9686c8668c63628
Teodora Argintaru <targintaru@google.com>
2020-08-11 10:24:59 +0000

    Add data models for annotations, enums, fidelity settings
    Data models will be used when passing data from input table's
    controllers to be parsed as .proto file.
    
    Test: none (no functionality to test yet)
    Change-Id: I4dfad6baadccba2eef8eeb8ca2c1a6182dd70bd5

---
+++ src/tuningfork/tools/plugin/src/main/java/FidelityDataModel.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+import java.util.HashMap;
+
+public class FidelityDataModel {
+
+  private HashMap<String, Object> fields;
+
+  FidelityDataModel() {
+    fields = new HashMap<>();
+  }
+
+  // Will be used when adding a new Fidelity Setting.
+  // 'type' will be an empty Integer/Float/EnumDataModel object.
+  public boolean addField(String paramName, Object type) {
+    if (fields.containsKey(paramName)) {
+      return false;
+    }
+    fields.put(paramName, type);
+    return true;
+  }
+
+  // Will be used when adding a new Quality Setting.
+  public void addValue(String paramName, Object value) {
+    fields.put(paramName, value);
+  }
+
+  public Object getValue(String paramName) {
+    return fields.get(paramName);
+  }
+}
