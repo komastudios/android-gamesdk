b84dc937c5532b8f90c58b55ff7a4635d10df621
Anastasiia Volobueva <volobushek@google.com>
2020-08-24 15:07:56 +0300

    ValidationSettings tab added with table, progress bar, radio buttons and text field. JTree changed.
    Test: None
    Change-Id: I93275679a14445162288d5fec0dd6667dcd6a3a6

--- src/tuningfork/tools/plugin/src/main/java/View/PluginLayout.java
+++ src/tuningfork/tools/plugin/src/main/java/View/PluginLayout.java
@@ -20,6 +20,7 @@ import Controller.Annotation.AnnotationTabController;
 import View.Annotation.AnnotationTab;
 import View.Fidelity.FidelityTab;
 import View.Quality.QualityTab;
+import View.ValidationSettings.ValidationSettingsTab;
 import com.intellij.openapi.project.Project;
 import java.awt.Color;
 import java.awt.Component;
@@ -42,6 +43,7 @@ public class PluginLayout extends JPanel {
   private JPanel qualitySettingsLayout;
   private JPanel annotationsLayout;
   private JPanel fidelitySettingsLayout;
+  private JPanel validationSettingsLayout;
   private JPanel menuPanel;
   private JTree menu;
   private static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
@@ -62,6 +64,7 @@ public class PluginLayout extends JPanel {
     this.add(fidelitySettingsLayout);
     this.add(annotationsLayout);
     this.add(qualitySettingsLayout);
+    this.add(validationSettingsLayout);
   }
 
   private void changeLayoutVisibility(JPanel toSetVisible) {
@@ -80,30 +83,41 @@ public class PluginLayout extends JPanel {
     DefaultMutableTreeNode fidelityNode = new DefaultMutableTreeNode("Fidelity settings");
     DefaultMutableTreeNode qualitySettingsNode = new DefaultMutableTreeNode("Quality settings");
 
+    //new root for validation and settings attached there
+    DefaultMutableTreeNode validationRoot = new DefaultMutableTreeNode("Validation");
+    DefaultMutableTreeNode validationSettings = new DefaultMutableTreeNode("Settings");
+
+    validationRoot.add(validationSettings);
+
     settingsRoot.add(annotationsNode);
     settingsRoot.add(fidelityNode);
     settingsRoot.add(qualitySettingsNode);
 
+    DefaultMutableTreeNode root = new DefaultMutableTreeNode("Preferences");
+
+    root.add(settingsRoot);
+    root.add(validationRoot);
+
     UIManager.put("Tree.rendererFillBackground", false);
-    menu = new JTree(settingsRoot);
+    menu = new JTree(root);
     menu.setCellRenderer(new CustomCellRenderer());
     menu.setBackground(new Color(0, true));
     menu.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-    menu.addTreeSelectionListener(
-        treeSelectionEvent -> {
-          DefaultMutableTreeNode node =
-              (DefaultMutableTreeNode)
-                  treeSelectionEvent.getNewLeadSelectionPath().getLastPathComponent();
-          if (node.equals(annotationsNode)) {
-            changeLayoutVisibility(annotationsLayout);
-          } else if (node.equals(fidelityNode)) {
-            changeLayoutVisibility(fidelitySettingsLayout);
-          } else if (node.equals(qualitySettingsNode)) {
-            changeLayoutVisibility(qualitySettingsLayout);
-          } else if (!node.isLeaf()) {
-            menu.setMinimumSize(new Dimension(SCREEN_SIZE.width / 6, SCREEN_SIZE.height / 2));
-          }
-        });
+    menu.addTreeSelectionListener(treeSelectionEvent -> {
+      DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+              treeSelectionEvent.getNewLeadSelectionPath().getLastPathComponent();
+      if (node.equals(annotationsNode)) {
+        changeLayoutVisibility(annotationsLayout);
+      } else if (node.equals(fidelityNode)) {
+        changeLayoutVisibility(fidelitySettingsLayout);
+      } else if (node.equals(qualitySettingsNode)) {
+        changeLayoutVisibility(qualitySettingsLayout);
+      } else if (node.equals(validationSettings)){
+        changeLayoutVisibility(validationSettingsLayout);
+      } else if (!node.isLeaf()) {
+        menu.setMinimumSize(new Dimension(SCREEN_SIZE.width / 6, SCREEN_SIZE.height / 2));
+      }
+    });
     menu.setMinimumSize(new Dimension(SCREEN_SIZE.width / 6, SCREEN_SIZE.height / 2));
     menu.setSelectionModel(new LeafOnlySelectionModel());
     menuPanel.setSize(new Dimension(SCREEN_SIZE.width / 6, SCREEN_SIZE.height / 2));
@@ -121,13 +135,17 @@ public class PluginLayout extends JPanel {
     fidelitySettingsLayout = new FidelityTab();
     fidelitySettingsLayout.setVisible(false);
     fidelitySettingsLayout.setSize(
-        new Dimension(5 * SCREEN_SIZE.width / 6, SCREEN_SIZE.height / 2));
+            new Dimension(5 * SCREEN_SIZE.width / 6, SCREEN_SIZE.height / 2));
+    validationSettingsLayout = new ValidationSettingsTab();
+    validationSettingsLayout.setSize(new Dimension(5 * SCREEN_SIZE.width / 6, SCREEN_SIZE.height / 2));
+    validationSettingsLayout.setVisible(false);
 
     menuPanel = new JPanel();
 
     panels.add(qualitySettingsLayout);
     panels.add(annotationsLayout);
     panels.add(fidelitySettingsLayout);
+    panels.add(validationSettingsLayout);
 
     initMenuTree();
   }
