99b8a9ac0627920dcdc72ac0234a0a31288399dd
Teodora Argintaru <targintaru@google.com>
2020-08-26 12:22:26 +0000

    Connect pieces for showing/writing back .proto annotation/fidelity
    Change-Id: I0363125d81c4412020b4d4d0eb7ffebfb5824fdc

--- src/tuningfork/tools/plugin/src/main/java/View/Dialog/MainDialogWrapper.java
+++ src/tuningfork/tools/plugin/src/main/java/View/Dialog/MainDialogWrapper.java
@@ -16,9 +16,20 @@
 
 package View.Dialog;
 
+import Controller.Annotation.AnnotationTabController;
+import Controller.Fidelity.FidelityTabController;
+import Model.EnumDataModel;
+import Model.MessageDataModel;
+import Utils.Assets.AssetsFinder;
+import Utils.Assets.AssetsWriter;
 import View.PluginLayout;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import java.util.List;
 import javax.swing.JComponent;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,17 +37,71 @@ public class MainDialogWrapper extends DialogWrapper {
 
   private static PluginLayout pluginLayout;
   private final Project project;
+  private final NotificationGroup NOTIFICATION_GROUP =
+      new NotificationGroup("Android Performance Tuner", NotificationDisplayType.BALLOON, true);
+  private AnnotationTabController annotationTabController;
+  private FidelityTabController fidelityTabController;
+  private MessageDataModel annotationData;
+  private MessageDataModel fidelityData;
+  private List<EnumDataModel> enumData;
 
-  public MainDialogWrapper(@Nullable Project project) {
-    super(project);
-    init();
-    setTitle("Android Performance Tuner Plugin");
-    this.project = project;
+  private void addNotification(String errorMessage) {
+    Notification notification = NOTIFICATION_GROUP.createNotification(
+        errorMessage,
+        NotificationType.ERROR);
+    notification.notify(project);
   }
 
   @Override
-  protected @Nullable JComponent createCenterPanel() {
-    pluginLayout = new PluginLayout(project);
+  protected void doOKAction() {
+    AssetsWriter assetsWriter = new AssetsWriter(
+        AssetsFinder.findAssets(project.getProjectFilePath().split(".idea")[0]).getAbsolutePath());
+    annotationTabController = pluginLayout.getAnnotationTabController();
+    fidelityTabController = pluginLayout.getFidelityTabController();
+
+    List<EnumDataModel> annotationEnums = annotationTabController.getEnums();
+
+    if (annotationEnums == null) {
+      addNotification(
+          "Unable to write annotation and quality settings back to .proto files. You must add some settings first.1");
+      return;
+    }
+
+    if (!pluginLayout.saveSettings()) {
+      addNotification(
+          "Unable to write annotation and quality settings back to .proto files. Different fields can't have the same name.");
+      return;
+    }
+
+    MessageDataModel fidelityModel = fidelityTabController.getFidelityData();
+    MessageDataModel annotationModel = annotationTabController.getAnnotationDataModel();
+
+    if (!assetsWriter.saveDevTuningForkProto(annotationEnums, annotationModel, fidelityModel)) {
+      addNotification("Unable to write annotation and quality settings back to .proto files");
+    } else {
+      Notification notification = NOTIFICATION_GROUP.createNotification(
+          "Android Performance Tuner settings saved successfully!",
+          NotificationType.INFORMATION);
+      notification.notify(project);
+      super.doOKAction();
+    }
+  }
+
+  public MainDialogWrapper(@Nullable Project project, MessageDataModel annotationData,
+      MessageDataModel fidelityData, List<EnumDataModel> enumData) {
+    super(project);
+    this.annotationData = annotationData;
+    this.enumData = enumData;
+    this.fidelityData = fidelityData;
+    this.project = project;
+    setTitle("Android Performance Tuner Plugin");
+    init();
+  }
+
+  @Override
+  protected @Nullable
+  JComponent createCenterPanel() {
+    pluginLayout = new PluginLayout(annotationData, fidelityData, enumData);
     return pluginLayout;
   }
 }
