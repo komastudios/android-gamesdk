4bd5cedc2c849ce4b8504330190c99dfd1ae5496
Insaf Latypov <saferif@google.com>
2020-09-17 13:26:23 +0100

    Make Windows build script honour DIST_DIR variable.
    Test: manual
    Change-Id: Id68c71085bc35dd02aa64d5c42e21dcfaab42903

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 set ANDROID_HOME=C:\win_build_extras\sdk
 
-gradlew packageLocalZip -Plibraries=swappy,tuningfork -PincludeSampleArtifacts -Pndk=21.3.6528147 --no-daemon
+if not defined DIST_DIR set DIST_DIR="%CD%\..\package"
+
+gradlew packageLocalZip -Plibraries=swappy,tuningfork -PincludeSampleArtifacts -PdistPath="%DIST_DIR%" -Pndk=21.3.6528147 --no-daemon
