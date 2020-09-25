2e6022bc7bdb4a514a482c241ed4f0246e24d78e
Jim Blackler <jimblackler@google.com>
2020-09-25 17:12:15 +0100

    GCloud executable defaults to the location Google official installers use
    Test: Manual
    Change-Id: Icd6f62438ae184ebcdfadcfdd2ee5512da16f206

--- test/collate/src/main/java/net/jimblackler/collate/Config.java
+++ test/collate/src/main/java/net/jimblackler/collate/Config.java
@@ -5,7 +5,13 @@ import java.nio.file.Path;
 public class Config {
   private static final Path HOME = Path.of(System.getProperty("user.home"));
   private static final Path SDK_BASE = HOME.resolve("code/android-games-sdk");
-  private static final Path GCLOUD_LOCATION = HOME.resolve("google-cloud-sdk/bin");
+  private static final Path GCLOUD_LOCATION = HOME.resolve("Library")
+                                                  .resolve("Application Support")
+                                                  .resolve("google-cloud-tools-java")
+                                                  .resolve("managed-cloud-sdk")
+                                                  .resolve("LATEST")
+                                                  .resolve("google-cloud-sdk")
+                                                  .resolve("bin");
 
   static final Path GRABBER_BASE = SDK_BASE.resolve("gamesdk/test/grabber");
   static final Path GCLOUD_EXECUTABLE = GCLOUD_LOCATION.resolve("gcloud");
