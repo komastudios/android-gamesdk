8d5f767bec7a5a2eb231c8b055b76785bfe99fdb
Baris Kaya <bkaya@google.com>
2020-09-27 18:14:37 +0300

    Added a skeleton library for Memory Advice Native Library
    Test: Manual
    Change-Id: Iabb437cc75d4f8d11d3b1979599f9bc3a54ef4b7

---
+++ src/memory_advice/core/memory_advice_c.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "memory_advice/memory_advice.h"
+#include "memory_hints_internal.h"
+
+extern "C" {
+
+void MemoryAdvice_init_internal(JNIEnv *env, jobject context) {
+    // TODO: implementation
+}
+
+bool MemoryAdvice_testLibraryAccess(bool testValue) {
+    return memory_advice::TestLibraryAccess(testValue);
+}
+
+void MEMORY_HINTS_VERSION_SYMBOL() {
+    // Intentionally empty: this function is used to ensure that the proper
+    // version of the library is linked against the proper headers.
+    // In case of mismatch, a linker error will be triggered because of an
+    // undefined symbol, as the name of the function depends on the version.
+}
+
+}  // extern "C" {
