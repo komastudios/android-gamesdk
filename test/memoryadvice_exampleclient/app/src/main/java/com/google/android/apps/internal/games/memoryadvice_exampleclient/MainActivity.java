116025c3553c2311ff1abec915fc9f100bd12b74
Jim Blackler <jimblackler@google.com>
2020-09-29 11:21:42 +0100

    An example client for the Memory Advice library.
    The main purpose is this is to help the public API to stay stable, and in testing the examples in the documentation.
    
    Test: Manual
    Change-Id: If037380e97eb84dbc940adfce0af813e402d1cbf

---
+++ test/memoryadvice_exampleclient/app/src/main/java/com/google/android/apps/internal/games/memoryadvice_exampleclient/MainActivity.java
@@ -0,0 +1,46 @@
+package com.google.android.apps.internal.games.memoryadvice_exampleclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.android.apps.internal.games.memoryadvice.MemoryAdvisor;
+import com.google.android.apps.internal.games.memoryadvice.MemoryWatcher;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity {
+  private MemoryAdvisor memoryAdvisor;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    memoryAdvisor = new MemoryAdvisor(this, new Runnable() {
+      @Override
+      public void run() {
+        MemoryWatcher memoryWatcher =
+            new MemoryWatcher(memoryAdvisor, 10, 1000, new MemoryWatcher.Client() {
+              @Override
+              public void newState(MemoryAdvisor.MemoryState state) {}
+            });
+
+        JSONObject deviceInfo = memoryAdvisor.getDeviceInfo(MainActivity.this);
+
+        System.out.println(deviceInfo);
+
+        JSONObject advice = memoryAdvisor.getAdvice();
+
+        MemoryAdvisor.MemoryState memoryState = MemoryAdvisor.getMemoryState(advice);
+
+        long availabilityEstimate = MemoryAdvisor.availabilityEstimate(advice);
+
+        System.out.println(advice);
+      }
+    });
+  }
+
+  @Override
+  public void onTrimMemory(int level) {
+    memoryAdvisor.setOnTrim(level);
+    super.onTrimMemory(level);
+  }
+}
