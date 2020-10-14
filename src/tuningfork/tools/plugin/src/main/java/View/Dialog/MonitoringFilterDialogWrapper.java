aeb623e95e9d40a68d33f8c72a30455d3d5cc7f9
Mohannad Farrag <aymanm@google.com>
2020-10-14 16:52:47 +0200

    Add logic for histograms filtering
    *Added filtration by phone model/annotation/fidelity
    *Merge different annotations together
    *More improvement on histogram UI
    
    Test: Experimentsdemo
    Change-Id: Iac30c994ba5cd268fc80ef66e1fe8a7b88006556

--- src/tuningfork/tools/plugin/src/main/java/View/Dialog/MonitoringFilterDialogWrapper.java
+++ src/tuningfork/tools/plugin/src/main/java/View/Dialog/MonitoringFilterDialogWrapper.java
@@ -18,6 +18,7 @@ package View.Dialog;
 
 import Controller.Monitoring.MonitoringController.HistogramTree;
 import Controller.Monitoring.MonitoringController.HistogramTree.Node;
+import Model.MonitorFilterModel;
 import View.Monitoring.MonitoringTab.JTreeNode;
 import View.Monitoring.MonitoringTab.TreeToolTipRenderer;
 import com.intellij.icons.AllIcons.Actions;
@@ -27,7 +28,10 @@ import com.intellij.ui.AnActionButton;
 import com.intellij.ui.ToolbarDecorator;
 import java.awt.Dimension;
 import java.awt.event.ItemEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -51,75 +55,61 @@ import org.jetbrains.annotations.Nullable;
 public class MonitoringFilterDialogWrapper extends DialogWrapper {
 
   private final HistogramTree histogramTree;
+  private final PropertyChangeSupport propertyChangeSupport;
+  private FilterPanel filterPanel;
 
   public MonitoringFilterDialogWrapper(HistogramTree tree) {
     super(true);
     histogramTree = tree;
+    propertyChangeSupport = new PropertyChangeSupport(this);
+    filterPanel = new FilterPanel();
     setTitle("Histograms Filter");
     init();
+  }
+
+  public void addPropertyChangeListener(PropertyChangeListener listener) {
+    propertyChangeSupport.addPropertyChangeListener(listener);
   }
 
   @Nullable
   @Override
   protected JComponent createCenterPanel() {
-    return new FilterPanel();
+    return filterPanel;
   }
 
   @Override
   protected void doOKAction() {
+    MonitorFilterModel model = new MonitorFilterModel(filterPanel.getPhoneModel(),
+        filterPanel.getAnnotations(), filterPanel.getFidelity());
+    propertyChangeSupport.firePropertyChange("addFilter", null, model);
     super.doOKAction();
   }
 
   private final class FilterPanel extends JPanel {
 
     private JTree annotationsTree;
-    private JTree fidelityTree;
-    private JPanel annotationDecorator, fidelityDecorator;
-    private JComboBox<Node> phoneModelComboBox;
-    private List<JTreeNode> selectedAnnotationNodes, selectedFidelityNodes;
+    private final List<JTreeNode> selectedAnnotationNodes;
+    private JPanel annotationDecorator;
+    private JComboBox<Node> phoneModelComboBox, fidelityComboBox;
 
     public FilterPanel() {
       super(new VerticalLayout());
       selectedAnnotationNodes = new ArrayList<>();
-      selectedFidelityNodes = new ArrayList<>();
       initComponent();
       addComponents();
     }
 
-    private void addComponents() {
-      add(new JLabel("Choose Phone Model to filter against"));
-      add(phoneModelComboBox);
-      add(new JLabel("Choosing multiple annotation will lead to merging"));
-      add(annotationDecorator);
-      add(Box.createVerticalStrut(10));
-      add(new JLabel("Each fidelity chosen will be shown individually"));
-      add(fidelityDecorator);
-      annotationDecorator.setPreferredSize(new Dimension(150, 200));
-      fidelityDecorator.setPreferredSize(new Dimension(150, 200));
+    private String getPhoneModel() {
+      return ((Node) phoneModelComboBox.getSelectedItem()).getNodeName();
     }
 
-    private void initComponent() {
-      annotationsTree = new JTree(new DefaultMutableTreeNode());
-      annotationsTree.setRootVisible(false);
-      annotationsTree.setCellRenderer(new TreeToolTipRenderer());
-      annotationDecorator = ToolbarDecorator.createDecorator(annotationsTree)
-          .addExtraAction(getAnnotationTreeButton()).createPanel();
-      fidelityTree = new JTree(new DefaultMutableTreeNode());
-      fidelityTree.setRootVisible(false);
-      fidelityTree.setCellRenderer(new TreeToolTipRenderer());
-      fidelityDecorator = ToolbarDecorator.createDecorator(fidelityTree)
-          .addExtraAction(getFidelityTreeButton()).createPanel();
-      phoneModelComboBox = new JComboBox<>(new Vector<>(histogramTree.getAllPhoneModels()));
-      Node selectedItem = (Node) phoneModelComboBox.getSelectedItem();
-      if (selectedItem != null) {
-        setAnnotations(selectedItem);
-      }
-      phoneModelComboBox.addItemListener(event -> {
-        if (event.getStateChange() == ItemEvent.SELECTED) {
-          Node chosenNode = (Node) phoneModelComboBox.getSelectedItem();
-          setAnnotations(chosenNode);
-        }
-      });
+    private String getFidelity() {
+      return ((Node) fidelityComboBox.getSelectedItem()).getNodeName();
+    }
+
+    private List<String> getAnnotations() {
+      return selectedAnnotationNodes.stream().map(JTreeNode::getNodeName)
+          .collect(Collectors.toList());
     }
 
     private void setAnnotations(Node chosenModel) {
@@ -133,29 +123,53 @@ public class MonitoringFilterDialogWrapper extends DialogWrapper {
         rootNode.add(jTreeNode);
       }
       ((DefaultTreeModel) annotationsTree.getModel()).setRoot(rootNode);
-      selectedFidelityNodes.clear();
-      updateFidelityTree();
+      fidelityComboBox.removeAllItems();
+      for (Node node : getAllFidelity()) {
+        fidelityComboBox.addItem(node);
+      }
     }
 
-    private void updateFidelityTree() {
+    private void addComponents() {
+      add(new JLabel("Choose Phone Model to filter against"));
+      add(phoneModelComboBox);
+      add(new JLabel("Choosing multiple annotation will lead to merging"));
+      add(annotationDecorator);
+      add(Box.createVerticalStrut(10));
+      add(new JLabel("Choose a fidelity to render"));
+      add(fidelityComboBox);
+      annotationDecorator.setPreferredSize(new Dimension(150, 200));
+    }
+
+    private void initComponent() {
+      annotationsTree = new JTree(new DefaultMutableTreeNode());
+      annotationsTree.setRootVisible(false);
+      annotationsTree.setCellRenderer(new TreeToolTipRenderer());
+      annotationDecorator = ToolbarDecorator.createDecorator(annotationsTree)
+          .addExtraAction(getAnnotationTreeButton()).createPanel();
+      phoneModelComboBox = new JComboBox<>(new Vector<>(histogramTree.getAllPhoneModels()));
+      fidelityComboBox = new JComboBox<>(new Vector<>(getAllFidelity()));
+      Node selectedItem = (Node) phoneModelComboBox.getSelectedItem();
+      if (selectedItem != null) {
+        setAnnotations(selectedItem);
+      }
+      phoneModelComboBox.addItemListener(event -> {
+        if (event.getStateChange() == ItemEvent.SELECTED) {
+          Node chosenNode = (Node) phoneModelComboBox.getSelectedItem();
+          setAnnotations(chosenNode);
+        }
+      });
+    }
+
+    private Set<Node> getAllFidelity() {
       List<Node> chosenAnnotationNodes = selectedAnnotationNodes.stream()
           .map(jTreeNode -> histogramTree.findNodeByName(jTreeNode.getNodeName())).collect(
               Collectors.toList());
-      Set<Node> fidelityNodes = histogramTree.getAllFidelity(chosenAnnotationNodes);
-      JTreeNode rootNode = new JTreeNode();
-      for (Node fidelityNode : fidelityNodes) {
-        JTreeNode jTreeNode = new JTreeNode(fidelityNode);
-        if (selectedFidelityNodes.contains(jTreeNode)) {
-          jTreeNode.setNodeState(true);
-        }
-        rootNode.add(jTreeNode);
-      }
-      ((DefaultTreeModel) fidelityTree.getModel()).setRoot(rootNode);
+      return histogramTree.getAllFidelity(chosenAnnotationNodes);
     }
 
     private SelectAction getAnnotationTreeButton() {
       return new SelectAction(annotationsTree, () -> annotationsTree.getSelectionPath() != null,
-          lastNode -> {
+          lastNodes -> lastNodes.forEach(lastNode -> {
             Optional<JTreeNode> nodeExist = selectedAnnotationNodes.stream()
                 .filter(node -> node.getNodeName().equals(lastNode.getNodeName())).findFirst();
             if (nodeExist.isPresent()) {
@@ -164,30 +178,17 @@ public class MonitoringFilterDialogWrapper extends DialogWrapper {
               selectedAnnotationNodes.add(lastNode);
             }
             setAnnotations((Node) phoneModelComboBox.getSelectedItem());
-          });
-    }
-
-    private SelectAction getFidelityTreeButton() {
-      return new SelectAction(fidelityTree, () -> fidelityTree.getSelectionPath() != null,
-          lastNode -> {
-            Optional<JTreeNode> nodeExist = selectedFidelityNodes.stream()
-                .filter(node -> node.getNodeName().equals(lastNode.getNodeName())).findFirst();
-            if (nodeExist.isPresent()) {
-              selectedFidelityNodes.remove(nodeExist.get());
-            } else {
-              selectedFidelityNodes.add(lastNode);
-            }
-            updateFidelityTree();
-          });
+          }));
     }
 
     private class SelectAction extends AnActionButton {
 
       private final JTree jTree;
-      private final Consumer<JTreeNode> consumer;
+      private final Consumer<List<JTreeNode>> consumer;
       private final Supplier<Boolean> supplier;
 
-      public SelectAction(JTree jTree, Supplier<Boolean> update, Consumer<JTreeNode> consumer) {
+      public SelectAction(JTree jTree, Supplier<Boolean> update,
+          Consumer<List<JTreeNode>> consumer) {
         super("Toggle", Actions.SetDefault);
         this.jTree = jTree;
         this.supplier = update;
@@ -201,12 +202,13 @@ public class MonitoringFilterDialogWrapper extends DialogWrapper {
 
       @Override
       public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        TreePath selectedPath = jTree.getSelectionPath();
+        TreePath[] selectedPath = jTree.getSelectionPaths();
         if (selectedPath == null) {
           return;
         }
-        JTreeNode lastNode = (JTreeNode) selectedPath.getLastPathComponent();
-        consumer.accept(lastNode);
+        List<JTreeNode> lastNodes = Arrays.stream(jTree.getSelectionPaths())
+            .map(path -> (JTreeNode) path.getLastPathComponent()).collect(Collectors.toList());
+        consumer.accept(lastNodes);
       }
     }
 
