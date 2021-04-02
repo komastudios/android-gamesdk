b6ad48d9e96ad837f6e2788e08597e2983b6b187
Jim Blackler <jimblackler@google.com>
2021-04-02 22:14:09 +0100

    Basic initial tests for memory advice library.
    Test: Manual
    Change-Id: I8c2a6fdbcca1580e9d6a391d4d122a8ea8c46d1f

---
+++ test/memoryadvice/memoryadvice/src/androidTest/java/com/google/android/apps/internal/games/memoryadvice/AdvisorTest.java
@@ -0,0 +1,34 @@
+package com.google.android.apps.internal.games.memoryadvice;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import com.google.android.apps.internal.games.memoryadvice_common.Schemas;
+import com.google.common.collect.ImmutableMap;
+import net.jimblackler.jsonschemafriend.GenerationException;
+import net.jimblackler.jsonschemafriend.Schema;
+import net.jimblackler.jsonschemafriend.SchemaException;
+import net.jimblackler.jsonschemafriend.SchemaStore;
+import net.jimblackler.jsonschemafriend.Validator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class AdvisorTest {
+  private final Validator validator;
+  private final Schema advisorParametersSchema;
+
+  public AdvisorTest() throws GenerationException {
+    SchemaStore schemaStore = new SchemaStore();
+    advisorParametersSchema =
+        schemaStore.loadSchema(Schemas.getSchema("advisorParameters.schema.json"));
+    validator = new Validator();
+  }
+
+  @Test
+  public void intitialize() throws SchemaException {
+    ImmutableMap<String, Object> params = ImmutableMap.of();
+    validator.validate(advisorParametersSchema, params);
+    MemoryAdvisor memoryAdvisor =
+        new MemoryAdvisor(InstrumentationRegistry.getInstrumentation().getTargetContext(), params);
+  }
+}
