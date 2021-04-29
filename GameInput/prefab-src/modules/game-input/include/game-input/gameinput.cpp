4c28f354486e851b28cc1e6fdfc1c79487b9ec66
Florian Rival <florianrival@google.com>
2021-04-29 15:24:29 +0200

    Add first version of GameActivity and GameInput
    * Unfortunately we use a shell script to build the GameActivity/GameInput AAR. This should be replaced by prefabPublishing in Gradle DSL, but we need to update Gradle and even with the latest version it errors with some cryptic Gradle error.
    * These libraries are not automatically built and do not yet have a task in the root build.gradle.
    * We need to add samples too.
    * Finally, GameActivity is embedding GameInput, but seems there is no way to have Prefab AAR to depend on other native libraries in other AARs.
    
    Test: ran build.sh for both libraries and checked their content.
    Change-Id: Ifc461cddfb7b54062c3800572db1c53465c2f60e

---
+++ GameInput/prefab-src/modules/game-input/include/game-input/gameinput.cpp
@@ -0,0 +1,241 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "game-input/gameinput.h"
+
+#include <android/log.h>
+#include <jni.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Convert to a Java structure
+extern "C" jobject GameInputState_ToJava(JNIEnv *env, jclass stateClass,
+                                         const GameInputState *state) {
+  static jmethodID constructor = nullptr;
+  if (constructor == nullptr) {
+    constructor =
+        env->GetMethodID(stateClass, "<init>", "(Ljava/lang/String;IIII)V");
+    if (constructor == nullptr) {
+      __android_log_print(ANDROID_LOG_ERROR, "GameInput",
+                          "Can't find gameinput.State constructor");
+      return nullptr;
+    }
+  }
+  const char *text = state->text_UTF8;
+  if (text == nullptr) {
+    static char empty_string[] = "";
+    text = empty_string;
+  }
+  // Note that this expects 'modified' UTF-8 which is not the same as UTF-8
+  // https://en.wikipedia.org/wiki/UTF-8#Modified_UTF-8
+  jstring jtext = env->NewStringUTF(text);
+  jobject jobj =
+      env->NewObject(stateClass, constructor, jtext, state->selection.start,
+                     state->selection.end, state->composingRegion.start,
+                     state->composingRegion.end);
+  env->DeleteLocalRef(jtext);
+  return jobj;
+}
+
+static struct {
+  jfieldID text;
+  jfieldID selectionStart;
+  jfieldID selectionEnd;
+  jfieldID composingRegionStart;
+  jfieldID composingRegionEnd;
+} gStateClassInfo;
+
+static char *string_copy(const char *c, size_t len) {
+  if (c == nullptr) return nullptr;
+  char *ret = (char *)malloc(len + 1);  // Include null
+  memcpy(ret, c, len);
+  ret[len] = 0;  // Add a null at the end, rather than assuming that the input
+                 // is null-termninated.
+  return ret;
+}
+
+extern "C" GameInputState GameInputState_FromJava(JNIEnv *env,
+                                                  jclass stateClass,
+                                                  jobject textInputEvent) {
+  static bool gStateClassInfoInitialized = false;
+  if (!gStateClassInfoInitialized) {
+    gStateClassInfo.text =
+        env->GetFieldID(stateClass, "text", "Ljava/lang/String;");
+    gStateClassInfo.selectionStart =
+        env->GetFieldID(stateClass, "selectionStart", "I");
+    gStateClassInfo.selectionEnd =
+        env->GetFieldID(stateClass, "selectionEnd", "I");
+    gStateClassInfo.composingRegionStart =
+        env->GetFieldID(stateClass, "composingRegionStart", "I");
+    gStateClassInfo.composingRegionEnd =
+        env->GetFieldID(stateClass, "composingRegionEnd", "I");
+
+    gStateClassInfoInitialized = true;
+  }
+
+  jstring text =
+      (jstring)env->GetObjectField(textInputEvent, gStateClassInfo.text);
+  // Note this is 'modified' UTF-8, not true UTF-8. It has no NULLs in it,
+  // except at the end. It's actually not specified whether the value returned
+  // by GetStringUTFChars includes a null at the end, but it *seems to* on
+  // Android.
+  const char *text_chars = env->GetStringUTFChars(text, NULL);
+  int text_len = env->GetStringUTFLength(
+      text);  // Length in bytes, *not* including the null.
+  int selectionStart =
+      env->GetIntField(textInputEvent, gStateClassInfo.selectionStart);
+  int selectionEnd =
+      env->GetIntField(textInputEvent, gStateClassInfo.selectionEnd);
+  int composingRegionStart =
+      env->GetIntField(textInputEvent, gStateClassInfo.composingRegionStart);
+  int composingRegionEnd =
+      env->GetIntField(textInputEvent, gStateClassInfo.composingRegionEnd);
+  GameInputState state;
+  GameInputState_construct(&state, text_chars, text_len, selectionStart,
+                           selectionEnd, composingRegionStart,
+                           composingRegionEnd);
+  env->ReleaseStringUTFChars(text, text_chars);
+  env->DeleteLocalRef(text);
+
+  return state;
+}
+
+extern "C" void GameInputState_constructEmpty(GameInputState *s) { *s = {}; }
+extern "C" void GameInputState_construct(GameInputState *s, const char *t,
+                                         int64_t l, int s0, int s1, int c0,
+                                         int c1) {
+  s->text_UTF8 = string_copy(t, l);
+  s->text_length = l;
+  s->selection = {s0, s1};
+  s->composingRegion = {c0, c1};
+  s->dealloc = free;
+}
+extern "C" void GameInputState_destruct(GameInputState *s) {
+  if (s->text_UTF8 && s->dealloc) s->dealloc((void *)s->text_UTF8);
+}
+extern "C" void GameInputState_set(GameInputState *s,
+                                   const GameInputState *rhs) {
+  GameInputState_destruct(s);
+  GameInputState_construct(
+      s, rhs->text_UTF8, rhs->text_length, rhs->selection.start,
+      rhs->selection.end, rhs->composingRegion.start, rhs->composingRegion.end);
+}
+
+struct GameInput {
+  JNIEnv *env;
+  // Needed because we can't find the class on a non-Java thread
+  jclass gameInputStateClass;
+  // The latest text input update
+  GameInputState gameInputState;
+  // An instance of gameinput.InputConnection
+  jclass inputConnectionClass;
+  jobject inputConnection;
+  jmethodID inputConnectionSetStateMethod;
+  jmethodID setSoftKeyboardActiveMethod;
+
+  void (*eventCallback)(void *, const struct GameInputState *);
+
+  void *eventCallbackContext;
+};
+
+// TODO: this shouldn't be static
+GameInput s_gameInput;
+
+extern "C" {
+
+struct GameInput *GameInput_init(JNIEnv *env) {
+  s_gameInput.env = env;
+  GameInputState_constructEmpty(&s_gameInput.gameInputState);
+  s_gameInput.gameInputStateClass = (jclass)env->NewGlobalRef(
+      env->FindClass("com/google/androidgamesdk/gameinput/State"));
+  s_gameInput.inputConnectionClass = (jclass)env->NewGlobalRef(
+      env->FindClass("com/google/androidgamesdk/gameinput/InputConnection"));
+  s_gameInput.inputConnectionSetStateMethod =
+      env->GetMethodID(s_gameInput.inputConnectionClass, "setState",
+                       "(Lcom/google/androidgamesdk/gameinput/State;)V");
+  s_gameInput.inputConnection = nullptr;
+  s_gameInput.eventCallback = nullptr;
+  s_gameInput.setSoftKeyboardActiveMethod = env->GetMethodID(
+      s_gameInput.inputConnectionClass, "setSoftKeyboardActive", "(ZI)V");
+  return &s_gameInput;
+}
+
+void GameInput_destroy(GameInput *input) {
+  if (input == nullptr) return;
+  if (input->gameInputStateClass != NULL) {
+    input->env->DeleteGlobalRef(input->gameInputStateClass);
+    input->gameInputStateClass = NULL;
+  }
+  if (input->inputConnectionClass != NULL) {
+    input->env->DeleteGlobalRef(input->inputConnectionClass);
+    input->inputConnectionClass = NULL;
+  }
+  if (input->inputConnection != NULL) {
+    input->env->DeleteGlobalRef(input->inputConnection);
+    input->inputConnection = NULL;
+  }
+  GameInputState_destruct(&input->gameInputState);
+}
+
+void GameInput_setState(GameInput *input, const GameInputState *state) {
+  if (input->inputConnection == nullptr) return;
+  jobject jstate =
+      GameInputState_ToJava(input->env, input->gameInputStateClass, state);
+  input->env->CallVoidMethod(input->inputConnection,
+                             input->inputConnectionSetStateMethod, jstate);
+  input->env->DeleteLocalRef(jstate);
+  GameInputState_set(&input->gameInputState, state);
+}
+
+const GameInputState *GameInput_getState(GameInput *input) {
+  return &input->gameInputState;
+}
+
+void GameInput_setInputConnection(GameInput *input, jobject inputConnection) {
+  if (input->inputConnection != NULL) {
+    input->env->DeleteGlobalRef(input->inputConnection);
+  }
+  input->inputConnection = input->env->NewGlobalRef(inputConnection);
+}
+
+void GameInput_processEvent(GameInput *input, jobject textInputEvent) {
+  GameInputState_destruct(&input->gameInputState);
+  input->gameInputState = GameInputState_FromJava(
+      input->env, input->gameInputStateClass, textInputEvent);
+  if (input->eventCallback) {
+    input->eventCallback(input->eventCallbackContext, &input->gameInputState);
+  }
+}
+
+void GameInput_showIme(struct GameInput *input, int32_t flags) {
+  if (input->inputConnection == nullptr) return;
+  input->env->CallVoidMethod(input->inputConnection,
+                             input->setSoftKeyboardActiveMethod, true, flags);
+}
+
+void GameInput_hideIme(struct GameInput *input, int32_t flags) {
+  if (input->inputConnection == nullptr) return;
+  input->env->CallVoidMethod(input->inputConnection,
+                             input->setSoftKeyboardActiveMethod, false, flags);
+}
+
+void GameInput_setEventCallback(struct GameInput *input,
+                                void (*callback)(void *,
+                                                 const struct GameInputState *),
+                                void *context) {
+  input->eventCallback = callback;
+  input->eventCallbackContext = context;
+}
+}
---
+++ GameInput/prefab-src/modules/game-input/include/game-input/gameinput.h
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file gameinput.h
+ */
+
+#pragma once
+
+#include <jni.h>
+
+/**
+ * This struct holds a span within a region of text from start (inclusive) to
+ * end (exclusive). An empty span or cursor position is specified with
+ * start==end. An undefined span is specified with start = end = SPAN_UNDEFINED.
+ */
+typedef struct GameInputSpan {
+  int start;
+  int end;
+} GameInputSpan;
+
+/**
+ * Values with special meaning in a GameInputSpan.
+ */
+enum { SPAN_UNDEFINED = -1 };
+
+/**
+ * This struct holds the state of an editable section of text.
+ * The text can have a selection and a composing region defined on it.
+ * A composing region is used by IMEs that allow input using multiple steps to
+ * compose a glyph or word. Use functions GameInput_getState and
+ * GameInput_setState to read and modify the state that an IME is editing.
+ */
+typedef struct GameInputState {
+  /**
+   * Text owned by the state, as a modified UTF-8 string. Null-terminated.
+   * https://en.wikipedia.org/wiki/UTF-8#Modified_UTF-8
+   */
+  const char *text_UTF8;
+  /**
+   * Length in bytes of text_UTF8, *not* including the null at end.
+   */
+  int64_t text_length;
+  /**
+   * A selection defined on the text.
+   */
+  GameInputSpan selection;
+  /**
+   * A composing region defined on the text.
+   */
+  GameInputSpan composingRegion;
+  /**
+   * Deallocator for text_UTF8 string
+   */
+  void (*dealloc)(void *);
+} GameInputState;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Fill in the state with empty values.
+ * @param state A struct owned by the caller.
+ */
+void GameInputState_constructEmpty(GameInputState *state);
+
+/**
+ * Fill in the state with the values passed-in. Ownership of the text is
+ * maintained by the caller.
+ * @param state A struct owned by the caller.
+ * @param text_UTF8 A modified UTF8 character array owned by the caller.
+ * @param text_length The length of text_UTF8.
+ * @param selection_start
+ * @param selection_end
+ * @param composing_region_start
+ * @param composing_region_end
+ */
+void GameInputState_construct(GameInputState *state, const char *text_UTF8,
+                              int64_t text_length, int selection_start,
+                              int selection_end, int composing_region_start,
+                              int composing_region_end);
+
+/**
+ * Free any memory owned by the state.
+ * @param state A struct owned by the caller.
+ */
+void GameInputState_destruct(GameInputState *state);
+
+/**
+ * Copy from rhs to state. Ownership of rhs and its internals is maintained by
+ * the caller.
+ * @param state
+ * @param rhs
+ */
+void GameInputState_set(GameInputState *state, const GameInputState *rhs);
+
+// Convert to a Java object. Don't forget to delete the local ref when you're
+// done.
+/**
+ * Convert a GameInputState struct to a Java gameinput.State object.
+ * @param env A JNI env valid on the calling thread.
+ * @param stateClass A Java class obtained using
+ * FindClass("com/google/androidgamesdk/gameinput/State") or equivalent.
+ * @param state Input state to convert.
+ * @return A Java object of class gameinput.State.
+ */
+jobject GameInputState_ToJava(JNIEnv *env, jclass stateClass,
+                              const GameInputState *state);
+
+/**
+ * Convert from a Java gameinput.State object into a C GameInputState struct.
+ * You are responsible for calling GameInputState_destruct on the returned
+ * value.
+ * @param env A JNI env valid on the calling thread.
+ * @param stateClass A Java class obtained using
+ * FindClass("com/google/androidgamesdk/gameinput/State") or equivalent.
+ * @param state A Java gameinput.State object.
+ * @return The converted GameInputState struct, now owned by the caller.
+ */
+GameInputState GameInputState_FromJava(JNIEnv *env, jclass stateClass,
+                                       jobject state);
+
+/**
+ * Opaque handle to the GameInput API.
+ */
+typedef struct GameInput GameInput;
+
+/**
+ * Initialize the GameInput library.
+ * @param env A JNI env valid on the calling thread.
+ * @return A handle to the library.
+ */
+GameInput *GameInput_init(JNIEnv *env);
+
+/**
+ * Free any resources owned by the GameInput library.
+ * @param input A valid GameInput library handle.
+ */
+void GameInput_destroy(GameInput *input);
+
+/**
+ * Show the IME. Calls InputMethodManager.showSoftInput().
+ * @param input A valid GameInput library handle.
+ * @param flags See
+ * https://developer.android.com/reference/android/view/inputmethod/InputMethodManager
+ */
+void GameInput_showIme(GameInput *input, int32_t flags);
+/**
+ * Show the IME. Calls InputMethodManager.hideSoftInputFromWindow().
+ * @param input A valid GameInput library handle.
+ * @param flags See
+ * https://developer.android.com/reference/android/view/inputmethod/InputMethodManager
+ */
+void GameInput_hideIme(GameInput *input, int32_t flags);
+
+/**
+ * Get the current GameInput state. The state is modified by changes in the IME
+ * and calls to GameInput_setState.
+ * @param input A valid GameInput library handle.
+ * @return The current maintained GameInputState or NULL if there is none.
+ */
+const GameInputState *GameInput_getState(GameInput *input);
+
+/**
+ * Set the current GameInput state. This state is reflected to any active IME.
+ * @param input A valid GameInput library handle.
+ * @param state The state to set. Ownership is maintained by the caller.
+ */
+void GameInput_setState(GameInput *input, const GameInputState *state);
+
+/**
+ * You must call this to tell the C interface about your Java
+ * gameinput.InputConnection, unless you are using GameActivity.
+ * @param input A valid GameInput library handle.
+ * @param inputConnection A gameinput.InputConnection object.
+ */
+void GameInput_setInputConnection(GameInput *input, jobject inputConnection);
+
+/**
+ * Optionally set a callback to be called whenever the IME changes the state.
+ * Not necessary if you are using GameActivity.
+ * @param input A valid GameInput library handle.
+ * @param callback The callback to call.
+ * @param context Context passed as first argument to the callback.
+ */
+void GameInput_setEventCallback(GameInput *input,
+                                void (*callback)(void *,
+                                                 const GameInputState *),
+                                void *context);
+
+// TODO (willosborn): can we avoid this by registering a native method with
+// InputConnection directly?
+/**
+ * Call this function from your Java gameinput.Listener.stateChanged method to
+ * automatically convert eventState and trigger the callback defined above. Not
+ * necessary if you are using GameActivity.
+ * @param input A valid GameInput library handle.
+ * @param eventState A Java gameinput.State object.
+ */
+void GameInput_processEvent(GameInput *input, jobject eventState);
+
+#ifdef __cplusplus
+}
+#endif
