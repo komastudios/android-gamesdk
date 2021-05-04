692bd45fdf45d007102975189f2dcd418b3714a0
Jim Blackler <jimblackler@google.com>
2021-05-04 10:56:52 +0100

    Unify PROC_REGEX and MEMINFO_REGEX and associated collection methods.
    In addition, collect results with a callback rather than a hash map; this is faster.
    
    Bug: 187147763
    Test: testProcessMemFormatFile
    Change-Id: I7a261382106dbe8a83bd9a46843d6bb73486a07b

--- test/memoryadvice/memoryadvice/src/main/java/com/google/android/apps/internal/games/memoryadvice/Utils.java
+++ test/memoryadvice/memoryadvice/src/main/java/com/google/android/apps/internal/games/memoryadvice/Utils.java
@@ -4,8 +4,6 @@ import android.util.Log;
 import com.google.android.apps.internal.games.memoryadvice_common.StreamUtils;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,8 +13,7 @@ import java.util.regex.Pattern;
  */
 public class Utils {
   private static final String TAG = Utils.class.getSimpleName();
-  private static final Pattern MEMINFO_REGEX = Pattern.compile("([^:]+)[^\\d]*(\\d+).*\n");
-  private static final Pattern PROC_REGEX = Pattern.compile("([a-zA-Z]+)[^\\d]*(\\d+) kB.*\n");
+  private static final Pattern MEMORY_REGEX = Pattern.compile("([a-zA-Z()_]+)[^\\d]*(\\d+) kB.*\n");
 
   /**
    * Loads all text from the specified file and returns the result as a string.
@@ -44,48 +41,28 @@ public class Utils {
   }
 
   /**
-   * Returns a dictionary of values extracted from the /proc/meminfo file.
+   * Extracts values from a Linux memory summary format file, converting kB values to bytes.
    *
-   * @return A dictionary of values, in bytes.
+   * @param filename The file to read.
+   * @param client The client to receive values.
    */
-  static Map<String, Long> processMeminfo() {
-    Map<String, Long> output = new LinkedHashMap<>();
-
-    String filename = "/proc/meminfo";
+  static void processMemFormatFile(String filename, MemFormatClient client) {
     try {
       String meminfoText = readFile(filename);
-      Matcher matcher = MEMINFO_REGEX.matcher(meminfoText);
+      Matcher matcher = MEMORY_REGEX.matcher(meminfoText);
       while (matcher.find()) {
-        output.put(
+        client.receive(
             matcher.group(1), Long.parseLong(Objects.requireNonNull(matcher.group(2))) * 1024);
       }
     } catch (IOException e) {
       Log.w(TAG, "Failed to read " + filename);
     }
-    return output;
   }
 
   /**
-   * Return a dictionary of values extracted from a processes' /proc/../status
-   * files.
-   *
-   * @param pid The process ID (pid).
-   * @return A dictionary of values, in bytes.
+   * A receiver of Linux memory file data.
    */
-  static Map<String, Long> processStatus(int pid) {
-    Map<String, Long> output = new LinkedHashMap<>();
-    String filename = "/proc/" + pid + "/status";
-    try {
-      String meminfoText = readFile(filename);
-      Matcher matcher = PROC_REGEX.matcher(meminfoText);
-      while (matcher.find()) {
-        String key = matcher.group(1);
-        long value = Long.parseLong(Objects.requireNonNull(matcher.group(2)));
-        output.put(key, value * 1024);
-      }
-    } catch (IOException e) {
-      Log.w(TAG, "Failed to read " + filename);
-    }
-    return output;
+  interface MemFormatClient {
+    void receive(String key, long bytes);
   }
 }
