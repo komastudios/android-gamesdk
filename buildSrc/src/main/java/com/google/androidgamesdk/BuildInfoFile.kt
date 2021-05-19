1f8c42d2e10fb7ee29183ced81a6038af83c00f4
Florian Rival <florianrival@google.com>
2021-05-19 13:30:39 +0100

    Add GameActivity/GameInput AARs to the build
    This adds support for building GameActivity/GameInput as part of the Game SDK build. Notably:
    - This adds a notion of "Android Library" (to oppose to "Native Library"), which is always built as an AAR (but can have header only C or C++ sources).
    - These libraries can be part of the zip (packageZip) distribution or have a zip file created for upload to Maven (packageMavenZip)
    
    Note that when we are able to switch to prefabPublishing, the AarPrefabPatcher can be entirely removed.
    
    Test: ./gradlew packageLocalZip --Plibraries=game_activity,game_input,swappy
    Test: ./gradlew packageMavenZip --Plibraries=game_activity,game_input
    Test: verified that presubmits are building the AARs
    
    Change-Id: I0cd45a5e25c7fb371396a2e634d1070d982b3f62

--- buildSrc/src/main/java/com/google/androidgamesdk/BuildInfoFile.kt
+++ buildSrc/src/main/java/com/google/androidgamesdk/BuildInfoFile.kt
@@ -47,22 +47,34 @@ class BuildInfoFile {
     }
 
     fun writeLibrariesBuildInfoFile(
-        libraries: Collection<NativeLibrary>,
+        nativeLibraries: Collection<NativeLibrary>,
+        androidLibraries: Collection<AndroidLibrary>,
         distPath: String,
         packageName: String
     ) {
         val headCommitSha = getGitCommitShaAtHead()
 
-        val artifactsBuildInfo: List<ArtifactBuildInfo> = libraries.map {
+        val artifactsBuildInfo: List<ArtifactBuildInfo> = nativeLibraries.map {
             nativeLibrary: NativeLibrary ->
             ArtifactBuildInfo(
                 groupId = "androidx.games",
                 artifactId = nativeLibrary.aarLibraryName,
                 path = "/src/" + nativeLibrary.nativeLibraryName,
                 projectZipPath = packageName + '/' +
-                        nativeLibrary.aarLibraryName + "-maven-zip.zip",
+                    nativeLibrary.aarLibraryName + "-maven-zip.zip",
                 sha = headCommitSha,
                 version = nativeLibrary.aarVersion
+            )
+        } + androidLibraries.map {
+            androidLibrary: AndroidLibrary ->
+            ArtifactBuildInfo(
+                groupId = "androidx.games",
+                artifactId = androidLibrary.aarLibraryName,
+                path = androidLibrary.projectName,
+                projectZipPath = packageName + '/' +
+                    androidLibrary.aarLibraryName + "-maven-zip.zip",
+                sha = headCommitSha,
+                version = androidLibrary.aarVersion
             )
         }
 
