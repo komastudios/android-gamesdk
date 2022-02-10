a9e595870adf0bc6889fbbb692fe3c4a74e370d3
Will Osborn <willosborn@google.com>
2022-02-10 13:24:33 +0000

    MemoryAdvice: Update the hogger app
    Test: build and run the app
    Bug: 218832935
    Change-Id: I68c3c6437bded69a839bc7e73a37ef8d8e0e1a53

--- samples/memory_advice/hogger/app/src/main/cpp/native-lib.cpp
+++ samples/memory_advice/hogger/app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,8 @@
 #include <jni.h>
 #include <string>
 #include <sstream>
+#include <list>
+#include <mutex>
 
 #include <memory_advice/memory_advice.h>
 
@@ -12,11 +14,17 @@ void callback(MemoryAdvice_MemoryState state) {
     MemoryAdvice_removeWatcher();
 }
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_memory_1advice_hogger_MainActivity_getAdvice(
+extern "C" JNIEXPORT jint JNICALL
+Java_com_memory_1advice_hogger_MainActivity_initMemoryAdvice(
         JNIEnv* env,
         jobject activity) {
-    MemoryAdvice_initDefaultParams(env, activity);
+    return MemoryAdvice_initDefaultParams(env, activity);
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_memory_1advice_hogger_MainActivity_getMemoryAdvice(
+        JNIEnv* env,
+        jobject activity) {
     MemoryAdvice_JsonSerialization advice;
     MemoryAdvice_getAdvice(&advice);
     ALOGE("Advice is: %s", advice.json);
@@ -24,4 +32,41 @@ Java_com_memory_1advice_hogger_MainActivity_getAdvice(
     MemoryAdvice_JsonSerialization_free(&advice);
     MemoryAdvice_setWatcher(1000, callback);
     return ret;
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_memory_1advice_hogger_MainActivity_getMemoryState(
+        JNIEnv* env,
+        jobject activity) {
+    MemoryAdvice_MemoryState state = MEMORYADVICE_STATE_UNKNOWN;
+    MemoryAdvice_getMemoryState(&state);
+    return state;
+}
+
+static std::list<std::unique_ptr<char[]>> allocated_bytes_list;
+static std::mutex allocated_mutex;
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_memory_1advice_hogger_MainActivity_allocate(
+        JNIEnv* env,
+        jobject activity,
+        jlong bytes) {
+    std::lock_guard<std::mutex> guard(allocated_mutex);
+    auto allocated_bytes = new char[bytes];
+    // Note that without setting the memory, the available memory figure doesn't go down.
+    memset(allocated_bytes, 0, bytes);
+    allocated_bytes_list.push_back(std::unique_ptr<char[]> {allocated_bytes });
+}
+
+extern "C" JNIEXPORT jboolean JNICALL
+Java_com_memory_1advice_hogger_MainActivity_deallocate(
+        JNIEnv* env,
+        jobject activity) {
+    std::lock_guard<std::mutex> guard(allocated_mutex);
+    if (allocated_bytes_list.size()>0) {
+        allocated_bytes_list.pop_back();
+        return true;
+    } else {
+        return false;
+    }
 }
--- samples/memory_advice/hogger/app/src/main/java/com/memory_advice/hogger/MainActivity.java
+++ samples/memory_advice/hogger/app/src/main/java/com/memory_advice/hogger/MainActivity.java
@@ -3,7 +3,10 @@ package com.memory_advice.hogger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +16,100 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private final class CheckMemoryAdviceTask extends TimerTask {
+        @Override public void run() {
+            // Post a task to update the memory on the UI thread.
+            getWindow().getDecorView().post(() -> {
+                    showMemoryAdvice(getMemoryAdvice(), getMemoryState());
+                });
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(getAdvice());
+        int init_retcode = initMemoryAdvice();
+        if (init_retcode!=0) {
+            showMemoryAdvice(String.format("Error initializing memory_advice: %d", init_retcode),
+                    0);
+            return;
+        }
+
+        showMemoryAdvice(getMemoryAdvice(), getMemoryState());
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new CheckMemoryAdviceTask(), 0, 1000);
+
+        findViewById(R.id.allocate_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onAllocate();
+            }
+        });
+        findViewById(R.id.deallocate_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onDeallocate();
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String getAdvice();
+    static final long BYTES_TO_ADD = 100000000; // 100MB
+    long memory_allocated;
+
+    public void onAllocate() {
+        new Thread(() ->  {
+                allocate(BYTES_TO_ADD);
+                memory_allocated += BYTES_TO_ADD;
+                updateMemoryAllocated();
+            }).start();
+    }
+
+    public void onDeallocate() {
+        new Thread(() -> {
+            if (deallocate()) {
+                memory_allocated -= BYTES_TO_ADD;
+                updateMemoryAllocated();
+            }
+        }).start();
+    }
+
+    private void updateMemoryAllocated() {
+        getWindow().getDecorView().post(() -> {
+            showMemoryAllocated();
+        });
+    }
+
+    private void showMemoryAdvice(String advice, int state) {
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(advice);
+        String stateString = "UNKNOWN";
+        switch(state) {
+            case 1:
+                stateString = "OK";
+                break;
+            case 2:
+                stateString = "APPROACHING_LIMIT";
+                break;
+            case 3:
+                stateString = "CRITICAL";
+                break;
+        }
+        TextView state_tv = findViewById(R.id.state_textview);
+        state_tv.setText("Memory State: " + stateString);
+    }
+
+    private void showMemoryAllocated() {
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.allocated_textview);
+        tv.setText(String.format("%d MB", memory_allocated/1000000));
+    }
+
+    public native int initMemoryAdvice();
+    public native String getMemoryAdvice();
+    public native int getMemoryState();
+    public native void allocate(long bytes);
+    public native boolean deallocate();
+
 }
