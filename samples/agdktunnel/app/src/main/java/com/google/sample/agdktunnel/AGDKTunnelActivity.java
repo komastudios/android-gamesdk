ea07662862031d29236bb0f1bde2d5aa3d2fa6b6
ruvalcabac <ruvalcabac@google.com>
2022-03-08 15:20:04 -0600

    Add PGS v2 cloud save to AGDK Tunnel
    Test: build and run in mobile and Google Play Games
    Bug: 221216698
    Change-Id: Iaea4ff74c07bd9075ab71e1fd580455f95f0d7e8

--- samples/agdktunnel/app/src/main/java/com/google/sample/agdktunnel/AGDKTunnelActivity.java
+++ samples/agdktunnel/app/src/main/java/com/google/sample/agdktunnel/AGDKTunnelActivity.java
@@ -18,7 +18,6 @@ package com.google.sample.agdktunnel;
 import static android.view.inputmethod.EditorInfo.IME_ACTION_NONE;
 import static android.view.inputmethod.EditorInfo.IME_FLAG_NO_FULLSCREEN;
 
-import android.content.pm.PackageManager;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
@@ -33,6 +32,8 @@ import androidx.core.view.WindowInsetsControllerCompat;
 import com.google.androidgamesdk.GameActivity;
 
 public class AGDKTunnelActivity extends GameActivity {
+
+    private PGSManager mPGSManager;
 
     // Some code to load our native library:
     static {
@@ -53,6 +54,35 @@ public class AGDKTunnelActivity extends GameActivity {
         System.loadLibrary("game");
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // When true, the app will fit inside any system UI windows.
+        // When false, we render behind any system UI windows.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        hideSystemUI();
+        // You can set IME fields here or in native code using GameActivity_setImeEditorInfoFields.
+        // We set the fields in native_engine.cpp.
+        // super.setImeEditorInfoFields(InputType.TYPE_CLASS_TEXT,
+        //     IME_ACTION_NONE, IME_FLAG_NO_FULLSCREEN );
+        super.onCreate(savedInstanceState);
+
+        if (isPlayGamesServicesLinked()) {
+            // Initialize Play Games Services
+            mPGSManager = new PGSManager(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // To learn best practices to handle lifecycle events visit
+        // https://developer.android.com/topic/libraries/architecture/lifecycle
+        if (isPlayGamesServicesLinked()) {
+            mPGSManager.onResume();
+        }
+    }
+
     private void hideSystemUI() {
         // This will put the game behind any cutouts and waterfalls on devices which have
         // them, so the corresponding insets will be non-zero.
@@ -71,35 +101,24 @@ public class AGDKTunnelActivity extends GameActivity {
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // When true, the app will fit inside any system UI windows.
-        // When false, we render behind any system UI windows.
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        hideSystemUI();
-        // You can set IME fields here or in native code using GameActivity_setImeEditorInfoFields.
-        // We set the fields in native_engine.cpp.
-        // super.setImeEditorInfoFields(InputType.TYPE_CLASS_TEXT,
-        //     IME_ACTION_NONE, IME_FLAG_NO_FULLSCREEN );
-        super.onCreate(savedInstanceState);
-
-        if (isPlayGamesServicesLinked()) {
-            // Initialize Play Games Services
-            PGSManager.getInstance(this);
-        }
-    }
-
-    public boolean isGooglePlayGames() {
-        PackageManager pm = getPackageManager();
-        return pm.hasSystemFeature("com.google.android.play.feature.HPE_EXPERIENCE");
-    }
-
-    public String getInternalStoragePath() {
-        return getFilesDir().getAbsolutePath();
-    }
-
     private boolean isPlayGamesServicesLinked() {
         String playGamesServicesPlaceholder = "0000000000";
         return !getString(R.string.game_services_project_id).equals(playGamesServicesPlaceholder);
+    }
+
+    private void loadCloudCheckpoint() {
+        if (isPlayGamesServicesLinked()) {
+            mPGSManager.loadCheckpoint();
+        }
+    }
+
+    private void saveCloudCheckpoint(int level) {
+        if (isPlayGamesServicesLinked()) {
+            mPGSManager.saveCheckpoint(level);
+        }
+    }
+
+    private String getInternalStoragePath() {
+        return getFilesDir().getAbsolutePath();
     }
 }
