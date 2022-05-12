a8902ba9853830b75328c6037d7409521ae8b1b0
Barış Kaya <bkaya@google.com>
2022-05-12 14:59:18 +0100

    Fix memory advice presubmits
    The memory advice library depended in tensorflow which depended on a bunch of libraries
    which where being downloaded through git. Instead, modify tensorflow so that the
    dependencies are pulled from external/ instead.
    
    Bug: b/230615289
    Test: Built the library and ran the hogger app
    Change-Id: I37053f20cf9d240903414220e23be51977b4577a

--- buildSrc/src/main/java/com/google/androidgamesdk/TensorflowPatcher.kt
+++ buildSrc/src/main/java/com/google/androidgamesdk/TensorflowPatcher.kt
@@ -7,12 +7,15 @@ import java.io.FileOutputStream
 import java.net.URL
 
 /**
- * Patches the Tensorflow library to work with the Game SDK.
+ * Patches the Tensorflow library and its dependencies to work with the Game SDK.
  */
 class TensorflowPatcher(val project: Project) {
     fun patch() {
         patchAbseilCpp()
         patchTensorflow()
+        patchCmakeDependencies()
+        patchXnnpack()
+        patchFarmhash()
     }
 
     /**
@@ -78,4 +81,106 @@ class TensorflowPatcher(val project: Project) {
         );
         serialization.writeText(serializationContent);
     }
+
+    /**
+     * Tensorflow depends on github downloads to populate its dependencies.
+     *  Override the github downloader to use libraries from external instead.
+     */
+    private fun patchCmakeDependencies() {
+
+        val overridableFetchContent = project.file(
+            "../external/tensorflow/tensorflow/lite/" +
+                "tools/cmake/modules/OverridableFetchContent.cmake");
+        val externalDependencies = project.file(
+            "src/memory_advice/ExternalDependencies.cmake");
+        overridableFetchContent.writeText(externalDependencies.readText());
+
+        val farmhashCMake = project.file(
+            "../external/tensorflow/tensorflow/lite/" +
+                "tools/cmake/modules/farmhash/CMakeLists.txt");
+        var farmhashCMakeContent = farmhashCMake.readText()
+        farmhashCMakeContent = farmhashCMakeContent.replace("/src", "");
+        farmhashCMake.writeText(farmhashCMakeContent);
+
+        val farmhashDownloader = project.file(
+            "../external/tensorflow/tensorflow/lite/" +
+                "tools/cmake/modules/farmhash.cmake");
+        var farmhashDownloaderContent = farmhashDownloader.readText()
+        farmhashDownloaderContent = farmhashDownloaderContent.replace(
+            ")\n" +
+                "\n" +
+                "add_subdirectory(",
+            ")\n" +
+                "set (farmhash_SOURCE_DIR \"\${EXTERNAL_SOURCE_DIR}/" +
+                "libtextclassifier/native/utils/hash\")\n" +
+                "set (FARMHASH_SOURCE_DIR \"\${farmhash_SOURCE_DIR}\")\n" +
+                "add_subdirectory(");
+        farmhashDownloader.writeText(farmhashDownloaderContent);
+
+        val fft2dDownloader = project.file(
+            "../external/tensorflow/tensorflow/lite/" +
+                "tools/cmake/modules/fft2d.cmake");
+        var fft2dDownloaderContent = fft2dDownloader.readText()
+        fft2dDownloaderContent = fft2dDownloaderContent.replace(
+            "source\")\n" +
+                "add_subdirectory(",
+            "source\")\n" +
+                "set(fft2d_SOURCE_DIR " +
+                "\"\${fft2d_SOURCE_DIR}/src/fft2d/fft2d\")\n" +
+                "set(FFT2D_SOURCE_DIR \"\${fft2d_SOURCE_DIR}\")\n" +
+                "add_subdirectory(");
+        fft2dDownloader.writeText(fft2dDownloaderContent);
+
+        val flatbuffersCMake = project.file(
+            "../external/tensorflow/tensorflow/lite/" +
+                "tools/cmake/modules/flatbuffers.cmake");
+        var flatbuffersCMakeContent = flatbuffersCMake.readText()
+        flatbuffersCMakeContent = flatbuffersCMakeContent.replace(
+            "include(ExternalProject)\n" +
+                "\n" +
+                "ExternalProject_Add(flatbuffers-flatc\n" +
+                "  PREFIX \${CMAKE_BINARY_DIR}/flatbuffers-flatc\n" +
+                "  SOURCE_DIR \${CMAKE_BINARY_DIR}/flatbuffers\n" +
+                "  CMAKE_ARGS -DCMAKE_CXX_FLAGS=\"-DNOMINMAX=1\"\n" +
+                "             -DFLATBUFFERS_BUILD_TESTS=OFF\n" +
+                "             -DFLATBUFFERS_BUILD_FLATLIB=OFF\n" +
+                "             -DFLATBUFFERS_STATIC_FLATC=ON\n" +
+                "             -DFLATBUFFERS_BUILD_FLATHASH=OFF\n" +
+                "             -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>\n" +
+                "  EXCLUDE_FROM_ALL 1\n" +
+                ")",
+            "");
+        flatbuffersCMake.writeText(flatbuffersCMakeContent);
+    }
+
+    /**
+    * The xnnpack library on platform/external is updated; fix its cmake file.
+    */
+    private fun patchXnnpack() {
+        val xnnpackCmake = project.file("../external/XNNPACK/CMakeLists.txt");
+        var xnnpackCmakeContent = xnnpackCmake.readText()
+        xnnpackCmakeContent = xnnpackCmakeContent.replace(
+            "\n" +
+                "  src/f16-maxpool/9p8x-minmax-f16c-c8.c\n" +
+                "  src/f16-prelu/gen/neonfp16arith-2x16.c\n" +
+                "  src/f16-vbinary/gen/vadd-minmax-f16c-x16.c",
+            "\n" +
+                "  src/f16-maxpool/9p8x-minmax-f16c-c8.c\n" +
+                "  src/f16-vbinary/gen/vadd-minmax-f16c-x16.c");
+        xnnpackCmake.writeText(xnnpackCmakeContent);
+    }
+
+    /**
+    * The farmhash library's structure doesn't match the expected structure
+    * from tensorflow, fix the headers
+    */
+    private fun patchFarmhash() {
+        val farmhash = project.file(
+            "../external/libtextclassifier/native/utils/hash/farmhash.cc");
+        var farmhashContent = farmhash.readText()
+        farmhashContent = farmhashContent.replace(
+            "#include \"utils/hash/farmhash.h\"",
+            "#include \"farmhash.h\"");
+        farmhash.writeText(farmhashContent);
+  }
 }
