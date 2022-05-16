4168cb76b6b3bc0d452272bcc718d0e578154d21
gfan <gfan@google.com>
2022-05-16 12:33:15 -0700

    Cosmetics update for sample library names.
    Change-Id: I8e5e7e4da68d868bd9eb25dbe619ad6649c3a8da

--- samples/agdktunnel/app/src/main/java/com/google/sample/agdktunnel/AGDKTunnelActivity.java
+++ samples/agdktunnel/app/src/main/java/com/google/sample/agdktunnel/AGDKTunnelActivity.java
@@ -51,7 +51,7 @@ public class AGDKTunnelActivity extends GameActivity {
         // It's not actually necessary to load the native 'game' library here as it's loaded in
         // GameActivity.onCreate when we have the following line in the manifest:
         // <meta-data android:name="android.app.lib_name" android:value="game" />
-        System.loadLibrary("game");
+        System.loadLibrary("agdktunnel");
     }
 
     @Override
