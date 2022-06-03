8b0d9268810987b951cbc9ba92ed8c76e58dc374
gfan <gfan@google.com>
2022-06-03 15:45:28 -0700

    Update native library names for the internal test apps. Test: build & run on Pixel 3A.
    Change-Id: Id69099e48063d037392d84d56391be92de69a980

--- test/grabber/app/src/main/java/com/google/gamesdk/grabber/MainActivity.java
+++ test/grabber/app/src/main/java/com/google/gamesdk/grabber/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,9 +23,16 @@ public class MainActivity extends Activity {
     Context context = this;
     button.setOnClickListener(
         v -> {
+          Integer memSize;    // memory size in mb.
+          try {
+              memSize = Integer.valueOf(editText.getText().toString());
+          } catch (NumberFormatException e) {
+              Log.w("Grabber", "invalid memory size: " + e);
+              return;
+          }
           Intent intent = new Intent(context, MemoryPressureService.class);
           intent.putExtra(ACTION_TYPE, ALLOCATE_MEMORY_ACTION);
-          intent.putExtra(TOTAL_MEMORY_MB, Integer.valueOf(editText.getText().toString()));
+          intent.putExtra(TOTAL_MEMORY_MB, memSize);
           context.startService(intent);
         });
   }
