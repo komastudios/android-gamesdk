6ebfaa471791fcb5dcc79c723dcc7ffb72edb5e0
Tom Murphy <tomnom@google.com>
2022-06-22 14:14:14 +0000

    Remove unused ndk/api versions
    The full build is failing because the generated zip file is bigger than
    4GB. Remove unused ndk/api versions to decrease build size
    
    Test: ran build.sh full
    Bug: 236393056
    Change-Id: I95e39c23dc77fc832375952bf2fe5d074d6eab59

--- buildSrc/src/test/kotlin/com/google/androidgamesdk/ToolchainEnumeratorTest.kt
+++ buildSrc/src/test/kotlin/com/google/androidgamesdk/ToolchainEnumeratorTest.kt
@@ -22,29 +22,28 @@ class ToolchainEnumeratorTest {
 
         // Do a few sanity checks on the enumerated toolchains
         assertEquals(
-            // 4 abis, 2 STLs, pre17 ndk/sdks:
-            (11 + 11 + 12 + 13 + 11 + 11 + 11 + 12 + 14) * 4 * 2,
+            416,
             allToolchains.size
         )
         assertEquals(
-            212,
+            104,
             allToolchains.filter { it.abi == "armeabi-v7a" }.count()
         )
         assertEquals(
-            424,
+            208,
             allToolchains.filter { it.stl == "c++_static" }.count()
         )
         assertEquals(
-            424,
+            208,
             allToolchains.filter { it.stl == "c++_shared" }.count()
         )
         assertEquals(
-            11 * 4 * 2, // 4 abis, 2 STLs, 11 pre17 ndk/sdks
+            0,
             allToolchains.filter { it.toolchain.getNdkVersion() == "r14" }
                 .count()
         )
         assertEquals(
-            11 * 4 * 2, // 4 abis, 2 STLs, 11 post17 ndk/sdks
+            64,
             allToolchains.filter { it.toolchain.getNdkVersion() == "r18" }
                 .count()
         )
@@ -58,18 +57,11 @@ class ToolchainEnumeratorTest {
 
         // Do a few sanity checks on the enumerated toolchains
         assertEquals(
-            // 2 32bits abis, 1 STLs, pre17 ndk/sdks:
-            2 * 1 * (11 + 11 + 12 + 13) +
-                // 2 32bits abis, 1 STL, post17 ndk/sdks:
-                2 * 1 * (11 + 11 + 11 + 12 + 14) +
-                // 2 64bits abis, 1 STLs, pre17 64 bits ndk/sdks:
-                2 * 1 * (4 + 5 + 6 + 7) +
-                // 2 64bits abis, 1 STL, post17 64bits ndk/sdks:
-                2 * 1 * (7 + 7 + 7 + 8 + 10),
+            196,
             allAarToolchains.size
         )
         assertEquals(
-            106,
+            52,
             allAarToolchains.filter { it.abi == "armeabi-v7a" }.count()
         )
         assertEquals(
@@ -77,18 +69,16 @@ class ToolchainEnumeratorTest {
             allAarToolchains.filter { it.stl == "c++_static" }.count()
         )
         assertEquals(
-            334,
+            196,
             allAarToolchains.filter { it.stl == "c++_shared" }.count()
         )
         assertEquals(
-            // 2 abis, 1 STLs, 4 r14 ndk/sdks + 2 abis, 1 STLs, 11 r14 ndk/sdks
-            4 * 2 * 1 + 11 * 2 * 1,
+            0,
             allAarToolchains.filter { it.toolchain.getNdkVersion() == "r14" }
                 .count()
         )
         assertEquals(
-            // 2 abis, 1 STLs, 7 r18 ndk/sdks + 2 abis, 1 STL, 11 r18 ndk/sdks
-            7 * 2 * 1 + 11 * 1 * 2,
+            30,
             allAarToolchains.filter { it.toolchain.getNdkVersion() == "r18" }
                 .count()
         )
