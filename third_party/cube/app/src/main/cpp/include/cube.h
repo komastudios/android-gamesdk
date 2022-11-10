2b73f68091f38985b13c9dd03e22e823e17e8b32
Vamsidhar reddy Gaddam <gvamsi@google.com>
2022-11-10 12:55:47 +0000

    Introduce swappy vulkan frame statistics API
    In this commit:
    * New API are added to the swappy vulkan API for collecting frame
      statistics
    * The API is not implemented, they are stubs
    * The cube sample is modified to use the new API and display the stats
      in the same manner as bouncyball sample.
    
    Bug: 123727506
    Test: Build and Run cube & bouncyball samples.
    Change-Id: I3321f2a49989f989e8e1a43f51e53a4fa006d912

--- third_party/cube/app/src/main/cpp/include/cube.h
+++ third_party/cube/app/src/main/cpp/include/cube.h
@@ -5,6 +5,10 @@
 #include <android/native_activity.h>
 #include <stdbool.h>
 
+#define VK_NO_PROTOTYPES 1
+#include <vulkan/vulkan.h>
+#undef VK_NO_PROTOTYPES
+
 // Struct for passing state from java app to native app.
 struct android_app_state {
     ANativeWindow* window;
@@ -22,3 +26,6 @@ void update_gpu_workload(int32_t new_workload);
 
 // Update the amount of CPU work done each frame.
 void update_cpu_workload(int32_t new_workload);
+
+// Get the current active swapchain.
+VkSwapchainKHR get_current_swapchain();
