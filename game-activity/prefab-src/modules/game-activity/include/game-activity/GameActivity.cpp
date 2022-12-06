14f4d57597f30846c07d4ac5cd5e5433049d81fe
Tom Murphy <tomnom@google.com>
2022-12-06 11:44:10 +0000

    Revert "Revert "Split historicalEventTimes in GameActivityMotionEvent into two.""
    This reverts commit 7d223593d5fd4203a5a2871a0a2bc5993f26d0db.
    
    Reason for revert: We can now remerge this now that the release is out.
    
    Change-Id: Ied49db7fdcbd212550c16a5917850888c3cbe80a

--- game-activity/prefab-src/modules/game-activity/include/game-activity/GameActivity.cpp
+++ game-activity/prefab-src/modules/game-activity/include/game-activity/GameActivity.cpp
@@ -1033,7 +1033,8 @@ static struct {
 extern "C" void GameActivityMotionEvent_destroy(
     GameActivityMotionEvent *c_event) {
     delete c_event->historicalAxisValues;
-    delete c_event->historicalEventTimes;
+    delete c_event->historicalEventTimesMillis;
+    delete c_event->historicalEventTimesNanos;
 }
 
 extern "C" void GameActivityMotionEvent_fromJava(
@@ -1141,12 +1142,16 @@ extern "C" void GameActivityMotionEvent_fromJava(
     out_event->historicalAxisValues =
         new float[historySize * pointerCount *
                   GAME_ACTIVITY_POINTER_INFO_AXIS_COUNT];
-    out_event->historicalEventTimes = new long[historySize];
+    out_event->historicalEventTimesMillis = new long[historySize];
+    out_event->historicalEventTimesNanos = new long[historySize];
 
     for (int historyIndex = 0; historyIndex < historySize; historyIndex++) {
-        out_event->historicalEventTimes[historyIndex] = env->CallLongMethod(
-            motionEvent, gMotionEventClassInfo.getHistoricalEventTime,
-            historyIndex);
+        out_event->historicalEventTimesMillis[historyIndex] =
+            env->CallLongMethod(motionEvent,
+                                gMotionEventClassInfo.getHistoricalEventTime,
+                                historyIndex);
+        out_event->historicalEventTimesNanos[historyIndex] =
+            out_event->historicalEventTimesMillis[historyIndex] * 1000000;
         for (int i = 0; i < pointerCount; ++i) {
             int pointerOffset = i * GAME_ACTIVITY_POINTER_INFO_AXIS_COUNT;
             int historyAxisOffset = historyIndex * pointerCount *
