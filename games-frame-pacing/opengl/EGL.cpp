7fb3224140bba11fabddf3fb683c8121c2e252b9
Vamsidhar reddy Gaddam <gvamsi@google.com>
2023-02-22 13:44:04 +0000

    [Experimental] Move to multiple syncs in EGL
    Bug: 267620851
    Test: N/A
    Change-Id: I9ac731a1e6a19f7c86384bcf2c4facd67a3f0004

--- games-frame-pacing/opengl/EGL.cpp
+++ games-frame-pacing/opengl/EGL.cpp
@@ -78,6 +78,13 @@ std::unique_ptr<EGL> EGL::create(std::chrono::nanoseconds fenceTimeout) {
         return nullptr;
     }
 
+    auto eglClientWaitSyncKHR = reinterpret_cast<eglClientWaitSyncKHR_type>(
+        eglGetProcAddress("eglClientWaitSyncKHR"));
+    if (eglClientWaitSyncKHR == nullptr) {
+        SWAPPY_LOGE("Failed to load eglClientWaitSyncKHR");
+        return nullptr;
+    }
+
     auto eglGetError =
         reinterpret_cast<eglGetError_type>(eglGetProcAddress("eglGetError"));
     if (eglGetError == nullptr) {
@@ -120,57 +127,39 @@ std::unique_ptr<EGL> EGL::create(std::chrono::nanoseconds fenceTimeout) {
     egl->eglSurfaceAttrib = eglSurfaceAttrib;
     egl->eglGetNextFrameIdANDROID = eglGetNextFrameIdANDROID;
     egl->eglGetFrameTimestampsANDROID = eglGetFrameTimestampsANDROID;
+    egl->mWaiterThreadContext.dummy = -42;
+
+    std::lock_guard<std::mutex> lock(egl->mWaiterThreadContext.lock);
+    egl->mWaiterThreadContext.thread =
+        Thread([&]() { egl->waitForFenceThreadMain(); });
+    SWAPPY_LOGE("[VAMSI] The creation context is %p",
+                &egl->mWaiterThreadContext);
+
     return egl;
 }
 
 EGL::~EGL() {
-    if (eglLib) {
-        dlclose(eglLib);
+    // Stop the fence waiter thread
+    {
+        std::lock_guard<std::mutex> lock(mWaiterThreadContext.lock);
+        mWaiterThreadContext.running = false;
+        mWaiterThreadContext.condition.notify_one();
     }
-}
-void EGL::resetSyncFence(EGLDisplay display) {
-    std::lock_guard<std::mutex> lock(mSyncFenceMutex);
 
-    if (mFenceWaiter.waitForIdle() && mSyncFence != EGL_NO_SYNC_KHR) {
-        EGLBoolean result = eglDestroySyncKHR(display, mSyncFence);
+    mWaiterThreadContext.thread.join();
+
+    while (mWaitPendingSyncs.size() > 0) {
+        auto sync = mWaitPendingSyncs.front();
+        mWaitPendingSyncs.pop_front();
+        // There is no need to wait here as the API allows for queueing pending
+        // sync for deleting.
+        EGLBoolean result = eglDestroySyncKHR(sync.display, sync.fence);
         if (result == EGL_FALSE) {
             SWAPPY_LOGE("Failed to destroy sync fence");
         }
     }
-
-    mSyncFence = eglCreateSyncKHR(display, EGL_SYNC_FENCE_KHR, nullptr);
-
-    if (mSyncFence != EGL_NO_SYNC_KHR) {
-        // kick of the thread work to wait for the fence and measure its time
-        mFenceWaiter.onFenceCreation(display, mSyncFence);
-    } else {
-        SWAPPY_LOGE("Failed to create sync fence");
-    }
-}
-
-bool EGL::lastFrameIsComplete(EGLDisplay display) {
-    std::lock_guard<std::mutex> lock(mSyncFenceMutex);
-
-    // This will be the case on the first frame
-    if (mSyncFence == EGL_NO_SYNC_KHR) {
-        return true;
-    }
-
-    EGLint status = 0;
-    EGLBoolean result =
-        eglGetSyncAttribKHR(display, mSyncFence, EGL_SYNC_STATUS_KHR, &status);
-    if (result == EGL_FALSE) {
-        SWAPPY_LOGE("Failed to get sync status");
-        return true;
-    }
-
-    if (status == EGL_SIGNALED_KHR) {
-        return true;
-    } else if (status == EGL_UNSIGNALED_KHR) {
-        return false;
-    } else {
-        SWAPPY_LOGE("Unexpected sync status: %d", status);
-        return true;
+    if (eglLib) {
+        dlclose(eglLib);
     }
 }
 
@@ -251,93 +240,103 @@ std::unique_ptr<EGL::FrameTimestamps> EGL::getFrameTimestamps(
 #endif
 }
 
-EGL::FenceWaiter::FenceWaiter(std::chrono::nanoseconds fenceTimeout,
-                              eglGetProcAddress_type getProcAddress)
-    : mFenceTimeout(fenceTimeout) {
-    std::unique_lock<std::mutex> lock(mFenceWaiterLock);
+void EGL::insertSyncFence(EGLDisplay display) {
+    EGLSyncKHR sync_fence =
+        eglCreateSyncKHR(display, EGL_SYNC_FENCE_KHR, nullptr);
 
-    eglClientWaitSyncKHR = reinterpret_cast<eglClientWaitSyncKHR_type>(
-        getProcAddress("eglClientWaitSyncKHR"));
-    if (eglClientWaitSyncKHR == nullptr)
-        SWAPPY_LOGE("Failed to load eglClientWaitSyncKHR");
-    eglDestroySyncKHR = reinterpret_cast<eglDestroySyncKHR_type>(
-        getProcAddress("eglDestroySyncKHR"));
-    if (eglDestroySyncKHR == nullptr)
-        SWAPPY_LOGE("Failed to load eglDestroySyncKHR");
-
-    mFenceWaiter = Thread([this]() { threadMain(); });
-}
-
-EGL::FenceWaiter::~FenceWaiter() {
-    {
-        std::lock_guard<std::mutex> lock(mFenceWaiterLock);
-        mFenceWaiterRunning = false;
-        mFenceWaiterCondition.notify_all();
+    if (sync_fence != EGL_NO_SYNC_KHR) {
+        SWAPPY_LOGE("[VAMSI] Created a fence and triggering notify - contxt %p",
+                    &mWaiterThreadContext);
+        EGLSync sync = {display, sync_fence};
+        // kick of the thread work to wait for the fence and measure its time
+        std::lock_guard<std::mutex> lock(mWaiterThreadContext.lock);
+        mWaitPendingSyncs.push_back(sync);
+        mWaiterThreadContext.hasPendingWork = true;
+        mWaiterThreadContext.condition.notify_all();
+    } else {
+        SWAPPY_LOGE("Failed to create sync fence");
     }
-    mFenceWaiter.join();
 }
 
-bool EGL::FenceWaiter::waitForIdle() {
-    std::lock_guard<std::mutex> lock(mFenceWaiterLock);
-    mFenceWaiterCondition.wait(
-        mFenceWaiterLock,
-        [this]() REQUIRES(mFenceWaiterLock) { return !mFenceWaiterPending; });
-    return mSyncFence != EGL_NO_SYNC_KHR;
+bool EGL::lastFrameIsComplete(EGLDisplay display, bool pipelineMode) {
+    std::lock_guard<std::mutex> lock(mWaiterThreadContext.lock);
+    if (pipelineMode) {
+        SWAPPY_LOGE("[VAMSI] The size is %d", (int)mWaitPendingSyncs.size());
+        // We are in pipeline mode so we need to check the fence of frame N-1
+        return mWaitPendingSyncs.size() < 3;
+    }
+    // We are not in pipeline mode so we need to check the fence the current
+    // frame. i.e. there are not unsignaled frames
+    return mWaitPendingSyncs.empty();
 }
 
-void EGL::FenceWaiter::onFenceCreation(EGLDisplay display,
-                                       EGLSyncKHR syncFence) {
-    std::lock_guard<std::mutex> lock(mFenceWaiterLock);
-    mDisplay = display;
-    mSyncFence = syncFence;
-    mFenceWaiterPending = true;
-    mFenceWaiterCondition.notify_all();
-}
+void EGL::waitForFenceThreadMain() {
+    pthread_setname_np(pthread_self(), "EGL Fence Waiter");
+    SWAPPY_LOGE("[VAMSI] thread running context %p and %d",
+                &mWaiterThreadContext, mWaiterThreadContext.dummy);
+    while (true) {
+        bool waitingSyncsEmpty;
+        {
+            std::lock_guard<std::mutex> lock(mWaiterThreadContext.lock);
+            SWAPPY_LOGE("[VAMSI] thread running context %p and %d",
+                        &mWaiterThreadContext, mWaiterThreadContext.dummy);
+            mWaiterThreadContext.condition.wait(
+                mWaiterThreadContext.lock,
+                [&]() REQUIRES(mWaiterThreadContext.lock) {
+                    return mWaiterThreadContext.hasPendingWork ||
+                           !mWaiterThreadContext.running;
+                });
 
-void EGL::FenceWaiter::threadMain() {
-    std::lock_guard<std::mutex> lock(mFenceWaiterLock);
-    while (mFenceWaiterRunning) {
-        // wait for new fence object
-        mFenceWaiterCondition.wait(
-            mFenceWaiterLock, [this]() REQUIRES(mFenceWaiterLock) {
-                return mFenceWaiterPending || !mFenceWaiterRunning;
-            });
+            mWaiterThreadContext.hasPendingWork = false;
 
-        if (!mFenceWaiterRunning) {
-            break;
-        }
-
-        gamesdk::ScopedTrace tracer("Swappy: GPU frame time");
-        const auto startTime = std::chrono::steady_clock::now();
-        EGLBoolean result = eglClientWaitSyncKHR(mDisplay, mSyncFence, 0,
-                                                 mFenceTimeout.count());
-        switch (result) {
-            case EGL_FALSE:
-                SWAPPY_LOGE("Failed to wait sync");
+            if (!mWaiterThreadContext.running) {
                 break;
-            case EGL_TIMEOUT_EXPIRED_KHR:
-                SWAPPY_LOGE("Timeout waiting for fence");
-                break;
-        }
-        if (result != EGL_CONDITION_SATISFIED_KHR) {
-            result = eglDestroySyncKHR(mDisplay, mSyncFence);
-            if (result == EGL_FALSE) {
-                SWAPPY_LOGE("Failed to destroy sync fence");
             }
-            mSyncFence = EGL_NO_SYNC_KHR;
+
+            waitingSyncsEmpty = mWaitPendingSyncs.empty();
         }
 
-        mFencePendingTime = std::chrono::steady_clock::now() - startTime;
+        while (!waitingSyncsEmpty) {
+            SWAPPY_LOGE("[VAMSI] thread is running with non-empty sync");
+            EGLSync sync;
+            {
+                // Get the latest fence to wait on.
+                std::lock_guard<std::mutex> lock(mWaiterThreadContext.lock);
+                sync = mWaitPendingSyncs.front();
+            }
 
-        mFenceWaiterPending = false;
-        mFenceWaiterCondition.notify_all();
+            gamesdk::ScopedTrace tracer("Swappy: GPU frame time");
+            const auto startTime = std::chrono::steady_clock::now();
+            SWAPPY_LOGE("[VAMSI] eglentry point is %p", eglClientWaitSyncKHR);
+            SWAPPY_LOGE("[VAMSI] eglentry point is %p", eglDestroySyncKHR);
+            EGLBoolean result = eglClientWaitSyncKHR(sync.display, sync.fence,
+                                                     0, mFenceTimeout.count());
+            switch (result) {
+                case EGL_FALSE:
+                    SWAPPY_LOGE("Failed to wait sync");
+                    break;
+                case EGL_TIMEOUT_EXPIRED_KHR:
+                    SWAPPY_LOGE("Timeout waiting for fence");
+                    break;
+            }
+
+            mFencePendingTime = std::chrono::steady_clock::now() - startTime;
+
+            {
+                std::lock_guard<std::mutex> lock(mWaiterThreadContext.lock);
+                mWaitPendingSyncs.pop_front();
+
+                // Once the wait has timed out/succeeded, we can submit it for
+                // deletion as the API allows for pending syncs to be queued for
+                // deletion.
+                result = eglDestroySyncKHR(sync.display, sync.fence);
+                if (result == EGL_FALSE) {
+                    SWAPPY_LOGE("Failed to destroy sync fence");
+                }
+                waitingSyncsEmpty = mWaitPendingSyncs.empty();
+            }
+        }
     }
-}
-
-std::chrono::nanoseconds EGL::FenceWaiter::getFencePendingTime() const {
-    // return mFencePendingTime without a lock to avoid blocking the main thread
-    // worst case, the time will be of some previous frame
-    return mFencePendingTime.load();
 }
 
 }  // namespace swappy
