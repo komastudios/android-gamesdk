c40fe2613ee06f4f510f30b368ac4814ae106498
Tom Murphy <tomnom@google.com>
2023-03-22 14:51:43 +0000

    Use int64_t instead of long for historicalEventTimes to avoid overrun on 32bit systems
    historicalEventTimesNanos uses long instead of int64_t, on 32 bit platform,
    it's only 4 bytes, thus time value can easily overrun.
    
    Test: compiled and ran sample app
    Bug: 274641135
    (cherry picked from https://android-review.googlesource.com/q/commit:e7b4b6857c026609f542ba4c332c8232ad7f3428)
    Merged-In: I9f3c246f1e09d9a7ecd0a9f6d5424314a17c7322
    Change-Id: I9f3c246f1e09d9a7ecd0a9f6d5424314a17c7322

--- game-activity/prefab-src/modules/game-activity/include/game-activity/GameActivityEvents.cpp
+++ game-activity/prefab-src/modules/game-activity/include/game-activity/GameActivityEvents.cpp
@@ -249,8 +249,8 @@ extern "C" void GameActivityMotionEvent_fromJava(
     out_event->historicalAxisValues =
         new float[historySize * pointerCount *
                   GAME_ACTIVITY_POINTER_INFO_AXIS_COUNT];
-    out_event->historicalEventTimesMillis = new long[historySize];
-    out_event->historicalEventTimesNanos = new long[historySize];
+    out_event->historicalEventTimesMillis = new int64_t[historySize];
+    out_event->historicalEventTimesNanos = new int64_t[historySize];
 
     for (int historyIndex = 0; historyIndex < historySize; historyIndex++) {
         out_event->historicalEventTimesMillis[historyIndex] =
