57b02c94a3df2f266d145dfcb3b19ed7ce558b05
Victor Repetskyi <vrepets@google.com>
2023-03-31 15:20:07 +0000

    Restructure project to run the native unit tests via connected Adnroid Bug: b/259440337 Test: ./gradlew :games-controller:connectedAndroidTest (cherry picked from https://android-review.googlesource.com/q/commit:47e48a7b20d53144c11707d55b796eb29380c5e7) Merged-In: I3a921eb838b7121d33524f781436d176ef167b5c Change-Id: I3a921eb838b7121d33524f781436d176ef167b5c

--- games-controller/src/androidTest/java/com/google/android/games/paddleboat/ExampleInstrumentedTest.java
+++ games-controller/src/androidTest/java/com/google/android/games/paddleboat/ExampleInstrumentedTest.java
@@ -13,26 +13,25 @@
 // limitations under the License.
 package com.google.android.games.paddleboat;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import static org.junit.Assert.*;
 
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void testAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        android.content.Context appContext =
+            InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.google.android.games.paddleboat.test", appContext.getPackageName());
     }
 }
