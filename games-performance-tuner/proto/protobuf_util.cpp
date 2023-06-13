720d4bc4eff1b853b4b96f6993eae463a194c351
Vamsidhar reddy Gaddam <gvamsi@google.com>
2023-06-13 13:31:59 +0000

    Modify the predictability API in APT
    Now the client API gives both the expected frame time and the
    fidelity parameters for all the quality levels in the game.
    
    In addition the feedback from API review (b/277771868) is also addressed.
    
    Test: Local test
    Bug: 282617180
    Change-Id: I9b731a9aa4181ba3035922f85490bcb742e04d49

--- games-performance-tuner/proto/protobuf_util.cpp
+++ games-performance-tuner/proto/protobuf_util.cpp
@@ -25,11 +25,19 @@ extern "C" void TuningFork_CProtobufSerialization_Dealloc(
     }
 }
 
-extern "C" void TuningFork_CProtobufArray_Dealloc(
-    TuningFork_CProtobufArray *array) {
-    for (int i = 0; i < array->size; i++) {
-        TuningFork_CProtobufSerialization_free(&(array->protobufs[i]));
+namespace tuningfork {
+
+void ToCQualityLevelPredictions(const QLTimePredictions& pred,
+                                TuningFork_QualityLevelPredictions& c_pred) {
+    c_pred.fidelity_params = new TuningFork_CProtobufSerialization[pred.size()];
+    c_pred.predicted_time_us = new uint32_t[pred.size()];
+
+    for (int i = 0; i < pred.size(); i++) {
+        ToCProtobufSerialization(pred[i].first, c_pred.fidelity_params[i]);
+        c_pred.predicted_time_us[i] = pred[i].second;
     }
-    free(array->protobufs);
-    array->size = 0;
+
+    c_pred.size = pred.size();
 }
+
+}  // namespace tuningfork
