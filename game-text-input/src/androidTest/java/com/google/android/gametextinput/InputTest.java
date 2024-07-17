306de7fc2cf13199acad435d77484cf4ca0d9d8c
Artyom Palvelev <artyompp@google.com>
2024-07-17 16:23:25 +0100

    fix tests for GameTextInput
    This CL fixes Activity lifecycle issues with Espresso. Now GameTextInput
    has functioning tests for both software and hardware keyboard inputs.
    
    Bug: 343915764
    Test: run ./build.sh tests
    Change-Id: I697d6bcb8b114e44c01b8661bda2405543db4f9e

--- game-text-input/src/androidTest/java/com/google/android/gametextinput/InputTest.java
+++ game-text-input/src/androidTest/java/com/google/android/gametextinput/InputTest.java
@@ -24,18 +24,12 @@ import org.junit.runner.RunWith;
 import java.util.concurrent.TimeUnit;
 import android.view.KeyEvent;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.*;
-// import static androidx.test.espresso.action.ViewActions.click;
-// import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-// import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -52,39 +46,100 @@ import com.google.androidgamesdk.gametextinput.test.MainActivity;
 @LargeTest
 public class InputTest {
 
-    static private final String STRING_TO_BE_TYPED = "this is test text";
-    static private final String RESULTING_STRING = "1";
+    static private final String INITIAL_VALUE = "this is test text";
 
     @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
+    public void canTypeCharOnSoftwareKeyboard() {
+      onView(withId(R.id.displayed_text)).check(matches(withText(INITIAL_VALUE)));
+
+      activityScenarioRule.getScenario().onActivity(activity -> activity.enableSoftKeyboard());
+
+      // Type some text
+      onView(withId(R.id.input_enabled_text_view)).perform(typeText("s"), closeSoftKeyboard());
+
+      // Check that the text was changed.
+      onView(withId(R.id.displayed_text)).check(matches(withText("s")));
+    }
+
+    @Test
     public void canTypeTextOnSoftwareKeyboard() {
-      try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
-        onView(withId(R.id.displayed_text)).check(matches(withText(STRING_TO_BE_TYPED)));
+      onView(withId(R.id.displayed_text)).check(matches(withText(INITIAL_VALUE)));
 
-        activityScenarioRule.getScenario().onActivity(activity -> activity.enableSoftKeyboard());
+      activityScenarioRule.getScenario().onActivity(activity -> activity.enableSoftKeyboard());
 
-        // Type some text
-        onView(withId(R.id.input_enabled_text_view)).perform(typeText("1"), closeSoftKeyboard());
+      // Type some text
+      onView(withId(R.id.input_enabled_text_view)).perform(typeText("abcdef"), closeSoftKeyboard());
 
-        // Check that the text was changed.
-        onView(withId(R.id.displayed_text)).check(matches(withText(RESULTING_STRING)));
-      }
+      // Check that the text was changed.
+      onView(withId(R.id.displayed_text)).check(matches(withText("abcdef")));
+    }
+
+    @Test
+    public void canTypeCharOnHardwareKeyboard() {
+      onView(withId(R.id.displayed_text)).check(matches(withText(INITIAL_VALUE)));
+
+      activityScenarioRule.getScenario().onActivity(activity -> activity.enableSoftKeyboard());
+
+      // Type some text
+      onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_1), closeSoftKeyboard());
+
+      // Check that the text was changed.
+      onView(withId(R.id.displayed_text)).check(matches(withText("1")));
     }
 
     @Test
     public void canTypeTextOnHardwareKeyboard() {
-      try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
-        onView(withId(R.id.displayed_text)).check(matches(withText(STRING_TO_BE_TYPED)));
+      onView(withId(R.id.displayed_text)).check(matches(withText(INITIAL_VALUE)));
 
-        activityScenarioRule.getScenario().onActivity(activity -> activity.enableSoftKeyboard());
+      activityScenarioRule.getScenario().onActivity(activity -> activity.enableSoftKeyboard());
 
-        // Type some text
-        onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_1), closeSoftKeyboard());
+      // Type some text
+      onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_1));
+      onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_A));
+      onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_2));
+      onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_Z));
+      onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_9));
+      onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_R));
+      onView(withId(R.id.input_enabled_text_view)).perform(closeSoftKeyboard());
 
-        // Check that the text was changed.
-        onView(withId(R.id.displayed_text)).check(matches(withText(RESULTING_STRING)));
-      }
+      // Check that the text was changed.
+      onView(withId(R.id.displayed_text)).check(matches(withText("1a2z9r")));
+    }
+
+    @Test
+    public void backspaceWorks() {
+      onView(withId(R.id.displayed_text)).check(matches(withText(INITIAL_VALUE)));
+
+      activityScenarioRule.getScenario().onActivity(activity -> activity.enableSoftKeyboard());
+
+      // Type some text
+      onView(withId(R.id.input_enabled_text_view)).perform(typeText("abcdef"));
+      onView(withId(R.id.input_enabled_text_view)).perform(pressKey(KeyEvent.KEYCODE_DEL));
+      onView(withId(R.id.input_enabled_text_view)).perform(closeSoftKeyboard());
+
+      // Check that the text was changed.
+      onView(withId(R.id.displayed_text)).check(matches(withText("abcde")));
+    }
+
+    @Test
+    public void multipleBackspaceWork() {
+      onView(withId(R.id.displayed_text)).check(matches(withText(INITIAL_VALUE)));
+
+      activityScenarioRule.getScenario().onActivity(activity -> activity.enableSoftKeyboard());
+
+      // Type some text
+      onView(withId(R.id.input_enabled_text_view)).perform(typeText("abcdef"));
+      onView(withId(R.id.input_enabled_text_view)).perform(
+          pressKey(KeyEvent.KEYCODE_DEL),
+          pressKey(KeyEvent.KEYCODE_DEL),
+          pressKey(KeyEvent.KEYCODE_DEL));
+      onView(withId(R.id.input_enabled_text_view)).perform(typeText("xyz"));
+      onView(withId(R.id.input_enabled_text_view)).perform(closeSoftKeyboard());
+
+      // Check that the text was changed.
+      onView(withId(R.id.displayed_text)).check(matches(withText("abcxyz")));
     }
 }
