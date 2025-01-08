f7656b84151dbc1570c8730861b7b5d3ed6be1db
Oleh Kuznetsov <okuznetsov@google.com>
2025-01-08 14:35:35 +0000

    GameTextInput: Handle IME_ACTION_UNSPECIFIED editor action as 'Enter'
    Fixes swiftkey multiline edit
    
    Test: manual
    Fix: 385287245
    Change-Id: I1cf3bffd4549c68396dd5480248e769cc85725e5

--- game-text-input/src/main/java/com/google/androidgamesdk/gametextinput/InputConnection.java
+++ game-text-input/src/main/java/com/google/androidgamesdk/gametextinput/InputConnection.java
@@ -15,10 +15,10 @@
  */
 package com.google.androidgamesdk.gametextinput;
 
+import static android.view.inputmethod.EditorInfo.IME_ACTION_UNSPECIFIED;
+
 import android.app.Activity;
 import android.content.Context;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -43,7 +43,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.google.androidgamesdk.gametextinput.GameTextInput.Pair;
-import java.util.BitSet;
 
 @Keep
 public class InputConnection extends BaseInputConnection implements View.OnKeyListener {
@@ -428,7 +427,7 @@ public class InputConnection extends BaseInputConnection implements View.OnKeyLi
         && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER
             || event.getKeyCode() == KeyEvent.KEYCODE_NUMPAD_ENTER)
         && event.hasNoModifiers()) {
-      this.performEditorAction(settings.mEditorInfo.actionId);
+      sendEditorAction(settings.mEditorInfo.actionId);
       return true;
     }
     if (event.getAction() != 0) {
@@ -609,12 +608,25 @@ public class InputConnection extends BaseInputConnection implements View.OnKeyLi
   @Override
   public boolean performEditorAction(int action) {
     Log.d(TAG, "performEditorAction, action=" + action);
+    if (action == IME_ACTION_UNSPECIFIED) {
+      // Super emulates Enter key press/release
+      return super.performEditorAction(action);
+    }
+    return sendEditorAction(action);
+  }
+
+  /**
+   * Delivers editor action to listener
+   *
+   * @param action Action code, either one from EditorInfo.imeOptions or a custom one.
+   * @return Returns true on success, false if the input connection is no longer valid.
+   */
+  private boolean sendEditorAction(int action) {
     Listener listener = this.listener;
     if (listener != null) {
       listener.onEditorAction(action);
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
 }
